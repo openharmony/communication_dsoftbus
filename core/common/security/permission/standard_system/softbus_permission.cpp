de7fbff4b5f73777fcc6da9cc5131663cb004ead
我是一只小小鸟 <zhangbing6@huawei.com>
2021-07-19 02:18:14 +0000

    update core/common/security/permission/standard_system/softbus_permission.cpp.

--- core/common/security/permission/standard_system/softbus_permission.cpp
+++ core/common/security/permission/standard_system/softbus_permission.cpp
@@ -38,9 +38,6 @@ namespace {
     const std::string DANGER_APP_PERMISSION = "ohos.permission.DISTRIBUTED_DATASYNC";
     const std::string BIND_DISCOVER_SERVICE = "com.huawei.hwddmp.permission.BIND_DISCOVER_SERVICE";
     const int32_t SYSTEM_UID = 1000;
-    const int32_t SHELL_UID = 2000;
-    const int32_t INVALID_UID = -1;
-    const int32_t FIRST_APPLICATION_UID = 10000;
     const int32_t MULTE_USER_RADIX = 100000;
 
 #ifdef PERMISSION_TEST
@@ -59,36 +56,6 @@ namespace {
         OHOS::sptr<IBundleMgr> bmgr = OHOS::iface_cast<IBundleMgr>(bundleObj);
         return bmgr->CheckPermission(std::string(pkgName), std::string(permission));
     }
-
-    int32_t GetPermType(int32_t callingUid, int32_t callingPid, const char *pkgName)
-    {
-        std::string pkg;
-        if (pkgName != nullptr) {
-            pkg = std::string(pkgName);
-        }
-        if (callingUid == SHELL_UID) {
-            return SOFTBUS_PERMISSION_DENIED;
-        }
-        if (callingUid == static_cast<int32_t>(getuid()) &&
-            callingPid == static_cast<int32_t>(getpid())) {
-            return SELF_APP;
-        }
-        int32_t ret = CheckSystemPermission(pkg, SYSTEM_APP_PERMISSION);
-        if (ret == PERMISSION_GRANTED) {
-            LOG_INFO("system app");
-            return SYSTEM_APP;
-        }
-        ret = CheckSystemPermission(pkg, DANGER_APP_PERMISSION);
-        if (ret == PERMISSION_GRANTED) {
-            LOG_INFO("normal app");
-            return NORMAL_APP;
-        }
-        if (callingUid > INVALID_UID && callingUid < FIRST_APPLICATION_UID && callingUid != SHELL_UID) {
-            LOG_INFO("native app");
-            return NATIVE_APP;
-        }
-        return SOFTBUS_PERMISSION_DENIED;
-    }
 }
 
 int32_t TransPermissionInit()
@@ -103,7 +70,9 @@ void TransPermissionDeinit(void)
 
 int32_t CheckTransPermission(const char *sessionName, const char *pkgName, uint32_t actions)
 {
-    int32_t ret;
+    if (sessionName == nullptr || pkgName == nullptr) {
+        return SOFTBUS_PERMISSION_DENIED;
+    }
 #ifdef PERMISSION_TEST
     int32_t callingUid = TEST_UID;
     int32_t callingPid = TEST_PID;
@@ -111,24 +80,11 @@ int32_t CheckTransPermission(const char *sessionName, const char *pkgName, uint3
     int32_t callingUid = OHOS::IPCSkeleton::GetCallingUid();
     int32_t callingPid = OHOS::IPCSkeleton::GetCallingPid();
 #endif
-    int32_t permType = GetPermType(callingUid, callingPid, pkgName);
-    if (permType < 0) {
-        return permType;
-    }
-    if (sessionName == nullptr && pkgName == nullptr) {
-        return permType;
-    }
-    if (sessionName == nullptr && pkgName != nullptr) {
-        if (IsValidPkgName(callingUid, pkgName) == SOFTBUS_OK) {
-            return permType;
-        }
-        return SOFTBUS_PERMISSION_DENIED;
-    }
-    SoftBusPermissionItem *pItem = CreatePermissionItem(permType, callingUid, callingPid, pkgName, actions);
+    SoftBusPermissionItem *pItem = CreatePermissionItem(NATIVE_APP, callingUid, callingPid, pkgName, actions);
     if (pItem == nullptr) {
         return SOFTBUS_MALLOC_ERR;
     }
-    ret = CheckPermissionEntry(sessionName, pItem);
+    int32_t ret = CheckPermissionEntry(sessionName, pItem);
     SoftBusFree(pItem);
     return ret;
 }
