2a7f0431b131e6a0aa86089cdb2a62f28b1483a8
zhufenghao <2581725389@qq.com>
2021-07-26 11:47:28 +0800

    spec codex clean

--- core/common/softbus_property/src/softbus_feature_config.c
+++ core/common/softbus_property/src/softbus_feature_config.c
@@ -113,7 +113,9 @@ int SoftbusSetConfig(ConfigType type, const unsigned char *val, int len)
     if ((type >= SOFTBUS_CONFIG_TYPE_MAX) || (type != g_configItems[type].type)) {
         return SOFTBUS_ERR;
     }
-    (void)memcpy_s(g_configItems[type].val, g_configItems[type].len, val, len);
+    if (memcpy_s(g_configItems[type].val, g_configItems[type].len, val, len) != EOK) {
+        return SOFTBUS_ERR;
+    }
     return SOFTBUS_OK;
 }
 
@@ -125,7 +127,9 @@ int SoftbusGetConfig(ConfigType type, unsigned char *val, int len)
     if ((type >= SOFTBUS_CONFIG_TYPE_MAX) || (type != g_configItems[type].type)) {
         return SOFTBUS_ERR;
     }
-    (void)memcpy_s((void*)val, len, g_configItems[type].val, g_configItems[type].len);
+    if (memcpy_s((void*)val, len, g_configItems[type].val, g_configItems[type].len) != EOK) {
+	return SOFTBUS_ERR;
+    }
     return SOFTBUS_OK;
 }
 
