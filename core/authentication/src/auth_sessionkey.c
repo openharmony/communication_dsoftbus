dbe488b83feb7bdb929078ca4d60b018200fc85a
YOUR_NAME <zhuyan40@huawei.com>
2021-08-09 06:33:52 +0000

    Description:softbus leave lnn modify Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <zhuyan40@huawei.com>

--- core/authentication/src/auth_sessionkey.c
+++ core/authentication/src/auth_sessionkey.c
@@ -38,7 +38,7 @@ void AuthSetLocalSessionKey(const NecessaryDevInfo *devInfo, const char *peerUdi
 {
     uint32_t listSize = 0;
     if (devInfo == NULL || peerUdid == NULL || sessionKey == NULL) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return;
     }
     SessionKeyList *sessionKeyList = NULL;
@@ -56,7 +56,7 @@ void AuthSetLocalSessionKey(const NecessaryDevInfo *devInfo, const char *peerUdi
     }
     sessionKeyList = (SessionKeyList *)SoftBusMalloc(sizeof(SessionKeyList));
     if (sessionKeyList == NULL) {
-        LOG_ERR("SoftBusMalloc failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "SoftBusMalloc failed");
         return;
     }
     (void)memset_s(sessionKeyList, sizeof(SessionKeyList), 0, sizeof(SessionKeyList));
@@ -64,34 +64,35 @@ void AuthSetLocalSessionKey(const NecessaryDevInfo *devInfo, const char *peerUdi
     sessionKeyList->side = devInfo->side;
     sessionKeyList->seq = devInfo->seq;
     if (memcpy_s(sessionKeyList->peerUdid, UDID_BUF_LEN, peerUdid, strlen(peerUdid)) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         SoftBusFree(sessionKeyList);
         return;
     }
     if (memcpy_s(sessionKeyList->deviceKey, MAX_DEVICE_KEY_LEN, devInfo->deviceKey, devInfo->deviceKeyLen) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         SoftBusFree(sessionKeyList);
         return;
     }
     sessionKeyList->deviceKeyLen = devInfo->deviceKeyLen;
     if (memcpy_s(sessionKeyList->sessionKey, SESSION_KEY_LENGTH, sessionKey, sessionKeyLen) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         SoftBusFree(sessionKeyList);
         return;
     }
     sessionKeyList->sessionKeyLen = sessionKeyLen;
+    SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_INFO, "auth add sessionkey, seq is:%d", sessionKeyList->seq);
     ListNodeInsert(&g_sessionKeyListHead, &sessionKeyList->node);
 }
 
 bool AuthIsDeviceVerified(uint32_t type, const char *deviceKey, uint32_t deviceKeyLen)
 {
     if (deviceKey == NULL) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return false;
     }
     SessionKeyList *sessionKeyList = NULL;
     if (IsListEmpty(&g_sessionKeyListHead) == true) {
-        LOG_WARN("no session key in memory, need to verify device");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_WARN, "no session key in memory, need to verify device");
         return false;
     }
     ListNode *item = NULL;
@@ -108,7 +109,7 @@ bool AuthIsSeqInKeyList(int32_t seq)
 {
     SessionKeyList *sessionKeyList = NULL;
     if (IsListEmpty(&g_sessionKeyListHead) == true) {
-        LOG_WARN("no session key in memory");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_WARN, "no session key in memory");
         return false;
     }
     ListNode *item = NULL;
@@ -124,44 +125,36 @@ bool AuthIsSeqInKeyList(int32_t seq)
 static SessionKeyList *AuthGetLastSessionKey(const NecessaryDevInfo *devInfo)
 {
     if (devInfo == NULL) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return NULL;
     }
     SessionKeyList *sessionKeyList = NULL;
     if (IsListEmpty(&g_sessionKeyListHead) == true) {
-        LOG_ERR("no session key in memory");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "no session key in memory");
         return NULL;
     }
     ListNode *item = NULL;
     LIST_FOR_EACH(item, &g_sessionKeyListHead) {
         sessionKeyList = LIST_ENTRY(item, SessionKeyList, node);
-        if (sessionKeyList->type == devInfo->type && sessionKeyList->side == CLIENT_SIDE_FLAG &&
+        if (sessionKeyList->type == devInfo->type &&
             strncmp(sessionKeyList->deviceKey, devInfo->deviceKey, devInfo->deviceKeyLen) == 0) {
-            LOG_INFO("get client last session key succ");
+            SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_INFO, "auth get last session key succ, seq is:%d", sessionKeyList->seq);
             return sessionKeyList;
         }
     }
-    LIST_FOR_EACH(item, &g_sessionKeyListHead) {
-        sessionKeyList = LIST_ENTRY(item, SessionKeyList, node);
-        if (sessionKeyList->type == devInfo->type && sessionKeyList->side == SERVER_SIDE_FLAG &&
-            strncmp(sessionKeyList->deviceKey, devInfo->deviceKey, devInfo->deviceKeyLen) == 0) {
-            LOG_INFO("get server last session key succ");
-            return sessionKeyList;
-        }
-    }
-    LOG_ERR("auth get last session key failed");
+    SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "auth get last session key failed");
     return NULL;
 }
 
 static SessionKeyList *GetSessionKeyByDevinfo(const NecessaryDevInfo *devInfo)
 {
     if (devInfo == NULL) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return NULL;
     }
     SessionKeyList *sessionKeyList = NULL;
     if (IsListEmpty(&g_sessionKeyListHead) == true) {
-        LOG_ERR("no session key in memory");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "no session key in memory");
         return NULL;
     }
     ListNode *item = NULL;
@@ -170,19 +163,76 @@ static SessionKeyList *GetSessionKeyByDevinfo(const NecessaryDevInfo *devInfo)
         if (sessionKeyList->type == devInfo->type &&
             sessionKeyList->seq == devInfo->seq &&
             strncmp(sessionKeyList->deviceKey, devInfo->deviceKey, devInfo->deviceKeyLen) == 0) {
-            LOG_INFO("get session key seccessfully.");
+            SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_INFO, "auth get session key by dev info succ, seq is:%d", sessionKeyList->seq);
             return sessionKeyList;
         }
     }
-    LOG_ERR("auth cannot find session key by seq");
+    SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "auth cannot find session key by dev info");
     return NULL;
+}
+
+static SessionKeyList *AuthGetSessionKeyBySeq(int32_t seq)
+{
+    SessionKeyList *sessionKeyList = NULL;
+    if (IsListEmpty(&g_sessionKeyListHead) == true) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "no session key in memory");
+        return NULL;
+    }
+    SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_INFO, "auth get session key by seq %d", seq);
+    ListNode *item = NULL;
+    ListNode *tmp = NULL;
+    LIST_FOR_EACH_SAFE(item, tmp, &g_sessionKeyListHead) {
+        sessionKeyList = LIST_ENTRY(item, SessionKeyList, node);
+        if (sessionKeyList->seq == seq) {
+            SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_INFO, "auth get session key by seq seccessfully.");
+            return sessionKeyList;
+        }
+    }
+    SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "auth get session key by seq failed");
+    return NULL;
+}
+
+int32_t AuthEncryptBySeq(int32_t seq, AuthSideFlag *side, uint8_t *data, uint32_t len, OutBuf *outBuf)
+{
+    if (data == NULL || outBuf == NULL || outBuf->bufLen < (len + ENCRYPT_OVER_HEAD_LEN)) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
+        return SOFTBUS_INVALID_PARAM;
+    }
+    SessionKeyList *sessionKeyList = NULL;
+    NecessaryDevInfo devInfo = {0};
+    uint32_t outLen;
+
+    sessionKeyList = AuthGetSessionKeyBySeq(seq);
+    if (sessionKeyList == NULL) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "AuthGetLastSessionKey failed");
+        return SOFTBUS_ENCRYPT_ERR;
+    }
+    *side = sessionKeyList->side;
+    // add seq first
+    if (memcpy_s(outBuf->buf, sizeof(int32_t), &sessionKeyList->seq, sizeof(int32_t)) != EOK) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
+        return SOFTBUS_ENCRYPT_ERR;
+    }
+    AesGcmCipherKey cipherKey = {0};
+    cipherKey.keyLen = SESSION_KEY_LENGTH;
+    if (memcpy_s(cipherKey.key, SESSION_KEY_LENGTH, sessionKeyList->sessionKey, sessionKeyList->sessionKeyLen) != EOK) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
+        return SOFTBUS_ENCRYPT_ERR;
+    }
+    if (SoftBusEncryptDataWithSeq(&cipherKey, data, len, outBuf->buf + MESSAGE_INDEX_LEN,
+        &outLen, sessionKeyList->seq) != SOFTBUS_OK) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "SoftBusEncryptDataWithSeq failed");
+        return SOFTBUS_ENCRYPT_ERR;
+    }
+    outBuf->outLen = outLen + MESSAGE_INDEX_LEN;
+    return SOFTBUS_OK;
 }
 
 int32_t AuthEncrypt(const ConnectOption *option, AuthSideFlag *side, uint8_t *data, uint32_t len, OutBuf *outBuf)
 {
     if (option == NULL || side == NULL || data == NULL ||
         outBuf == NULL || outBuf->bufLen < (len + ENCRYPT_OVER_HEAD_LEN)) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return SOFTBUS_INVALID_PARAM;
     }
     int32_t ret;
@@ -193,29 +243,29 @@ int32_t AuthEncrypt(const ConnectOption *option, AuthSideFlag *side, uint8_t *da
     devInfo.type = option->type;
     ret = AuthGetDeviceKey(devInfo.deviceKey, MAX_DEVICE_KEY_LEN, &(devInfo.deviceKeyLen), option);
     if (ret != SOFTBUS_OK) {
-        LOG_ERR("AuthGetDeviceKey failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "AuthGetDeviceKey failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     sessionKeyList = AuthGetLastSessionKey(&devInfo);
     if (sessionKeyList == NULL) {
-        LOG_ERR("AuthGetLastSessionKey failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "AuthGetLastSessionKey failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     *side = sessionKeyList->side;
     // add seq first
     if (memcpy_s(outBuf->buf, sizeof(int32_t), &sessionKeyList->seq, sizeof(int32_t)) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     AesGcmCipherKey cipherKey = {0};
     cipherKey.keyLen = SESSION_KEY_LENGTH;
     if (memcpy_s(cipherKey.key, SESSION_KEY_LENGTH, sessionKeyList->sessionKey, sessionKeyList->sessionKeyLen) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     if (SoftBusEncryptDataWithSeq(&cipherKey, data, len, outBuf->buf + MESSAGE_INDEX_LEN,
         &outLen, sessionKeyList->seq) != SOFTBUS_OK) {
-        LOG_ERR("SoftBusEncryptDataWithSeq failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "SoftBusEncryptDataWithSeq failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     outBuf->outLen = outLen + MESSAGE_INDEX_LEN;
@@ -225,7 +275,7 @@ int32_t AuthEncrypt(const ConnectOption *option, AuthSideFlag *side, uint8_t *da
 int32_t AuthDecrypt(const ConnectOption *option, AuthSideFlag side, uint8_t *data, uint32_t len, OutBuf *outBuf)
 {
     if (option == NULL || data == NULL || outBuf == NULL || outBuf->bufLen < (len - ENCRYPT_OVER_HEAD_LEN)) {
-        LOG_ERR("invalid parameter");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "invalid parameter");
         return SOFTBUS_INVALID_PARAM;
     }
     SessionKeyList *sessionKeyList = NULL;
@@ -234,12 +284,12 @@ int32_t AuthDecrypt(const ConnectOption *option, AuthSideFlag side, uint8_t *dat
 
     int32_t ret = AuthGetDeviceKey(devInfo.deviceKey, MAX_DEVICE_KEY_LEN, &(devInfo.deviceKeyLen), option);
     if (ret != SOFTBUS_OK) {
-        LOG_ERR("AuthGetDeviceKey failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "AuthGetDeviceKey failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     int32_t seq;
     if (memcpy_s(&seq, sizeof(int32_t), data, sizeof(int32_t)) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     devInfo.seq = seq;
@@ -248,19 +298,19 @@ int32_t AuthDecrypt(const ConnectOption *option, AuthSideFlag side, uint8_t *dat
     devInfo.side = side;
     sessionKeyList = GetSessionKeyByDevinfo(&devInfo);
     if (sessionKeyList == NULL) {
-        LOG_ERR("GetSessionKeyByDevinfo failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "GetSessionKeyByDevinfo failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
 
     AesGcmCipherKey cipherKey = {0};
     cipherKey.keyLen = SESSION_KEY_LENGTH;
     if (memcpy_s(cipherKey.key, SESSION_KEY_LENGTH, sessionKeyList->sessionKey, sessionKeyList->sessionKeyLen) != EOK) {
-        LOG_ERR("memcpy_s failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     if (SoftBusDecryptDataWithSeq(&cipherKey, data, len, outBuf->buf,
         &outBuf->outLen, sessionKeyList->seq) != SOFTBUS_OK) {
-        LOG_ERR("SoftBusDecryptDataWithSeq failed");
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "SoftBusDecryptDataWithSeq failed");
         return SOFTBUS_ENCRYPT_ERR;
     }
     return SOFTBUS_OK;
@@ -271,17 +321,18 @@ uint32_t AuthGetEncryptHeadLen(void)
     return ENCRYPT_OVER_HEAD_LEN;
 }
 
-void AuthClearSessionKeyByDeviceInfo(uint32_t type, const char *deviceKey, uint32_t deviceKeyLen)
+void AuthClearSessionKeyBySeq(int32_t seq)
 {
     SessionKeyList *sessionKeyList = NULL;
     if (IsListEmpty(&g_sessionKeyListHead) == true) {
+        SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "no session key in memory");
         return;
     }
     ListNode *item = NULL;
     ListNode *tmp = NULL;
     LIST_FOR_EACH_SAFE(item, tmp, &g_sessionKeyListHead) {
         sessionKeyList = LIST_ENTRY(item, SessionKeyList, node);
-        if (sessionKeyList->type == type && strncmp(sessionKeyList->deviceKey, deviceKey, deviceKeyLen) == 0) {
+        if (sessionKeyList->seq == seq) {
             (void)memset_s(sessionKeyList->sessionKey, SESSION_KEY_LENGTH, 0, SESSION_KEY_LENGTH);
             ListDelete(&sessionKeyList->node);
             SoftBusFree(sessionKeyList);
