9448707ec78365265351915ae6eea7f3869569ca
YOUR_NAME <zhuyan40@huawei.com>
2021-08-24 07:21:55 +0000

    Description:softbus bus center adapter log modify Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <zhuyan40@huawei.com>

--- adapter/common/bus_center/platform/bus_center_adapter_weak.c
+++ adapter/common/bus_center/platform/bus_center_adapter_weak.c
@@ -28,7 +28,7 @@
 int __attribute__ ((weak)) GetCommonDevInfo(const CommonDeviceKey key, char *value, uint32_t len)
 {
     if (value == NULL) {
-        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "fail: para error!");
+        HILOG_ERROR(LOG_CORE, "fail: para error!");
         return SOFTBUS_INVALID_PARAM;
     }
     switch (key) {
