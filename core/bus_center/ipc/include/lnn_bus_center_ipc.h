6722c7825d2468c4efc41a323027e0b4c3a5ba39
heyingjiao <heyingjiao@huawei.com>
2021-08-25 02:55:26 +0000

    update core/bus_center/ipc/include/lnn_bus_center_ipc.h. Signed-off-by:heyingjiao<heyingjiao@huawei.com>

--- core/bus_center/ipc/include/lnn_bus_center_ipc.h
+++ core/bus_center/ipc/include/lnn_bus_center_ipc.h
@@ -28,7 +28,7 @@ int32_t LnnIpcServerLeave(const char *pkgName, const char *networkId);
 int32_t LnnIpcGetAllOnlineNodeInfo(const char *pkgName, void **info, uint32_t infoTypeLen, int *infoNum);
 int32_t LnnIpcGetLocalDeviceInfo(const char *pkgName, void *info, uint32_t infoTypeLen);
 int32_t LnnIpcGetNodeKeyInfo(const char *pkgName, const char *networkId, int key, unsigned char *buf, uint32_t len);
-int32_t LnnIpcStartTimeSync(const char *pkgName, const char *targetNetworkId, int accuracy, int period);
+int32_t LnnIpcStartTimeSync(const char *pkgName, const char *targetNetworkId, int32_t accuracy, int32_t period);
 int32_t LnnIpcStopTimeSync(const char *pkgName, const char *targetNetworkId);
 
 int32_t LnnIpcNotifyJoinResult(void *addr, uint32_t addrTypeLen, const char *networkId, int32_t retCode);
