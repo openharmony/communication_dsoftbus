0826ba0ad7e78180ea1cde49b8f72bcad0d817b9
jiangkuaixue <jiangchenzhou@huawei.com>
2021-09-06 16:46:08 +0800

    modicy
    Signed-off-by: jiangkuaixue <jiangchenzhou@huawei.com>

--- core/frame/small/init/src/softbus_server_stub.c
+++ core/frame/small/init/src/softbus_server_stub.c
@@ -169,8 +169,8 @@ ServerInvokeCmd g_serverInvokeCmdTbl[] = {
     { SERVER_CREATE_SESSION_SERVER, ServerCreateSessionServer },
     { SERVER_REMOVE_SESSION_SERVER, ServerRemoveSessionServer },
     { SERVER_OPEN_SESSION, ServerOpenSession },
-    { SERVER_OPEN_AUTH_SESSION, ServerOpenAuthSession},
-    { SERVER_NOTIFY_AUTH_SUCCESS, ServerNotifyAuthSuccess},
+    { SERVER_OPEN_AUTH_SESSION, ServerOpenAuthSession },
+    { SERVER_NOTIFY_AUTH_SUCCESS, ServerNotifyAuthSuccess },
     { SERVER_CLOSE_CHANNEL, ServerCloseChannel },
     { SERVER_SESSION_SENDMSG, ServerSendSessionMsg },
 };
