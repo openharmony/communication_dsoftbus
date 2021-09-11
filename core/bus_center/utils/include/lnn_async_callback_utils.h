a53735eabe0aff3676c1031318592fbaa0add0a7
heyingjiao <heyingjiao@huawei.com>
2021-09-11 07:52:27 +0000

    update core/bus_center/utils/include/lnn_async_callback_utils.h. Signed-off-by:heyingjiao<heyingjiao@huawei.com>

--- core/bus_center/utils/include/lnn_async_callback_utils.h
+++ core/bus_center/utils/include/lnn_async_callback_utils.h
@@ -24,11 +24,11 @@
 extern "C" {
 #endif
 
-typedef void (*LnnAsyncCallbackFunc)(void *para);
+typedef void (*LnnAsyncCallbackFunc)(const void *para);
 
-int32_t LnnAsyncCallbackHelper(SoftBusLooper *looper, LnnAsyncCallbackFunc callback, void *para);
+int32_t LnnAsyncCallbackHelper(SoftBusLooper *looper, LnnAsyncCallbackFunc callback, const void *para);
 int32_t LnnAsyncCallbackDelayHelper(SoftBusLooper *looper, LnnAsyncCallbackFunc callback,
-    void *para, uint64_t delayMillis);
+    const void *para, uint64_t delayMillis);
 
 #ifdef __cplusplus
 }
