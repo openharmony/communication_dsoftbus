4b46c916ec581d1d965d7cfa4f051966e669332f
zhufenghao <2581725389@qq.com>
2021-09-11 18:39:27 +0800

    l0
    Signed-off-by: zhufenghao <2581725389@qq.com>

--- core/common/softbus_property/src/softbus_feature_config.c
+++ core/common/softbus_property/src/softbus_feature_config.c
@@ -94,12 +94,12 @@ static TransConfigItem g_tranConfig = {0};
 
 ConfigVal g_configItems[SOFTBUS_CONFIG_TYPE_MAX] = {
     {
-        SOFTBUS_INT_MAX_BYTES_LENGTH, 
+        SOFTBUS_INT_MAX_BYTES_LENGTH,
         (unsigned char*)&(g_config.maxByteLen),
         sizeof(g_config.maxByteLen)
     },
     {
-        SOFTBUS_INT_MAX_MESSAGE_LENGTH, 
+        SOFTBUS_INT_MAX_MESSAGE_LENGTH,
         (unsigned char*)&(g_config.maxMsgLen),
         sizeof(g_config.maxMsgLen)
     },
@@ -109,37 +109,37 @@ ConfigVal g_configItems[SOFTBUS_CONFIG_TYPE_MAX] = {
         sizeof(g_config.connBrMaxDataLen)
     },
     {
-        SOFTBUS_INT_CONN_RFCOM_SEND_MAX_LEN, 
+        SOFTBUS_INT_CONN_RFCOM_SEND_MAX_LEN,
         (unsigned char*)&(g_config.connRfcomSendMaxLen),
         sizeof(g_config.connRfcomSendMaxLen)
     },
     {
-        SOFTBUS_INT_CONN_BR_RECEIVE_MAX_LEN, 
+        SOFTBUS_INT_CONN_BR_RECEIVE_MAX_LEN,
         (unsigned char*)&(g_config.connBrRecvMaxLen),
         sizeof(g_config.connBrRecvMaxLen)
     },
     {
-        SOFTBUS_INT_CONN_TCP_MAX_LENGTH, 
+        SOFTBUS_INT_CONN_TCP_MAX_LENGTH,
         (unsigned char*)&(g_config.connTcpMaxLen),
         sizeof(g_config.connTcpMaxLen)
     },
     {
-        SOFTBUS_INT_CONN_TCP_MAX_CONN_NUM, 
+        SOFTBUS_INT_CONN_TCP_MAX_CONN_NUM,
         (unsigned char*)&(g_config.connTcpMaxConnNum),
         sizeof(g_config.connTcpMaxConnNum)
     },
     {
-        SOFTBUS_INT_CONN_TCP_TIME_OUT, 
+        SOFTBUS_INT_CONN_TCP_TIME_OUT,
         (unsigned char*)&(g_config.connTcpTimeOut),
         sizeof(g_config.connTcpTimeOut)
     },
     {
-        SOFTBUS_INT_MAX_NODE_STATE_CB_CNT, 
+        SOFTBUS_INT_MAX_NODE_STATE_CB_CNT,
         (unsigned char*)&(g_config.maxNodeStateCbCnt),
         sizeof(g_config.maxNodeStateCbCnt)
     },
     {
-        SOFTBUS_INT_MAX_LNN_CONNECTION_CNT, 
+        SOFTBUS_INT_MAX_LNN_CONNECTION_CNT,
         (unsigned char*)&(g_config.maxLnnConnCnt),
         sizeof(g_config.maxLnnConnCnt)
     },
@@ -149,17 +149,17 @@ ConfigVal g_configItems[SOFTBUS_CONFIG_TYPE_MAX] = {
         sizeof(g_config.maxLnnSupportCap)
     },
     {
-        SOFTBUS_INT_AUTH_ABILITY_COLLECTION, 
+        SOFTBUS_INT_AUTH_ABILITY_COLLECTION,
         (unsigned char*)&(g_config.authAbilityConn),
         sizeof(g_config.authAbilityConn)
     },
     {
-        SOFTBUS_INT_ADAPTER_LOG_LEVEL, 
+        SOFTBUS_INT_ADAPTER_LOG_LEVEL,
         (unsigned char*)&(g_config.adapterLogLevel),
         sizeof(g_config.adapterLogLevel)
     },
     {
-        SOFTBUS_STR_STORAGE_DIRECTORY, 
+        SOFTBUS_STR_STORAGE_DIRECTORY,
         (unsigned char*)(g_config.storageDir),
         sizeof(g_config.storageDir)
     },
