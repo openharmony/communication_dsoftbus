7c216392ff307513312d22bab36c5384cb7e6ef9
defeng2020 <wudefeng@huawei.com>
2021-09-23 16:55:40 +0800

    proxy inner
    Signed-off-by: defeng2020 <wudefeng@huawei.com>

--- core/transmission/trans_channel/proxy/src/softbus_proxychannel_manager.c
+++ core/transmission/trans_channel/proxy/src/softbus_proxychannel_manager.c
@@ -594,7 +594,7 @@ void TransProxyProcessHandshakeAckMsg(const ProxyMessage *msg)
 static int TransProxyGetLocalInfo(ProxyChannelInfo *chan)
 {
     if (chan->appInfo.appType != APP_TYPE_INNER) {
-        ret = TransProxyGetPkgName(chan->appInfo.myData.sessionName,
+        int32_t ret = TransProxyGetPkgName(chan->appInfo.myData.sessionName,
         chan->appInfo.myData.pkgName, sizeof(chan->appInfo.myData.pkgName));
         if (ret != SOFTBUS_OK) {
             SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "proc handshake get pkg name fail");
@@ -612,7 +612,6 @@ static int TransProxyGetLocalInfo(ProxyChannelInfo *chan)
 
 void TransProxyProcessHandshakeMsg(const ProxyMessage *msg)
 {
-    int32_t ret;
     SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_INFO,
         "recv Handshake myid %d peerid %d", msg->msgHead.myId, msg->msgHead.peerId);
     ProxyChannelInfo *chan = (ProxyChannelInfo *)SoftBusCalloc(sizeof(ProxyChannelInfo));
@@ -632,7 +631,7 @@ void TransProxyProcessHandshakeMsg(const ProxyMessage *msg)
     }
 
     int16_t newChanId = TransProxyGetNewMyId();
-    ret = OnProxyChannelOpened(newChanId, &(chan->appInfo), 1);
+    int32_t ret = OnProxyChannelOpened(newChanId, &(chan->appInfo), 1);
     if (ret != SOFTBUS_OK) {
         SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "OnProxyChannelOpened  fail");
         SoftBusFree(chan);
