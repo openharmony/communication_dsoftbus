9217cc16b903a7fdfa008a34757a1dbf53fe47df
liubb_0516 <liubeibei8@huawei.com>
2021-09-29 18:03:37 +0800

    modify compile warning
    Signed-off-by: liubb_0516 <liubeibei8@huawei.com>

--- core/connection/common/src/softbus_base_listener.c
+++ core/connection/common/src/softbus_base_listener.c
@@ -408,8 +408,8 @@ static int32_t SelectThread(void)
     struct timeval tv;
     tv.tv_sec = 0;
     tv.tv_usec = TIMEOUT;
-    unsigned char timeOut = 0;
-    if (SoftbusGetConfig(SOFTBUS_INT_SUPPORT_SECLECT_INTERVAL, &timeOut, sizeof(timeOut)) == SOFTBUS_OK) {
+    int32_t timeOut = 0;
+    if (SoftbusGetConfig(SOFTBUS_INT_SUPPORT_SECLECT_INTERVAL, (unsigned char *)&timeOut, sizeof(timeOut)) == SOFTBUS_OK) {
         tv.tv_usec = (long)timeOut;
     }
     fd_set readSet;
