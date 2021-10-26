f92f5936e78087534074e45cfcd0afefbb228616
zgit2021 <zhaowenqiang14@huawei.com>
2021-10-26 14:38:55 +0800

    fix transmission sdk unittest
    Signed-off-by: zgit2021 <zhaowenqiang14@huawei.com>

--- tests/sdk/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_test.cpp
+++ tests/sdk/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_test.cpp
@@ -13,33 +13,24 @@
  * limitations under the License.
  */
 
-#include <securec.h>
+#include <gtest/gtest.h>
 
-#include "gtest/gtest.h"
 #include "session.h"
+#include "softbus_def.h"
 #include "softbus_errcode.h"
-#include "softbus_json_utils.h"
+#include "softbus_feature_config.h"
 #include "softbus_log.h"
-#include "trans_tcp_direct_listener.h"
-#include "trans_tcp_direct_manager.h"
-#include "trans_tcp_direct_message.h"
-
-#define TEST_ASSERT_TRUE(ret)  \
-    if (ret) {                 \
-        LOG_INFO("[succ]:%d\n", __LINE__);    \
-        printf("[succ]:%d\n", __LINE__);    \
-        g_succTestCount++;       \
-    } else {                   \
-        LOG_INFO("[error]:%d\n", __LINE__);    \
-        printf("[error]:%d\n", __LINE__);    \
-        g_failTestCount++;       \
-    }
 
 using namespace testing::ext;
 
 namespace OHOS {
-static int32_t g_succTestCount = 0;
-static int32_t g_failTestCount = 0;
+const char *g_pkgName = "com.dsoftbus.demo";
+const char *g_sessionName = "com.dsoftbus.demo.JtOnOpenSession";
+const char *g_networkid = "ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00";
+const char *g_groupid = "TEST_GROUP_ID";
+static SessionAttribute g_sessionAttr = {
+    .dataType = TYPE_BYTES,
+};
 
 class TransTcpDirectTest : public testing::Test {
 public:
@@ -82,21 +73,12 @@ static void OnMessageReceived(int sessionId, const void *data, unsigned int len)
     LOG_INFO("session msg received, session id = %d\r\n", sessionId);
 }
 
-static SessionAttribute g_sessionAttr = {
-    .dataType = TYPE_BYTES
-};
-
 static ISessionListener g_sessionlistener = {
     .OnSessionOpened = OnSessionOpened,
     .OnSessionClosed = OnSessionClosed,
     .OnBytesReceived = OnBytesReceived,
-    .OnMessageReceived = OnMessageReceived
+    .OnMessageReceived = OnMessageReceived,
 };
-char const *g_pkgName = "com.huawei.communication.demo";
-char const *g_sessionName = "com.huawei.communication.demo.JtOnOpenSession";
-char const *g_networkid = "ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00ABCDEF00";
-char const *g_groupid = "TEST_GROUP_ID";
-#define TEST_SESSION_KEY "TEST_SESSION_KEY"
 
 /**
  * @tc.name: CreateSessionServerTest001
@@ -106,21 +88,21 @@ char const *g_groupid = "TEST_GROUP_ID";
  */
 HWTEST_F(TransTcpDirectTest, CreateSessionServerTest001, TestSize.Level1)
 {
-    int ret = 0;
+    int ret;
     ret = CreateSessionServer(NULL, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = CreateSessionServer(g_pkgName, NULL, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = CreateSessionServer(g_pkgName, g_sessionName, NULL);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = CreateSessionServer(g_pkgName, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = RemoveSessionServer(g_pkgName, g_sessionName);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 }
 
 /**
@@ -133,10 +115,10 @@ HWTEST_F(TransTcpDirectTest, CreateSessionServerTest002, TestSize.Level1)
 {
     int ret;
     ret = CreateSessionServer(g_pkgName, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = RemoveSessionServer(g_pkgName, g_sessionName);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 }
 
 /**
@@ -147,15 +129,15 @@ HWTEST_F(TransTcpDirectTest, CreateSessionServerTest002, TestSize.Level1)
  */
 HWTEST_F(TransTcpDirectTest, CreateSessionServerTest003, TestSize.Level1)
 {
-    int ret = 0;
+    int ret;
     ret = CreateSessionServer(g_pkgName, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = CreateSessionServer(g_pkgName, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = RemoveSessionServer(g_pkgName, g_sessionName);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 }
 
 /**
@@ -167,42 +149,28 @@ HWTEST_F(TransTcpDirectTest, CreateSessionServerTest003, TestSize.Level1)
 HWTEST_F(TransTcpDirectTest, CreateSessionServerTest004, TestSize.Level1)
 {
     int ret, i;
-    char const *sessionName[] = {
-        "com.huawei.communication.demo.JtOnOpenSession0",
-        "com.huawei.communication.demo.JtOnOpenSession1",
-        "com.huawei.communication.demo.JtOnOpenSession2",
-        "com.huawei.communication.demo.JtOnOpenSession3",
-        "com.huawei.communication.demo.JtOnOpenSession4",
-        "com.huawei.communication.demo.JtOnOpenSession5",
-        "com.huawei.communication.demo.JtOnOpenSession6",
-        "com.huawei.communication.demo.JtOnOpenSession7",
-        "com.huawei.communication.demo.JtOnOpenSession8",
-        "com.huawei.communication.demo.JtOnOpenSession9"
+    char const *sessionName[MAX_SESSION_SERVER_NUMBER + 1] = {
+        "com.dsoftbus.demo.JtOnOpenSession0",
+        "com.dsoftbus.demo.JtOnOpenSession1",
+        "com.dsoftbus.demo.JtOnOpenSession2",
+        "com.dsoftbus.demo.JtOnOpenSession3",
+        "com.dsoftbus.demo.JtOnOpenSession4",
+        "com.dsoftbus.demo.JtOnOpenSession5",
+        "com.dsoftbus.demo.JtOnOpenSession6",
+        "com.dsoftbus.demo.JtOnOpenSession7",
+        "com.dsoftbus.demo.JtOnOpenSession8"
     };
 
-    char const *pkgName[] = {
-        "com.huawei.communication.demo0",
-        "com.huawei.communication.demo1",
-        "com.huawei.communication.demo2",
-        "com.huawei.communication.demo3",
-        "com.huawei.communication.demo4",
-        "com.huawei.communication.demo5",
-        "com.huawei.communication.demo6",
-        "com.huawei.communication.demo7",
-        "com.huawei.communication.demo8",
-        "com.huawei.communication.demo9"
-    };
-
-    for (i = 0; i < 8; i++) {
-        ret = CreateSessionServer(pkgName[i], sessionName[i], &g_sessionlistener);
-        TEST_ASSERT_TRUE(ret == 0);
+    for (i = 0; i < MAX_SESSION_SERVER_NUMBER; i++) {
+        ret = CreateSessionServer(g_pkgName, sessionName[i], &g_sessionlistener);
+        EXPECT_EQ(SOFTBUS_OK, ret);
     }
-    ret = CreateSessionServer(pkgName[i], sessionName[i], &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret != 0);
+    ret = CreateSessionServer(g_pkgName, sessionName[i], &g_sessionlistener);
+    EXPECT_EQ(SOFTBUS_INVALID_NUM, ret);
 
-    for (i = 0; i < 8; i++) {
-        ret = RemoveSessionServer(pkgName[i], sessionName[i]);
-        TEST_ASSERT_TRUE(ret == 0);
+    for (i = 0; i < MAX_SESSION_SERVER_NUMBER; i++) {
+        ret = RemoveSessionServer(g_pkgName, sessionName[i]);
+        EXPECT_EQ(SOFTBUS_OK, ret);
     }
 }
 
@@ -216,10 +184,10 @@ HWTEST_F(TransTcpDirectTest, RemoveSessionServerTest001, TestSize.Level1)
 {
     int ret;
     ret = RemoveSessionServer(NULL, g_sessionName);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = RemoveSessionServer(g_pkgName, NULL);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 }
 
 /**
@@ -232,13 +200,13 @@ HWTEST_F(TransTcpDirectTest, RemoveSessionServerTest002, TestSize.Level1)
 {
     int ret;
     ret = CreateSessionServer(g_pkgName, g_sessionName, &g_sessionlistener);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = RemoveSessionServer(g_pkgName, g_sessionName);
-    TEST_ASSERT_TRUE(ret == 0);
+    EXPECT_EQ(SOFTBUS_OK, ret);
 
     ret = RemoveSessionServer(g_pkgName, g_sessionName);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
 }
 
 /**
@@ -250,50 +218,27 @@ HWTEST_F(TransTcpDirectTest, RemoveSessionServerTest002, TestSize.Level1)
 HWTEST_F(TransTcpDirectTest, OpenSessionTest001, TestSize.Level1)
 {
     int ret;
+    g_sessionAttr.dataType = TYPE_BYTES;
+
     ret = OpenSession(NULL, g_sessionName, g_networkid, g_groupid, &g_sessionAttr);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 
     ret = OpenSession(g_sessionName, NULL, g_networkid, g_groupid, &g_sessionAttr);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 
     ret = OpenSession(g_sessionName, g_sessionName, NULL, g_groupid, &g_sessionAttr);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 
     ret = OpenSession(g_sessionName, g_sessionName, g_networkid, NULL, &g_sessionAttr);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 
     ret = OpenSession(g_sessionName, g_sessionName, g_networkid, g_groupid, NULL);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 
     g_sessionAttr.dataType = TYPE_BUTT;
     ret = OpenSession(g_sessionName, g_sessionName, g_networkid, g_groupid, &g_sessionAttr);
     g_sessionAttr.dataType = TYPE_BYTES;
-    TEST_ASSERT_TRUE(ret != 0);
-}
-
-/**
- * @tc.name: SendMessageTest001
- * @tc.desc: extern module active publish, use the wrong parameter.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(TransTcpDirectTest, SendMessageTest001, TestSize.Level1)
-{
-    int ret;
-    int sessionId = 1;
-    char const *data = "testdata";
-    uint32_t len = strlen(data);
-
-    ret = SendMessage(-1, data, len);
-    TEST_ASSERT_TRUE(ret != 0);
-
-    ret = SendMessage(sessionId, NULL, len);
-    TEST_ASSERT_TRUE(ret != 0);
-
-    ret = SendMessage(sessionId, data, -1);
-    TEST_ASSERT_TRUE(ret != 0);
-
-    ret = SendMessage(sessionId, data, 0);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(INVALID_SESSION_ID, ret);
 }
 
 /**
@@ -306,19 +251,57 @@ HWTEST_F(TransTcpDirectTest, SendBytesTest001, TestSize.Level1)
 {
     int ret;
     int sessionId = 1;
-    char const *data = "testdata";
+    const char *data = "testdata";
     uint32_t len = strlen(data);
+    uint32_t maxLen;
 
     ret = SendBytes(-1, data, len);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_TRANS_INVALID_SESSION_ID, ret);
 
     ret = SendBytes(sessionId, NULL, len);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = SendBytes(sessionId, data, -1);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 
     ret = SendBytes(sessionId, data, 0);
-    TEST_ASSERT_TRUE(ret != 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SoftbusGetConfig(SOFTBUS_INT_MAX_BYTES_LENGTH, (unsigned char *)&maxLen, sizeof(maxLen));
+    ASSERT_EQ(SOFTBUS_OK, ret);
+    ret = SendMessage(sessionId, data, maxLen + 1);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+}
+
+/**
+ * @tc.name: SendMessageTest001
+ * @tc.desc: extern module active publish, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransTcpDirectTest, SendMessageTest001, TestSize.Level1)
+{
+    int ret;
+    int sessionId = 1;
+    const char *data = "testdata";
+    uint32_t len = strlen(data);
+    uint32_t maxLen;
+
+    ret = SendMessage(-1, data, len);
+    EXPECT_EQ(SOFTBUS_TRANS_INVALID_SESSION_ID, ret);
+
+    ret = SendMessage(sessionId, NULL, len);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SendMessage(sessionId, data, -1);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SendMessage(sessionId, data, 0);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SoftbusGetConfig(SOFTBUS_INT_MAX_MESSAGE_LENGTH, (unsigned char *)&maxLen, sizeof(maxLen));
+    ASSERT_EQ(SOFTBUS_OK, ret);
+    ret = SendMessage(sessionId, data, maxLen + 1);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
 }
 }
---
+++ tests/sdk/transmission/trans_channel/udp/stream/trans_sdk_stream_test.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "session.h"
+#include "softbus_errcode.h"
+
+using namespace testing::ext;
+
+namespace OHOS {
+class TransSdkStreamTest : public testing::Test {
+public:
+    TransSdkStreamTest()
+    {}
+    ~TransSdkStreamTest()
+    {}
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp() override
+    {}
+    void TearDown() override
+    {}
+};
+
+void TransSdkStreamTest::SetUpTestCase(void)
+{}
+
+void TransSdkStreamTest::TearDownTestCase(void)
+{}
+
+/**
+ * @tc.name: SendStreamTest001
+ * @tc.desc: extern module active publish, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransSdkStreamTest, SendStreamTest001, TestSize.Level1)
+{
+    int ret;
+    int sessionId = 1;
+    char *data = const_cast<char *>("testdata");
+    const StreamData streamData = {data, strlen(data)};
+    const StreamData ext = {data, strlen(data)};
+    const FrameInfo param = {0};
+
+    ret = SendStream(-1, &streamData, &ext, &param);
+    EXPECT_EQ(SOFTBUS_TRANS_INVALID_SESSION_ID, ret);
+
+    ret = SendStream(sessionId, NULL, &ext, &param);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SendStream(sessionId, &streamData, NULL, &param);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+
+    ret = SendStream(sessionId, &streamData, &ext, NULL);
+    EXPECT_EQ(SOFTBUS_INVALID_PARAM, ret);
+}
+}
