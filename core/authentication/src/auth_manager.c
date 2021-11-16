5978cc22cef83d38550323c1a757fd70d39833ec
YOUR_NAME <xiaoqiang21@huawei.com>
2021-11-16 08:17:49 +0000

    deal with tran get the uuid from auth is invalid problem
    Signed-off-by: YOUR_NAME <xiaoqiang21@huawei.com>

--- core/authentication/src/auth_manager.c
+++ core/authentication/src/auth_manager.c
@@ -1278,6 +1278,10 @@ int32_t AuthGetUuidByOption(const ConnectOption *option, char *buf, uint32_t buf
         if ((option->type == CONNECT_TCP && strncmp(auth->option.info.ipOption.ip, option->info.ipOption.ip,
             strlen(auth->option.info.ipOption.ip)) == 0) || (option->type == CONNECT_BR &&
             strncmp(auth->option.info.brOption.brMac, option->info.brOption.brMac, BT_MAC_LEN) == 0)) {
+            if (strlen(auth->peerUuid) == 0) {
+                SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "client list no peerUuid");
+                break;
+            }
             if (strncpy_s(buf, bufLen, auth->peerUuid, strlen(auth->peerUuid)) != EOK) {
                 (void)pthread_mutex_unlock(&g_authLock);
                 SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
@@ -1292,6 +1296,10 @@ int32_t AuthGetUuidByOption(const ConnectOption *option, char *buf, uint32_t buf
         if ((option->type == CONNECT_TCP && strncmp(auth->option.info.ipOption.ip, option->info.ipOption.ip,
             strlen(auth->option.info.ipOption.ip)) == 0) || (option->type == CONNECT_BR &&
             strncmp(auth->option.info.brOption.brMac, option->info.brOption.brMac, BT_MAC_LEN) == 0)) {
+            if (strlen(auth->peerUuid) == 0) {
+                SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "server list no peerUuid");
+                break;
+            }
             if (strncpy_s(buf, bufLen, auth->peerUuid, strlen(auth->peerUuid)) != EOK) {
                 (void)pthread_mutex_unlock(&g_authLock);
                 SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "memcpy_s failed");
