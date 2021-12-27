2bb3674ac0a1b2cac4c045518581149ad8742541
handyohos <zhangxiaotian@huawei.com>
2021-12-27 19:39:20 +0800

    bugfix: Remove uncessary bms dependency in dsoftbus.
    去掉permission中无效的bms依赖。
    
    Signed-off-by: handyohos <zhangxiaotian@huawei.com>
    Change-Id: I27d92f6c7c650feb3125d8e9fa3068319e6c1fc8
    
    #I4NWKB

--- core/common/security/permission/standard_system/permission_utils.cpp
+++ core/common/security/permission/standard_system/permission_utils.cpp
@@ -15,33 +15,15 @@
 
 #include "permission_utils.h"
 
-#include "bundle_mgr_interface.h"
 #include "softbus_errcode.h"
 #include "softbus_log.h"
 #include "sys_mgr_client.h"
-#include "system_ability_definition.h"
 
-using namespace OHOS::AppExecFwk;
 
 extern "C" int32_t IsValidPkgName(int32_t uid, const char *pkgName)
 {
     if (pkgName == NULL) {
         return SOFTBUS_ERR;
     }
-    auto bundleObj =
-    OHOS::DelayedSingleton<SysMrgClient>::GetInstance()->GetSystemAbility(OHOS::BUNDLE_MGR_SERVICE_SYS_ABILITY_ID);
-    if (bundleObj == nullptr) {
-        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "failed to get bundle manager service");
-        return SOFTBUS_ERR;
-    }
-    OHOS::sptr<IBundleMgr> bmgr = OHOS::iface_cast<IBundleMgr>(bundleObj);
-    std::string bundleName;
-#ifndef PERMISSION_TEST
-    bmgr->GetBundleNameForUid(uid, bundleName);
-    printf("BundleName: %s\n", bundleName.c_str());
-    if (std::string(pkgName) != bundleName) {
-        return SOFTBUS_ERR;
-    }
-#endif
     return SOFTBUS_OK;
 }
