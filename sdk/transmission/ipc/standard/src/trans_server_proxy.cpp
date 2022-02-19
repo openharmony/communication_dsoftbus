5a8d320fc2ba0be2b819b59853dc9bc36456acf1
liangliang_ma <maliangliang1@huawei.com>
2022-02-19 10:30:35 +0000

    Description: modify RegNodeDeviceStateCb fail issue. Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: liangliang_ma <maliangliang1@huawei.com>

--- sdk/transmission/ipc/standard/src/trans_server_proxy.cpp
+++ sdk/transmission/ipc/standard/src/trans_server_proxy.cpp
@@ -52,9 +52,13 @@ int32_t TransServerProxyInit(void)
 {
     std::lock_guard<std::mutex> lock(g_mutex);
     sptr<IRemoteObject> object = GetSystemAbility();
+    if (object == nullptr) {
+        SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "Get remote softbus object failed!\n");
+        return SOFTBUS_ERR;
+    }
     g_serverProxy = new (std::nothrow) TransServerProxy(object);
     if (g_serverProxy == nullptr) {
-        SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "Get remote softbus object failed!\n");
+        SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "Create trans server proxy failed!\n");
         return SOFTBUS_ERR;
     }
     return SOFTBUS_OK;
