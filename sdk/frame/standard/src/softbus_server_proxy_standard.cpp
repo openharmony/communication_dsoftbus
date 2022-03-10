f61b72950b46dc162f4e469cc17224783503b69f
虞仕浩 <yushihao4@huawei.com>
2022-03-10 01:35:37 +0000

    Signed-off-by: yushihao <yushihao4@huawei.com>

--- sdk/frame/standard/src/softbus_server_proxy_standard.cpp
+++ sdk/frame/standard/src/softbus_server_proxy_standard.cpp
@@ -70,8 +70,8 @@ int32_t SoftBusServerProxyFrame::SoftbusRegisterService(const char *clientPkgNam
         return SOFTBUS_ERR;
     }
     MessageParcel data;
-    if(!data.WriteInterfaceToken(GetDescriptor())) {
-		SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "SoftbusRegisterService write InterfaceToken failed!");
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "SoftbusRegisterService write InterfaceToken failed!");
         return SOFTBUS_ERR;
     }
     int ret = data.WriteRemoteObject(clientStub);
