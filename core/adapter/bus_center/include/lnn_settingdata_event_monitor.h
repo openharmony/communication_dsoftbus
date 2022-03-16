00016dbeda2d3c0ffbfc5daea8a3567a86afd8df
sun-zhongguo <sunzhongguo@huawei.com>
2022-03-16 06:58:37 +0000

    sync get device feature from release branch
    Signed-off-by: sun-zhongguo <sunzhongguo@huawei.com>

---
+++ core/adapter/bus_center/include/lnn_settingdata_event_monitor.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LNN_SETTINGDATA_EVENT_MONITOR_H
+#define LNN_SETTINGDATA_EVENT_MONITOR_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*LnnDeviceNameHandler)(void);
+int32_t LnnGetSettingDeviceName(char *deviceName, uint32_t len);
+int32_t LnnInitGetDeviceName(LnnDeviceNameHandler handler);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* LNN_SETTINGDATA_EVENT_MONITOR_H */
