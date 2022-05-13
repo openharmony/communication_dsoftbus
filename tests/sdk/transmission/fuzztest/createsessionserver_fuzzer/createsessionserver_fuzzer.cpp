bafa5524a12e6398050b2c9389fd4f2071927eaf
孙旭辉 <sunxuhui4@huawei.com>
2022-05-13 16:26:18 +0800

    非功能FUZZ
    Signed-off-by: 孙旭辉 <sunxuhui4@huawei.com>

---
+++ tests/sdk/transmission/fuzztest/createsessionserver_fuzzer/createsessionserver_fuzzer.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "createsessionserver_fuzzer.h"
+#include <cstddef>
+#include <cstdint>
+#include "inner_session.h"
+#include "session.h"
+#include "softbus_utils.h"
+
+namespace OHOS {
+const char *g_sessionName = "objectstore";
+
+static int OnSessionOpened(int sessionId, int result)
+{
+    return 0;
+}
+
+static void OnSessionClosed(int sessionId) {}
+
+static void OnBytesReceived(int sessionId, const void *data, unsigned int len) {}
+
+static void OnMessageReceived(int sessionId, const void *data, unsigned int len) {}
+
+static ISessionListener g_sessionlistener = {
+    .OnSessionOpened = OnSessionOpened,
+    .OnSessionClosed = OnSessionClosed,
+    .OnBytesReceived = OnBytesReceived,
+    .OnMessageReceived = OnMessageReceived,
+};
+
+void CreateSessionServerTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size <= 0)) {
+        return;
+    }
+
+    CreateSessionServer((const char*)data, g_sessionName, &g_sessionlistener);
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    OHOS::CreateSessionServerTest(data, size);
+
+    return 0;
+}
