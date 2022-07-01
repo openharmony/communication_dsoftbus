3b0d5b65b88f63ff2edb284f64b52187f7eeb06f
whm <weihongming2@huawei.com>
2022-07-01 07:53:18 +0000

    fix: connect failed handle
    Signed-off-by: whm <weihongming2@huawei.com>

--- core/authentication/src/auth_manager.c
+++ core/authentication/src/auth_manager.c
@@ -567,9 +567,10 @@ int64_t AuthVerifyDevice(AuthVerifyModule moduleId, const ConnectionAddr *addr)
             return SOFTBUS_ERR;
         }
     } else if (option.type == CONNECT_BR || option.type == CONNECT_BLE) {
+        int64_t authId = auth->authId;
         if (ConnConnectDevice(&option, auth->requestId, &g_connResult) != SOFTBUS_OK) {
             SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "auth ConnConnectDevice failed");
-            DeleteAuth(auth);
+            (void)AuthHandleLeaveLNN(authId);
             return SOFTBUS_ERR;
         }
     } else {
@@ -1680,9 +1681,10 @@ static int32_t TryCreateConnection(const ConnectOption *option, uint32_t request
         .OnConnectSuccessed = OnConnectSuccessed,
         .OnConnectFailed = OnConnectFailed,
     };
+    int64_t authId = conn->authId;
     if (ConnConnectDevice(option, conn->requestId, &result) != SOFTBUS_OK) {
         SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "conn connect device failed.");
-        DeleteAuth(conn);
+        (void)AuthHandleLeaveLNN(authId);
         return SOFTBUS_ERR;
     }
     return SOFTBUS_OK;
@@ -1766,10 +1768,10 @@ static int32_t AuthOpenCommonConn(const AuthConnInfo *info, uint32_t requestId, 
         return SOFTBUS_ERR;
     }
     auth->connCb = *callback;
-
+    int64_t authId = auth->authId;
     if (ConnConnectDevice(&option, auth->requestId, &g_connResult) != SOFTBUS_OK) {
         SoftBusLog(SOFTBUS_LOG_AUTH, SOFTBUS_LOG_ERROR, "conn connect device failed.");
-        DeleteAuth(auth);
+        (void)AuthHandleLeaveLNN(authId);
         return SOFTBUS_ERR;
     }
 
