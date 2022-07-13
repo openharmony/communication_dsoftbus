293b0dc12e34baefa0aaaec2d359c4f65cbb7cc8
“lixin” <lixin503@huawei.com>
2022-07-13 14:24:53 +0800

    fix: Function arguments are changed to references.
    Signed-off-by: lixin <lixin503@huawei.com>

--- tests/core/bus_center/lnn/unittest/net_buscenter_test.cpp
+++ tests/core/bus_center/lnn/unittest/net_buscenter_test.cpp
@@ -103,7 +103,7 @@ void NetBusCenterTest::TearDown()
 {
 }
 
-static int SetIpaddr(const std::string ip)
+static int SetIpaddr(const std::string &ip)
 {
     int sockFd  = -1;
     struct sockaddr_in addr;
