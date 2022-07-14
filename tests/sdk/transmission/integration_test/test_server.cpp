33733ba8da039ae68626a64c0bb654ed5ded5d0a
duxbbo <duxiaobo@huawei.com>
2022-07-14 11:52:03 +0000

    fix code check
    Signed-off-by: duxbbo <duxiaobo@huawei.com>
    Change-Id: Iebe54b1b420f3fa162a9d906a14ffb9868406cfa

--- tests/sdk/transmission/integration_test/test_server.cpp
+++ tests/sdk/transmission/integration_test/test_server.cpp
@@ -1,15 +1,29 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "test_suite.h"
 
 #include <getopt.h>
-#include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
+#include <cstdint>
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/times.h>
-#include <time.h>
+#include <ctime>
 #include <unistd.h>
-#include <inttypes.h>
+#include <cinttypes>
 
 #include "transport/session.h"
 #include "softbus_error_code.h"
@@ -27,37 +41,9 @@ double g_maxRtt = 0;
 
 volatile bool g_sessionEnabled = false;
 volatile uint32_t g_recvSeq = 0;
-volatile clock_t g_responseTime = 0;
 int g_sessionId = -1;
 
-void CalcRTT(uint32_t seq, clock_t sendTime, clock_t rspTime)
-{
-    double rtt = (double)(rspTime - sendTime);
-    LOG("%s:[RTT for %u] %lf", __func__, seq, rtt);
-    if(rtt < 0) {
-        LOG("%s:bad rtt %lf", __func__, rtt);
-        return;
-    }
-    if (seq == 0 || seq == 1) {
-        g_aveRtt = rtt;
-        g_maxRtt = rtt;
-        return;
-    }
-
-    g_aveRtt = g_aveRtt * (((double)(seq - 1)) / seq) + (rtt / seq);
-    if (rtt > g_maxRtt) {
-        g_maxRtt = rtt;
-    }
-    LOG("%s:[RTT]ave=%lf,max=%lf", __func__, g_aveRtt, g_maxRtt);
-}
-
-void ShowTestResult(void)
-{
-    LOG("average RTT:%lf", g_aveRtt / 2);
-    LOG("max RTT:%lf", g_maxRtt / 2);
-}
-
-int EsOnSessionOpened(int sessionId, int result)
+static int EsOnSessionOpened(int sessionId, int result)
 {
     LOG("%s:enter", __func__);
     if(result != SOFTBUS_OK) {
@@ -71,7 +57,7 @@ int EsOnSessionOpened(int sessionId, int result)
     LOG("%s:Unexpected session %d opened!", __func__, sessionId);
     return 0;
 }
-void EsOnSessionClosed(int sessionId)
+static void EsOnSessionClosed(int sessionId)
 {
     LOG("%s:enter", __func__);
     if (sessionId == g_sessionId) {
@@ -80,16 +66,16 @@ void EsOnSessionClosed(int sessionId)
     }
 }
 
-void EsOnDataReceived(int sessionId, const void *data, unsigned int dataLen)
+static void EsOnDataReceived(int sessionId, const void *data, unsigned int dataLen)
 {
     LOG("%s:enter", __func__);
 }
 
-void EsOnStreamReceived(int sessionId, const StreamData *data, const StreamData *ext, const StreamFrameInfo *param)
+static void EsOnStreamReceived(int sessionId, const StreamData *data, const StreamData *ext, const StreamFrameInfo *param)
 {
     LOG("%s:enter", __func__);
 }
-void EsOnQosEvent(int sessionId, int eventId, int tvCount, const QosTv *tvList)
+static void EsOnQosEvent(int sessionId, int eventId, int tvCount, const QosTv *tvList)
 {
     LOG("%s:enter", __func__);
 }
@@ -110,7 +96,7 @@ static void TsOnFileTransError(int sessionId) {
     LOG("%s:session=%d", __func__, sessionId);
 }
 
-static int ExecTestSuite(const TestConfig *config)
+static int ExecTestSuite(void)
 {
     static ISessionListener listener = {
         .OnSessionOpened = EsOnSessionOpened,
@@ -151,53 +137,11 @@ static int ExecTestSuite(const TestConfig *config)
 
 int main(int argc, char * const *argv)
 {
-    TestConfig config = {.targetUdid = NULL, .dataType = TYPE_MESSAGE, .packageSize = 1000, .sampleSize = 1000};
+    LOG("%s:started", __func__);
 
-    const char *optStr = "t:MB:P:S";
-    static struct option options[] = {
-        {"target",      required_argument, NULL, 't'},
-        {"message",     no_argument,       NULL, 'M'},
-        {"bytes",       no_argument,       NULL, 'B'},
-        {"packageSize", required_argument, NULL, 'P'},
-        {"sampleSize",  required_argument, NULL, 'S'},
-        {0,             0,                 0,    0  },
-    };
-    int opt = 0;
-    while ((opt = getopt_long(argc, argv, optStr, options, NULL)) != -1) {
-        switch (opt) {
-            case 't': {
-                config.targetUdid = optarg;
-                break;
-            }
-            case 'M': {
-                config.dataType = TYPE_MESSAGE;
-                break;
-            }
-            case 'B': {
-                config.dataType = TYPE_BYTES;
-                break;
-            }
-            case 'P': {
-                config.packageSize = atoi(optarg);
-                break;
-            }
-            case 'S': {
-                config.sampleSize = atoi(optarg);
-                break;
-            }
-            default: {
-                break;
-            }
-        };
-    }
-
-    LOG("%s:send test", __func__);
-
-    int ret = ExecTestSuite(&config);
+    int ret = ExecTestSuite();
     if (ret != 0) {
         LOG("%s:test failed!ret=%d", __func__, ret);
-    } else {
-        ShowTestResult();
     }
     return ret;
 }
