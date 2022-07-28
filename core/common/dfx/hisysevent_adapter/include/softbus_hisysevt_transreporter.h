de760daf73dc19cc961d362ba4964786a56ed64b
breakfei1 <zhangshen6@huawei.com>
2022-07-28 14:18:21 +0800

    增加传输模块的HiSysEvt打点
    Signed-off-by: breakfei1 <zhangshen6@huawei.com>

---
+++ core/common/dfx/hisysevent_adapter/include/softbus_hisysevt_transreporter.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * miscservices under the License is miscservices on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef HISYSEVENT_TRANS_REPORTER_H
+#define HISYSEVENT_TRANS_REPORTER_H
+
+
+
+#endif /* HISYSEVENT_TRANS_REPORTER_H */
