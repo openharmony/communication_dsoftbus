48459dd84de43820189a6db84e93949beef4c26b
w00644978 <wudy062379@126.com>
2022-08-01 12:48:51 +0800

    add hisysevent in disc & conn
    Signed-off-by: w00644978 <wudy062379@126.com>

---
+++ core/common/dfx/hisysevent_adapter/softbus_hisysevt_connreporter.c
@@ -0,0 +1,307 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * miscservices under the License is miscservices on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "softbus_hisysevt_connreporter.h"
+
+#include "securec.h"
+#include "softbus_errcode.h"
+#include "softbus_log.h"
+#include "softbus_adapter_thread.h"
+#include "softbus_adapter_mem.h"
+#include "softbus_hisysevt_common.h"
+
+#define STATISTIC_EVT_CONN_DURATION "CONN_DURATION"
+#define STATISTIC_EVT_CONN_SUCC_RATE "CONN_SUCC_RATE"
+#define FAULT_EVT_CONN_FAULT "CONN_FAULT"
+
+#define CONN_PARAM_MEDIUM "MEDIUM"
+#define CONN_PARAM_MAX_CONN_DURATION "MAX_CONN_DURATION"
+#define CONN_PARAM_MIN_CONN_DURATION "MIN_CONN_DURATION"
+#define CONN_PARAM_AVG_CONN_DURATION "AVG_CONN_DURATION"
+#define CONN_PARAM_CONN_SUCC_RATE "SUCC_RATE"
+#define CONN_PARAM_CONN_SUCC_TIMES "SUCC_TIMES"
+#define CONN_PARAM_CONN_FAIL_TIMES "FAIL_TIMES"
+#define CONN_PARAM_ERROR_CODE "ERROR_CODE"
+#define CONN_PARAM_ERROR_COUNTER "ERROR_COUNTER"
+
+typedef struct {
+    SoftBusMutex lock;
+    uint32_t maxConnDur;
+    uint32_t minConnDur;
+    uint32_t avgConnDur;
+    uint64_t totalConnTime;
+} ConnTimeDur;
+
+typedef struct {
+    SoftBusMutex lock;
+    uint32_t succTime;
+    uint32_t failTime;
+    uint32_t totalCnt;
+    float succRate;
+} ConnSuccRate;
+
+static ConnTimeDur g_connTimeDur[SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT];
+static ConnSuccRate g_connSuccRate[SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT];
+
+static inline int32_t InitConnEvtMutexLock(void)
+{
+    SoftBusMutexAttr mutexAttr = {SOFTBUS_MUTEX_RECURSIVE};
+    for (int i = 0; i < SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT; i++) {
+        if (SoftBusMutexInit(&g_connTimeDur[i].lock, &mutexAttr) != SOFTBUS_OK) {
+            return SOFTBUS_ERR;
+        }
+
+        if (SoftBusMutexInit(&g_connSuccRate[i].lock, &mutexAttr) != SOFTBUS_OK) {
+            return SOFTBUS_ERR;
+        }
+    }
+    return SOFTBUS_OK;
+}
+
+static inline void ClearConnTimeDur(void)
+{
+    for (int32_t i = 0; i < SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT; i++) {
+        memset_s(&g_connTimeDur[i].maxConnDur, sizeof(ConnTimeDur) - sizeof(SoftBusMutex),
+            0, sizeof(ConnTimeDur) - sizeof(SoftBusMutex));
+    }
+}
+
+static int32_t SoftBusCreateConnDurMsg(SoftBusEvtReportMsg *msg, uint8_t medium)
+{
+    if (SoftBusMutexLock(&g_connTimeDur[medium].lock) != SOFTBUS_OK) {
+        return SOFTBUS_ERR;
+    }
+
+    if (strcpy_s(msg->evtName, SOFTBUS_HISYSEVT_NAME_LEN, STATISTIC_EVT_CONN_DURATION) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    msg->evtType = SOFTBUS_EVT_TYPE_STATISTIC;
+    msg->paramNum = SOFTBUS_EVT_PARAM_FOUR;
+
+    SoftBusEvtParam *param = &msg->paramArray[SOFTBUS_EVT_PARAM_ZERO];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT8;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_MEDIUM) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u8v = medium;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_ONE];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_MAX_CONN_DURATION) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u32v = g_connTimeDur[medium].maxConnDur;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_TWO];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_MIN_CONN_DURATION) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u32v = g_connTimeDur[medium].minConnDur;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_THREE];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_AVG_CONN_DURATION) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u32v = g_connTimeDur[medium].avgConnDur;
+
+    (void)SoftBusMutexUnlock(&g_connTimeDur[medium].lock);
+    return SOFTBUS_OK;
+}
+
+static int32_t SoftBusReportConnTimeDurEvt()
+{
+    SoftBusEvtReportMsg *msg = SoftbusCreateEvtReportMsg(SOFTBUS_EVT_PARAM_FOUR);
+    if (msg == NULL) {
+        return SOFTBUS_ERR;
+    }
+    for (int i = 0; i < SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT; i++) {
+        if (SoftBusCreateConnDurMsg(msg, i) == SOFTBUS_ERR) {
+            return SOFTBUS_ERR;
+        }
+        if (SoftbusWriteHisEvt(msg) ==SOFTBUS_ERR) {
+            return SOFTBUS_ERR;
+        }
+    }
+    SoftbusFreeEvtReporMsg(msg);
+    ClearConnTimeDur();
+    return SOFTBUS_OK;
+}
+
+static inline void ClearConnSuccRate(void)
+{
+    for (int i = 0; i < SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT; i++) {
+        memset_s(&g_connSuccRate[i].succTime, sizeof(ConnSuccRate) - sizeof(SoftBusMutex),
+            0, sizeof(ConnSuccRate) - sizeof(SoftBusMutex));
+    }
+}
+
+static int32_t SoftBusCreateConnSuccRateMsg(SoftBusEvtReportMsg *msg, uint8_t medium)
+{
+    if (SoftBusMutexLock(&g_connSuccRate[medium].lock) != SOFTBUS_OK) {
+        return SOFTBUS_ERR;
+    }
+
+    if (strcpy_s(msg->evtName, SOFTBUS_HISYSEVT_NAME_LEN, STATISTIC_EVT_CONN_SUCC_RATE) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    msg->evtType = SOFTBUS_EVT_TYPE_STATISTIC;
+    msg->paramNum = SOFTBUS_EVT_PARAM_FOUR;
+
+    SoftBusEvtParam *param = &msg->paramArray[SOFTBUS_EVT_PARAM_ZERO];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT8;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_MEDIUM) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u8v = medium;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_ONE];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_CONN_SUCC_TIMES) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u32v = g_connSuccRate[medium].succTime;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_TWO];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_CONN_FAIL_TIMES) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u32v = g_connSuccRate[medium].failTime;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_THREE];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_FLOAT;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_CONN_SUCC_RATE) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.f = g_connSuccRate[medium].succRate;
+    (void)SoftBusMutexUnlock(&g_connSuccRate[medium].lock);
+    return SOFTBUS_OK;
+}
+
+static int32_t SoftBusReportConnSuccRateEvt()
+{
+    SoftBusEvtReportMsg *msg = SoftbusCreateEvtReportMsg(SOFTBUS_EVT_PARAM_FOUR);
+    if (msg == NULL) {
+        return SOFTBUS_ERR;
+    }
+    for (int i = 0; i < SOFTBUS_HISYSEVT_CONN_MEDIUM_BUTT; i++) {
+        if (SoftBusCreateConnSuccRateMsg(msg, i) == SOFTBUS_ERR) {
+            return SOFTBUS_ERR;
+        }
+        if (SoftbusWriteHisEvt(msg) == SOFTBUS_ERR) {
+            return SOFTBUS_ERR;
+        }
+    }
+    SoftbusFreeEvtReporMsg(msg);
+    ClearConnSuccRate();
+    return SOFTBUS_OK;
+}
+
+static int32_t SoftBusCreateConnFaultMsg(SoftBusEvtReportMsg *msg, uint8_t medium, int32_t errCode)
+{
+    if (msg == NULL) {
+        return SOFTBUS_ERR;
+    }
+
+    if (strcpy_s(msg->evtName, SOFTBUS_HISYSEVT_NAME_LEN, FAULT_EVT_CONN_FAULT) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    msg->evtType = SOFTBUS_EVT_TYPE_FAULT;
+    msg->paramNum = SOFTBUS_EVT_PARAM_TWO;
+
+    SoftBusEvtParam *param = &msg->paramArray[SOFTBUS_EVT_PARAM_ZERO];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_UINT8;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_MEDIUM) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.u8v = medium;
+
+    param = &msg->paramArray[SOFTBUS_EVT_PARAM_ONE];
+    param->paramType = SOFTBUS_EVT_PARAMTYPE_INT32;
+    if (strcpy_s(param->paramName, SOFTBUS_HISYSEVT_PARAM_LEN, CONN_PARAM_ERROR_CODE) != EOK) {
+        return SOFTBUS_ERR;
+    }
+    param->paramValue.i32v = errCode;
+
+    return SOFTBUS_OK;
+}
+
+int32_t SoftBusReportConnFaultEvt(uint8_t medium, int32_t errCode)
+{
+    SoftBusEvtReportMsg *msg = SoftbusCreateEvtReportMsg(SOFTBUS_EVT_PARAM_THREE);
+    if (msg == NULL) {
+        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "Alloc EvtReport Msg Fail!");
+        return SOFTBUS_ERR;
+    }
+
+    SoftBusCreateConnFaultMsg(msg, medium, errCode);
+    int ret = SoftbusWriteHisEvt(msg);
+    SoftbusFreeEvtReporMsg(msg);
+    if (ret != SOFTBUS_OK) {
+        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "Sys Evt Witre conn fault msg fail!");
+    }
+    return ret;
+}
+
+void SoftbusRecordConnInfo(uint8_t medium, SoftBusConnStatus isSucc, uint32_t time)
+{
+    if (SoftBusMutexLock(&g_connSuccRate[medium].lock) != SOFTBUS_OK) {
+        return;
+    }
+    
+    g_connSuccRate[medium].failTime += (isSucc != SOFTBUS_EVT_CONN_SUCC);
+    g_connSuccRate[medium].succTime += (isSucc == SOFTBUS_EVT_CONN_SUCC);
+    g_connSuccRate[medium].totalCnt += 1;
+    g_connSuccRate[medium].succRate = (float)(g_connSuccRate[medium].succTime)/(float)(g_connSuccRate[medium].totalCnt);
+
+    (void)SoftBusMutexUnlock(&g_connSuccRate[medium].lock);
+
+    if (isSucc != SOFTBUS_EVT_CONN_SUCC) {
+        return;
+    }
+
+    if (SoftBusMutexLock(&g_connTimeDur[medium].lock) != SOFTBUS_OK) {
+        return;
+    }
+    
+    if (time > g_connTimeDur[medium].maxConnDur) {
+        g_connTimeDur[medium].maxConnDur = time;
+    } else if (time < g_connTimeDur[medium].minConnDur) {
+        g_connTimeDur[medium].minConnDur = time;
+    }
+
+    g_connTimeDur[medium].totalConnTime += time;
+    g_connTimeDur[medium].avgConnDur = (uint32_t)(g_connTimeDur[medium].totalConnTime / g_connSuccRate[medium].succTime);
+
+    (void)SoftBusMutexUnlock(&g_connTimeDur[medium].lock);
+}
+
+int32_t InitConnStatisticSysEvt(void)
+{
+    if (InitConnEvtMutexLock() != SOFTBUS_OK) {
+        SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_ERROR, "Conn Statistic Evt Lock Init Fail!");
+        return SOFTBUS_ERR;
+    }
+    
+    ClearConnTimeDur();
+    ClearConnSuccRate();
+
+    SetStatisticEvtReportFunc(SOFTBUS_STATISTIC_EVT_CONN_DURATION, SoftBusReportConnTimeDurEvt);
+    SetStatisticEvtReportFunc(SOFTBUS_STATISTIC_EVT_CONN_SUCC_RATE, SoftBusReportConnSuccRateEvt);
+
+    return SOFTBUS_OK;
+}
