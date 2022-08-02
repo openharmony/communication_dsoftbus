f51945fd3a7b76e6a0f46c81d0fdbbcb329ae78e
y500286920 <yangliu158@huawei.com>
2022-08-02 19:16:05 +0800

    review代码规范整改
    Signed-off-by: y500286920 <yangliu158@huawei.com>

--- components/nstackx/nstackx_core/dfile/core/nstackx_dfile_retransmission.c
+++ components/nstackx/nstackx_core/dfile/core/nstackx_dfile_retransmission.c
@@ -52,7 +52,7 @@ static void SendBackPressureFrame(DFileTrans *dFileTrans)
     if (((allSize >= dFileTrans->fileManager->iowCount * FILE_RECV_LIST_IO_WRITE_THRESHOLD) ||
         (allSize >= dFileTrans->fileManager->maxRecvBlockListSize * FILE_RECV_LIST_IO_WRITE_THRESHOLD) ||
         (allSize * blockFrameSize >= recvListWindowSize)) &&
-        allSize > 0) {
+        (allSize > 0)) {
         dFileTrans->fileManager->recvListOverIo = 1;
     } else {
         dFileTrans->fileManager->recvListOverIo = 0;
