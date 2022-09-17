07f33ab96652b06f6b84852ea59645d73ff064c7
zhangguang <jean.zhangguang@huawei.com>
2022-09-17 11:21:30 +0800

    【安全】增加认证通道启用限制条件
    Signed-off-by: zhangguang <jean.zhangguang@huawei.com>

--- core/transmission/trans_channel/auth/src/trans_auth_manager.c
+++ core/transmission/trans_channel/auth/src/trans_auth_manager.c
@@ -37,6 +37,13 @@
 #define AUTH_GROUP_ID "auth group id"
 #define AUTH_SESSION_KEY "auth session key"
 
+#define AUTH_SESSION_WHITE_LIST_NUM (2)
+
+static char g_sessionWhiteList[AUTH_SESSION_WHITE_LIST_NUM][SESSION_NAME_SIZE_MAX] = {
+    "ohos.distributedhardware.devicemanager.resident",
+    "com.huawei.devicegroupmanage"
+};
+
 typedef struct {
     ListNode node;
     AppInfo appInfo;
@@ -230,6 +237,22 @@ static int32_t OnRequsetUpdateAuthChannel(int64_t authId, AppInfo *appInfo)
     return SOFTBUS_OK;
 } 
 
+static bool CheckAuthChannelOfPkgInfoIsValid(const AppInfo *appInfo)
+{
+    if (appInfo == NULL) {
+        return false;
+    }
+    size_t len = 0;
+    for (uint16_t index = 0; index < AUTH_SESSION_WHITE_LIST_NUM; ++index) {
+        len = strnlen(g_sessionWhiteList[index], SESSION_NAME_SIZE_MAX);
+        if (strncmp(appInfo->myData.sessionName, g_sessionWhiteList[index], len) == 0) {
+            return true;
+        }
+    }
+    SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR,
+        "auth channel sessionname[%s] pkgName[%s] invalid.", appInfo->myData.sessionName, appInfo->myData.pkgName);
+    return false;
+}
 
 static void OnRecvAuthChannelRequest(int64_t authId, const char *data, int32_t len)
 {
@@ -242,6 +265,11 @@ static void OnRecvAuthChannelRequest(int64_t authId, const char *data, int32_t l
     if (ret != SOFTBUS_OK) {
         SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "unpackRequest failed");
         TransPostAuthChannelErrMsg(authId, ret, "unpackRequest");
+        goto EXIT_ERR;
+    }
+    if (!CheckAuthChannelOfPkgInfoIsValid(&appInfo)) {
+        SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_ERROR, "check auth channel pkginfo invalid.");
+        TransPostAuthChannelErrMsg(authId, ret, "check msginfo failed");
         goto EXIT_ERR;
     }
     ret = OnRequsetUpdateAuthChannel(authId, &appInfo);
