686363fec81758ce54e99ea8cd539f6821ff86c4
openharmony_ci <120357966@qq.com>
2022-09-29 04:05:09 +0000

    回退 'Pull Request !2459 : 补充传输业务新增分支的UT'

--- tests/core/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_test.cpp
+++ tests/core/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_test.cpp
@@ -20,18 +20,27 @@
 #include "softbus_errcode.h"
 #include "softbus_json_utils.h"
 #include "softbus_log.h"
-#include "auth_interface.h"
 #include "trans_tcp_direct_listener.h"
 #include "trans_tcp_direct_manager.h"
 #include "trans_tcp_direct_message.h"
-#include "trans_tcp_direct_wifi.h"
-#include "trans_tcp_direct_sessionconn.h"
-#include "trans_tcp_direct_message.h"
 #include "softbus_protocol_def.h"
+
+#define TEST_ASSERT_TRUE(ret)  \
+    if (ret) {                 \
+        LOG_INFO("[succ]:%d\n", __LINE__);    \
+        printf("[succ]:%d\n", __LINE__);    \
+        g_succTestCount++;       \
+    } else {                   \
+        LOG_INFO("[error]:%d\n", __LINE__);    \
+        printf("[error]:%d\n", __LINE__);    \
+        g_failTestCount++;       \
+    }
 
 using namespace testing::ext;
 
 namespace OHOS {
+static int32_t g_succTestCount = 0;
+static int32_t g_failTestCount = 0;
 
 class TransTcpDirectTest : public testing::Test {
 public:
@@ -72,7 +81,7 @@ HWTEST_F(TransTcpDirectTest, StartSessionListenerTest001, TestSize.Level1)
         }
     };
     ret = TransTdcStartSessionListener(UNUSE_BUTT, &info);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 
     LocalListenerInfo info2 = {
         .type = CONNECT_TCP,
@@ -84,7 +93,7 @@ HWTEST_F(TransTcpDirectTest, StartSessionListenerTest001, TestSize.Level1)
         }
     };
     ret = TransTdcStartSessionListener(DIRECT_CHANNEL_SERVER_WIFI, &info2);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 
     LocalListenerInfo info3 = {
         .type = CONNECT_TCP,
@@ -96,7 +105,7 @@ HWTEST_F(TransTcpDirectTest, StartSessionListenerTest001, TestSize.Level1)
         }
     };
     ret = TransTdcStartSessionListener(DIRECT_CHANNEL_SERVER_WIFI, &info3);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 }
 
 /**
@@ -109,7 +118,7 @@ HWTEST_F(TransTcpDirectTest, StoptSessionListenerTest001, TestSize.Level1)
 {
     int ret = 0;
     ret = TransTdcStopSessionListener(DIRECT_CHANNEL_SERVER_WIFI);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 }
 
 /**
@@ -138,116 +147,12 @@ HWTEST_F(TransTcpDirectTest, OpenTcpDirectChannelTest001, TestSize.Level1)
     int fd = 1;
 
     ret = TransOpenDirectChannel(NULL, &connInfo, &fd);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 
     ret = TransOpenDirectChannel(&appInfo, NULL, &fd);
-    EXPECT_TRUE(ret != 0);
+    TEST_ASSERT_TRUE(ret != 0);
 
     ret = TransOpenDirectChannel(&appInfo, &connInfo, NULL);
-    EXPECT_TRUE(ret != 0);
-}
-
-/**
- * @tc.name: OpenTcpDirectChannelTest002
- * @tc.desc: extern module active publish, start channel with wrong parms.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(TransTcpDirectTest, OpenTcpDirectChannelTest002, TestSize.Level1)
-{
-    int ret = 0;
-    AppInfo appInfo;
-    (void)memset_s(&appInfo, sizeof(AppInfo), 0, sizeof(AppInfo));
-    ConnectOption connInfo = {
-        .type = CONNECT_TCP,
-        .socketOption = {
-            .addr = {0},
-            .port = 6000,
-            .protocol = LNN_PROTOCOL_IP,
-            .moduleId = MODULE_MESSAGE_SERVICE
-        }
-    };
-    if (strcpy_s(connInfo.socketOption.addr, sizeof(connInfo.socketOption.addr), "192.168.8.1") != EOK) {
-        return;
-    }
-    int32_t channelId = 0;
-
-    ret = OpenTcpDirectChannel(&appInfo, &connInfo, &channelId);
-    EXPECT_TRUE(ret != 0);
-}
-
-/**
- * @tc.name: TransTdcPostBytesTest001
- * @tc.desc: TransTdcPostBytesTest, start with wrong parms.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(TransTcpDirectTest, TransTdcPostBytesTest001, TestSize.Level1)
-{
-    int ret = 0;
-    const char *bytes = "Get Message";
-    TdcPacketHead packetHead = {
-        .magicNumber = MAGIC_NUMBER,
-        .module = MODULE_SESSION,
-        .seq = 0,
-        .flags = FLAG_REQUEST,
-        .dataLen = strlen(bytes), /* reset after encrypt */
-    };
-    int32_t channelId = 0;
-
-    ret = TransTdcPostBytes(channelId, NULL, bytes);
-    EXPECT_TRUE(ret != 0);
-
-    ret = TransTdcPostBytes(channelId, &packetHead, NULL);
-    EXPECT_TRUE(ret != 0);
-
-    packetHead.dataLen = 0;
-    ret = TransTdcPostBytes(channelId, &packetHead, bytes);
-    EXPECT_TRUE(ret != 0);
-}
-
-/**
- * @tc.name: GetCipherFlagByAuthIdTest001
- * @tc.desc: GetCipherFlagByAuthId, start channel with wrong parms.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(TransTcpDirectTest, GetCipherFlagByAuthIdTest001, TestSize.Level1)
-{
-    int64_t authId = 0;
-    uint32_t flag = 0;
-
-    int ret = GetCipherFlagByAuthId(authId, &flag, NULL);
-    EXPECT_TRUE(ret != 0);
-}
-
-/**
- * @tc.name: SessionConnListTest001
- * @tc.desc: SessionConnListTest001, start channel with wrong parms.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(TransTcpDirectTest, SessionConnListTest001, TestSize.Level1)
-{
-    SessionConn conn;
-    ListInit(&conn.node);
-
-    int ret = CreatSessionConnList();
-    ASSERT_TRUE(ret == SOFTBUS_OK);
-
-    ret = TransTdcAddSessionConn(&conn);
-    EXPECT_TRUE(ret == SOFTBUS_OK);
-
-    AppInfo appInfo;
-    ret = GetAppInfoById(conn.channelId, &appInfo);
-    EXPECT_TRUE(ret == SOFTBUS_OK);
-
-    ret = SetAuthIdByChanId(conn.channelId, 0);
-    EXPECT_TRUE(ret == SOFTBUS_OK);
-
-    int authId = GetAuthIdByChanId(conn.channelId);
-    EXPECT_TRUE(authId != AUTH_INVALID_ID);
-
-    DestroySoftBusList(GetSessionConnList());
+    TEST_ASSERT_TRUE(ret != 0);
 }
 } // namespace OHOS
