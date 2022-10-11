3aa9c7a1ef502a1d191bfc4ac09a130b7e0fa577
lbp <libaoping1@huawei.com>
2022-10-11 10:34:13 +0800

    fix:fix softbus FUZZ
    Signed-off-by: lbp <libaoping1@huawei.com>

--- tests/sdk/bus_center/fuzztest/publishlnn_fuzzer/publishlnn_fuzzer.cpp
+++ tests/sdk/bus_center/fuzztest/publishlnn_fuzzer/publishlnn_fuzzer.cpp
@@ -15,6 +15,7 @@
 
 #include "publishlnn_fuzzer.h"
 #include <cstddef>
+#include <securec.h>
 #include "softbus_bus_center.h"
 #include "softbus_errcode.h"
 
@@ -61,7 +62,11 @@ namespace OHOS {
             return true;
         }
         GenRanPublishInfo(data, size);
-        int32_t ret = PublishLNN((const char *)data, &g_pInfo, &g_publishCb);
+        char tmp[65] = {0};
+        if (memcpy_s(tmp, sizeof(tmp) - 1, data, size) != EOK) {
+            return true;
+        }
+        int32_t ret = PublishLNN((const char *)tmp, &g_pInfo, &g_publishCb);
         if (ret == SOFTBUS_OK) {
             StopPublishLNN((const char *)data, g_pInfo.publishId);
         }
