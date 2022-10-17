3f893983b2ffcfb92aafe47276e64369d162625f
wanglei744 <wanglei744@huawei.com>
2022-10-17 08:06:27 +0000

    update core/transmission/trans_channel/manager/src/trans_channel_manager.c.
    Signed-off-by: wanglei744 <wanglei744@huawei.com>

--- core/transmission/trans_channel/manager/src/trans_channel_manager.c
+++ core/transmission/trans_channel/manager/src/trans_channel_manager.c
@@ -216,7 +216,8 @@ int32_t TransOpenChannel(const SessionParam *param, TransInfo *transInfo)
         SoftbusReportTransErrorEvt(SOFTBUS_TRANS_GET_LANE_INFO_ERR);
         goto EXIT_ERR;
     }
-    SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_INFO, "sessionName[%s], get laneId[%u], link type[%u].", param->sessionName, laneId, connInfo.type);
+    SoftBusLog(SOFTBUS_LOG_TRAN, SOFTBUS_LOG_INFO,
+        "sessionName[%s], get laneId[%u], link type[%u].", param->sessionName, laneId, connInfo.type);
 
     if (TransGetConnectOptByConnInfo(&connInfo, &connOpt) != SOFTBUS_OK) {
         goto EXIT_ERR;
