1b1a99cd424395c11dd8054ca7bb48ed957c0c63
verystone <xudaqing@huawei.com>
2022-10-24 17:08:11 +0800

    fixed 9a155f0 from https://gitee.com/verystone/communication_dsoftbus/pulls/2639 Add fuzz testcase
    Signed-off-by: verystone <xudaqing@huawei.com>

---
+++ tests/core/connection/common/fuzztest/connectioncommon_fuzzer/connectioncommon_fuzzer.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "connectioncommon_fuzzer.h"
+#include <vector>
+#include <securec.h>
+#include "softbus_datahead_transform.h"
+#include "softbus_socket.h"
+#include "softbus_tcp_socket.h"
+
+namespace OHOS {
+static void DoDataHeadTransformFuzz(const uint8_t *data, size_t size)
+{
+    if (size < sizeof(ConnPktHead)) {
+        return;
+    }
+    ConnPktHead head;
+    if (memcpy_s(&head, sizeof(head), data, sizeof(head)) != EOK) {
+        return;
+    }
+    PackConnPktHead(&head);
+    UnpackConnPktHead(&head);
+
+    if (size < sizeof(ProxyMessageHead)) {
+        return;
+    }
+    ProxyMessageHead  proxyMessageHead;
+    if (memcpy_s(&proxyMessageHead, sizeof(proxyMessageHead), data, sizeof(proxyMessageHead)) != EOK) {
+        return;
+    }
+    PackProxyMessageHead(&proxyMessageHead);
+    UnpackProxyMessageHead(&proxyMessageHead);
+}
+
+static ConnectOption GenerateConnectOption(const uint8_t *data, size_t size)
+{
+    ConnectOption connectOption = {
+        .type = CONNECT_TCP,
+        .socketOption = {
+            .addr = "127.0.0.1",
+            .protocol = LNN_PROTOCOL_IP,
+        },
+    };
+    if (size < sizeof(int32_t)) {
+        return connectOption;
+    }
+    if (memcpy_s(&connectOption.socketOption.port, sizeof(int32_t), data, sizeof(int32_t)) != EOK) {
+        return connectOption;
+    }
+    return connectOption;
+}
+
+static constexpr int TCP_KEEP_ALIVE_TIME = 5;
+static constexpr int TCP_USER_TIMEOUT = 5;
+static void DoSocketFuzz(const uint8_t *data, size_t size)
+{
+    ConnInitSockets();
+    ConnectOption connectOption = GenerateConnectOption(data, size);
+    int socketFd = ConnOpenClientSocket(&connectOption, "127.0.0.1", false);
+    if (socketFd > 0) {
+        ConnSendSocketData(socketFd, reinterpret_cast<const char *>(data), size, 0);
+        std::vector<char> recvBuf(size);
+        ConnRecvSocketData(socketFd, recvBuf.data(), size, 0);
+        ConnSetTcpKeepAlive(socketFd, TCP_KEEP_ALIVE_TIME);
+        ConnSetTcpUserTimeOut(socketFd, TCP_USER_TIMEOUT);
+        ConnToggleNonBlockMode(socketFd, true);
+        ConnGetLocalSocketPort(socketFd);
+        ConnGetSocketError(socketFd);
+        SocketAddr socketAddr;
+        ConnGetPeerSocketAddr(socketFd, &socketAddr);
+        ConnCloseSocket(socketFd);
+        ConnShutdownSocket(socketFd);
+    }
+    ConnDeinitSockets();
+}
+}
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int32_t)) {
+        return 0;
+    }
+    /* Run your code on data */
+    OHOS::DoDataHeadTransformFuzz(data, size);
+    OHOS::DoSocketFuzz(data, size);
+    return 0;
+}
