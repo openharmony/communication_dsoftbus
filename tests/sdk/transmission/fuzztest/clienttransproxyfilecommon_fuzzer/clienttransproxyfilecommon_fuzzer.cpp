a6dc9de3cf24c72d5b6c569a1561c9025ef340ed
dong-peng25 <dongpeng25@huawei.com>
2022-10-25 23:04:33 +0800

    fixed df07f32 from https://gitee.com/dong-peng25/communication_dsoftbus/pulls/2666 fix:The dsoftbus transport module adds the FUZZ use case
    Signed-off-by: dong-peng25 <dongpeng25@huawei.com>

---
+++ tests/sdk/transmission/fuzztest/clienttransproxyfilecommon_fuzzer/clienttransproxyfilecommon_fuzzer.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "clienttransproxyfilecommon_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string>
+
+#include "client_trans_proxy_file_common.h"
+#include "securec.h"
+#include "softbus_adapter_file.h"
+#include "softbus_adapter_mem.h"
+#include "softbus_adapter_timer.h"
+#include "softbus_def.h"
+#include "softbus_errcode.h"
+#include "softbus_log.h"
+#include "softbus_type_def.h"
+
+
+namespace OHOS {
+void ClientTransProxyFileCommonTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size == 0)) {
+        return;
+    }
+
+    char *filePath = nullptr;
+    char *absPath = nullptr;
+    char *destFile = nullptr;
+    uint64_t index = *(reinterpret_cast<const uint64_t*>(data));
+    uint64_t frameNumber = *(reinterpret_cast<const uint64_t*>(data));
+    uint8_t buffer = *(reinterpret_cast<const uint8_t*>(data));
+    uint32_t bufferSize = *(reinterpret_cast<const uint32_t*>(data));
+    int32_t fileCount = *(reinterpret_cast<const int32_t*>(data));
+    char *path = nullptr;
+    int32_t fd = *(reinterpret_cast<const int32_t*>(data));
+    int32_t type = *(reinterpret_cast<const int32_t*>(data));
+    bool isBlock = *(reinterpret_cast<const bool*>(data));
+
+    IsPathValid(filePath);
+
+    GetAndCheckRealPath(filePath, absPath);
+
+    CheckDestFilePathValid(destFile);
+
+    FrameIndexToType(index, frameNumber);
+
+    BufferToFileList(&buffer, bufferSize, &fileCount);
+
+    TransGetFileName(path);
+
+    FileLock(fd, type, isBlock);
+
+    FileUnLock(fd);
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::ClientTransProxyFileCommonTest(data, size);
+    return 0;
+}
+
---
+++ tests/sdk/transmission/fuzztest/clienttransstream_fuzzer/clienttransstream_fuzzer.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "clienttransstream_fuzzer.h"
+#include <cstddef>
+#include <cstdint>
+#include "client_trans_stream.h"
+#include "session.h"
+#include "softbus_def.h"
+
+
+namespace OHOS {
+    void TransOnstreamChannelOpenedTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return;
+        }
+        ChannelInfo *channel = nullptr;
+
+        TransOnstreamChannelOpened(channel, (int32_t *)size);
+    }
+
+    void TransSendStreamTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return;
+        }
+        StreamData *streamdata = nullptr;
+        StreamData *ext = nullptr;
+        StreamFrameInfo *param = nullptr;
+
+        TransSendStream(size, streamdata, ext, param);
+    }
+
+    void TransCloseStreamChannelTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return;
+        }
+
+        TransCloseStreamChannel(size);
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::TransOnstreamChannelOpenedTest(data, size);
+    OHOS::TransSendStreamTest(data, size);
+    OHOS::TransCloseStreamChannelTest(data, size);
+
+    return 0;
+}
+
