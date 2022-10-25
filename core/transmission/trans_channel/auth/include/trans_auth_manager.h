84170d3ad9d96d29b21ee4814acdfa99f2f4301b
shixintao <shixintao1@huawei.com>
2022-10-25 10:53:12 +0000

    fix notify auth success failed on br/ble Signed-off-by: samuel.shi <shixintao1@huawei.com>
    Change-Id: I3b36a6b3571cddf1ec804414418730e7eb7aa979

--- core/transmission/trans_channel/auth/include/trans_auth_manager.h
+++ core/transmission/trans_channel/auth/include/trans_auth_manager.h
@@ -31,10 +31,12 @@ void TransAuthDeinit(void);
 int32_t TransAuthGetNameByChanId(int32_t chanId, char *pkgName, char *sessionName,
     uint16_t pkgLen, uint16_t sessionLen);
 int32_t TransOpenAuthMsgChannel(const char *sessionName, const ConnectOption *connOpt, int32_t *channedId);
-int32_t TransNotifyAuthDataSuccess(int32_t channelId);
+int32_t TransNotifyAuthDataSuccess(int32_t channelId, const ConnectOption *connOpt);
 int32_t TransCloseAuthChannel(int32_t channelId);
 int32_t TransSendAuthMsg(int32_t channelId, const char *msg, int32_t len);
-int32_t TransGetAuthAppInfoByChanId(int32_t channelId, AppInfo *appInfo);
+int32_t TransAuthGetAppInfoByChanId(int32_t channelId, AppInfo *appInfo);
+int32_t TransAuthGetConnOptionByChanId(int32_t channelId, ConnectOption *connOpt);
+
 #ifdef __cplusplus
 #if __cplusplus
 }
--- core/transmission/trans_channel/proxy/include/softbus_proxychannel_manager.h
+++ core/transmission/trans_channel/proxy/include/softbus_proxychannel_manager.h
@@ -31,21 +31,30 @@ int32_t TransProxyGetNewChanSeq(int32_t channelId);
 int32_t TransProxyOpenProxyChannel(const AppInfo *appInfo, const ConnectOption *connInfo, int32_t *channelId);
 int32_t TransProxyCloseProxyChannel(int32_t channelId);
 int32_t TransProxySendMsg(int32_t channelId, const char *data, uint32_t dataLen, int32_t priority);
+
 void TransProxyDelByConnId(uint32_t connId);
+void TransProxyDelChanByReqId(int32_t reqId);
+void TransProxyDelChanByChanId(int32_t chanlId);
+
 void TransProxyOpenProxyChannelSuccess(int32_t chanId);
 void TransProxyOpenProxyChannelFail(int32_t channelId, const AppInfo *appInfo, int32_t errCode);
 void TransProxyonMessageReceived(const ProxyMessage *msg);
+
 int32_t TransProxyGetSessionKeyByChanId(int32_t channelId, char *sessionKey, uint32_t sessionKeySize);
 int16_t TransProxyGetNewMyId(void);
 int32_t TransProxyGetSendMsgChanInfo(int32_t channelId, ProxyChannelInfo *chanInfo);
-void TransProxyDelChanByReqId(int32_t reqId);
+
 int32_t TransProxyCreateChanInfo(ProxyChannelInfo *chan, int32_t channelId, const AppInfo *appInfo);
 void TransProxyChanProcessByReqId(int32_t reqId, uint32_t connId);
-void TransProxyDelChanByChanId(int32_t chanlId);
+
 int64_t TransProxyGetAuthId(int32_t channelId);
 int32_t TransProxyGetNameByChanId(int32_t chanId, char *pkgName, char *sessionName,
     uint16_t pkgLen, uint16_t sessionLen);
+
 void TransProxyDeathCallback(const char *pkgName, int32_t pid);
+
 int32_t TransProxyGetAppInfoByChanId(int32_t chanId, AppInfo* appInfo);
 int32_t TransProxyGetConnIdByChanId(int32_t channelId, int32_t *connId);
+int32_t TransProxyGetConnOptionByChanId(int32_t channelId, ConnectOption *connOpt);
+
 #endif
