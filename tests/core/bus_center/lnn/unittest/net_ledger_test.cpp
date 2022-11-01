a249668ed8801784cdb722041fa27b2f70189e33
seanpeak <eastwinder@126.com>
2022-11-01 19:53:02 +0800

    fix:Supplemental UT test Cases
    Signed-off-by: koujiaxin <koujiaxin@huawei.com>

--- tests/core/bus_center/lnn/unittest/net_ledger_test.cpp
+++ tests/core/bus_center/lnn/unittest/net_ledger_test.cpp
@@ -15,12 +15,29 @@
 
 #include <gtest/gtest.h>
 #include <securec.h>
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "auth_interface.h"
 #include "lnn_decision_db.h"
 #include "lnn_decision_db.c"
+#include "lnn_distributed_net_ledger.h"
+#include "lnn_event_monitor.h"
+#include "lnn_local_net_ledger.h"
+#include "lnn_network_manager.h"
+#include "softbus_log.h"
+#include "softbus_utils.h"
+#include "softbus_errcode.h"
+#include "softbus_conn_interface.h"
 
 namespace OHOS {
 using namespace testing::ext;
 constexpr uint32_t TEST_DATA_LEN = 10;
+constexpr uint8_t DEFAULT_SIZE = 5;
 
 class NetLedgerTest : public testing::Test {
 public:
@@ -75,9 +92,87 @@ HWTEST_F(NetLedgerTest, LNN_GET_TRUSTED_DEV_INFO_FROM_DB_Test_001, TestSize.Leve
     uint32_t num = 0;
     int32_t ret;
 
-    char *udidArray = (char *)SoftBusMalloc(TEST_DATA_LEN);
+    char *udidArray = new char[TEST_DATA_LEN];
     ret = LnnGetTrustedDevInfoFromDb(&udidArray, &num);
     EXPECT_TRUE(ret == SOFTBUS_ERR);
-    SoftBusFree(udidArray);
+    delete[] udidArray;
+}
+
+/*
+* @tc.name: DL_GET_Test_001
+* @tc.desc: Dl Get Auth test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerTest, DL_GET_Test_001, TestSize.Level0)
+{
+    char networkId[DEFAULT_SIZE] = "1234";
+    int32_t info = 1234;
+    EXPECT_TRUE(LnnGetRemoteNumInfo(nullptr, NUM_KEY_SESSION_PORT, &info) == SOFTBUS_INVALID_PARAM);
+    EXPECT_TRUE(LnnGetRemoteNumInfo(networkId, STRING_KEY_END, &info) == SOFTBUS_INVALID_PARAM);
+    EXPECT_TRUE(LnnGetRemoteNumInfo(networkId, NUM_KEY_END, &info) == SOFTBUS_INVALID_PARAM);
+    EXPECT_TRUE(LnnGetRemoteNumInfo(networkId, NUM_KEY_END, nullptr) == SOFTBUS_INVALID_PARAM);
+    EXPECT_TRUE(LnnGetRemoteNumInfo(networkId, NUM_KEY_AUTH_PORT, &info) == SOFTBUS_ERR);
+}
+
+/*
+* @tc.name: LNN_ADD_META_INFO_Test_001
+* @tc.desc: Lnn Add Meta Info test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerTest, LNN_ADD_META_INFO_Test_001, TestSize.Level0)
+{
+    NodeInfo info;
+    (void)memset_s(&info, sizeof(NodeInfo), 0, sizeof(NodeInfo));
+    EXPECT_TRUE(LnnAddMetaInfo(&info) == SOFTBUS_OK);
+}
+
+/*
+* @tc.name: LNN_DELETE_META_INFO_Test_001
+* @tc.desc: Lnn Delete Meta Info test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerTest, LNN_DELETE_META_INFO_Test_001, TestSize.Level0)
+{
+    char udid[DEFAULT_SIZE] = "1234";
+    ConnectionAddrType type = CONNECTION_ADDR_WLAN;
+    EXPECT_TRUE(LnnDeleteMetaInfo(udid, type) == SOFTBUS_ERR);
+}
+
+/*
+* @tc.name: LNN_ADD_ONLINE_NODE_Test_001
+* @tc.desc: Lnn Add Online Node test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerTest, LNN_ADD_ONLINE_NODE_Test_001, TestSize.Level0)
+{
+    NodeInfo *info = nullptr;
+    EXPECT_TRUE(LnnAddOnlineNode(info) == REPORT_NONE);
+    NodeInfo infoValue;
+    (void)memset_s(&infoValue, sizeof(NodeInfo), 0, sizeof(NodeInfo));
+    EXPECT_TRUE(LnnAddOnlineNode(&infoValue) == REPORT_ONLINE);
+}
+
+/*
+* @tc.name: GET_ALL_ONLINE_AND_META_NODE_INFO_Test_001
+* @tc.desc: Get All Online And Meta Node Info test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerTest, GET_ALL_ONLINE_AND_META_NODE_INFO_Test_001, TestSize.Level0)
+{
+    NodeBasicInfo base;
+    NodeBasicInfo *info = nullptr;
+    int32_t infoNum = 0;
+    EXPECT_TRUE(LnnGetAllOnlineAndMetaNodeInfo(nullptr, &infoNum) == SOFTBUS_ERR);
+    info = &base;
+    (void)memset_s(info, sizeof(NodeBasicInfo), 0, sizeof(NodeBasicInfo));
+    EXPECT_TRUE(LnnGetAllOnlineAndMetaNodeInfo(&info, nullptr) == SOFTBUS_ERR);
+    EXPECT_TRUE(LnnGetAllOnlineAndMetaNodeInfo(&info, &infoNum) == SOFTBUS_OK);
+    infoNum = DEFAULT_SIZE;
+    EXPECT_TRUE(LnnGetAllOnlineAndMetaNodeInfo(&info, &infoNum) == SOFTBUS_OK);
 }
 } // namespace OHOS
