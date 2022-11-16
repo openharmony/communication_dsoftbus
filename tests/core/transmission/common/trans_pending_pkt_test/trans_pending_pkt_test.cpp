2644d6dc8f7b6e2548796d2676bc3c6175ed3c42
wangyb0625 <wangyibo38@huawei.com>
2022-11-16 17:08:12 +0800

    上传core\transmission\common目录ut
    Signed-off-by: wangyb0625 <wangyibo38@huawei.com>

---
+++ tests/core/transmission/common/trans_pending_pkt_test/trans_pending_pkt_test.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <securec.h>
+
+#include "gtest/gtest.h"
+#include "session.h"
+#include "softbus_errcode.h"
+#include "softbus_json_utils.h"
+#include "softbus_log.h"
+#include "softbus_protocol_def.h"
+#include "trans_pending_pkt.h"
+
+using namespace testing::ext;
+namespace OHOS {
+#define TEST_SESSION_NAME "com.softbus.transmission.test"
+#define TEST_CONN_IP "192.168.8.1"
+#define TEST_AUTH_PORT 6000
+#define TEST_AUTH_DATA "test auth message data"
+
+class TransPendingPktTest : public testing::Test {
+public:
+    TransPendingPktTest()
+    {}
+    ~TransPendingPktTest()
+    {}
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp() override
+    {}
+    void TearDown() override
+    {}
+};
+
+void TransPendingPktTest::SetUpTestCase(void)
+{}
+
+void TransPendingPktTest::TearDownTestCase(void)
+{}
+
+/**
+ * @tc.name: PendingInit001
+ * @tc.desc: PendingInit001, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransPendingPktTest, PendingInit001, TestSize.Level1)
+{
+    int type = -88;
+    int32_t ret = PendingInit(type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+
+    type = 999;
+    ret = PendingInit(type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+
+    type = PENDING_TYPE_DIRECT;
+    ret = PendingInit(type);
+    EXPECT_EQ(SOFTBUS_OK, ret);
+}
+
+/**
+ * @tc.name: PendingDeinit001
+ * @tc.desc: PendingDeinit001, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransPendingPktTest, PendingDeinit001, TestSize.Level1)
+{
+    int type = -21;
+    PendingDeinit(type);
+
+    type = 999;
+    PendingDeinit(type);
+
+    type = PENDING_TYPE_DIRECT;
+    int32_t ret = PendingInit(type);
+    EXPECT_EQ(SOFTBUS_OK, ret);
+    PendingDeinit(type);
+}
+
+/**
+ * @tc.name: ProcPendingPacket001
+ * @tc.desc: ProcPendingPacket001, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransPendingPktTest, ProcPendingPacket001, TestSize.Level1)
+{
+    int32_t seqNum = 1111;
+    int32_t channelId = 222;
+    int type = PENDING_TYPE_BUTT + 1;
+    int32_t ret = ProcPendingPacket(channelId, seqNum, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+    type = PENDING_TYPE_PROXY - 1;
+    ret = ProcPendingPacket(channelId, seqNum, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+}
+
+/**
+ * @tc.name: SetPendingPacket001
+ * @tc.desc: SetPendingPacket001, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransPendingPktTest, SetPendingPacket001, TestSize.Level1)
+{
+    int32_t channelId = 1111;
+    int32_t seqNum = 222;
+    int type = PENDING_TYPE_BUTT + 1;
+    int32_t ret = SetPendingPacket(channelId, seqNum, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+    type = PENDING_TYPE_PROXY - 1;
+    ret = SetPendingPacket(channelId, seqNum, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+
+    type = PENDING_TYPE_DIRECT;
+    channelId = -1;
+    ret = SetPendingPacket(channelId, seqNum, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+}
+
+/**
+ * @tc.name: DelPendingPacket001
+ * @tc.desc: DelPendingPacket001, use the wrong parameter.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(TransPendingPktTest, DelPendingPacket001, TestSize.Level1)
+{
+    int32_t channelId = 1111;
+    int type = PENDING_TYPE_BUTT + 1;
+    int32_t ret = DelPendingPacket(channelId, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+    type = PENDING_TYPE_PROXY - 1;
+    ret = DelPendingPacket(channelId, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+
+    PendingDeinit(type);
+    ret = DelPendingPacket(channelId, type);
+    EXPECT_EQ(SOFTBUS_ERR, ret);
+}
+} // OHOS
