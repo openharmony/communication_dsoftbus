e53f5735acf3b10ad8b9492196f16ebb97b3dee5
maodong5 <maodong5@huawei.com>
2022-11-23 08:43:03 +0000

    update tests/core/transmission/trans_channel/tcp_direct/unittest/trans_core_tcp_direct_test.cpp.
    Signed-off-by: maodong5 <maodong5@huawei.com>

--- tests/core/transmission/trans_channel/tcp_direct/unittest/trans_core_tcp_direct_test.cpp
+++ tests/core/transmission/trans_channel/tcp_direct/unittest/trans_core_tcp_direct_test.cpp
@@ -66,7 +66,7 @@ namespace OHOS {
 
 static const char *g_sessionName = "com.test.trans.auth.demo";
 static const char *g_pkgName = "dms";
-static const char *g_ip = "192.168.8.1";
+static char *g_ip = "192.168.8.1";
 static int32_t g_port = 6000;
 
 class TransTcpDirectTest : public testing::Test {
@@ -342,10 +342,10 @@ HWTEST_F(TransTcpDirectTest, VerifyP2pUnPackTest009, TestSize.Level1)
     int32_t ret = VerifyP2pUnPack(json, peerIp, IP_LEN, &peerPort);
     EXPECT_TRUE(ret != SOFTBUS_OK);
 
-    ret = VerifyP2pUnPack(json, (char*)g_ip, IP_LEN, &g_port);
+    ret = VerifyP2pUnPack(json, g_ip, IP_LEN, &g_port);
     EXPECT_TRUE(ret != SOFTBUS_OK);
 
-    ret = VerifyP2pUnPack(NULL, (char*)g_ip, IP_LEN, &g_port);
+    ret = VerifyP2pUnPack(NULL, g_ip, IP_LEN, &g_port);
     EXPECT_TRUE(ret != SOFTBUS_OK);
     cJSON_Delete(json);
 }
