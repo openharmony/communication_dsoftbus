9fbec015e070d0c56ba5439e994608c2a1c124a2
张再尧 <zhangzaiyao@huawei.com>
2022-11-25 04:32:45 +0000

    update adapter/common/kernel/posix/softbus_adapter_socket.c.
    Signed-off-by: 张再尧 <zhangzaiyao@huawei.com>

--- adapter/common/kernel/posix/softbus_adapter_socket.c
+++ adapter/common/kernel/posix/softbus_adapter_socket.c
@@ -203,6 +203,9 @@ int32_t SoftBusSocketGetPeerName(int32_t socketFd, SoftBusSockAddr *addr, int32_
 
 int32_t SoftBusSocketBind(int32_t socketFd, SoftBusSockAddr *addr, int32_t addrLen)
 {
+    if (addrLen < 0) {
+        return SOFTBUS_ADAPTER_ERR;
+    }
     struct sockaddr sysAddr;
     uint32_t len = (sizeof(SoftBusSockAddr) > (uint32_t)addrLen) ? (uint32_t)addrLen : sizeof(SoftBusSockAddr);
     if (SoftBusAddrToSysAddr(addr, &sysAddr, len) != SOFTBUS_ADAPTER_OK) {
