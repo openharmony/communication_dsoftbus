1c030d2ae1d76edeeef273a3ecfd8d1b0694ea53
liangqi <liangqi22@huawei.com>
2022-12-05 22:13:00 +0800

    add auth ut
    Signed-off-by: liangqi <liangqi22@huawei.com>

--- tests/core/bus_center/mock_common/include/lnn_connection_mock.h
+++ tests/core/bus_center/mock_common/include/lnn_connection_mock.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,11 +16,16 @@
 #ifndef LNN_CONNECTION_MOCK_H
 #define LNN_CONNECTION_MOCK_H
 
+#include "auth_common.h"
+#include "disc_interface.h"
+#include "lnn_node_info.h"
+#include "map"
+#include "securec.h"
+#include "softbus_adapter_mem.h"
+#include "softbus_conn_interface.h"
+#include "softbus_log.h"
 #include <gmock/gmock.h>
 #include <mutex>
-
-#include "disc_interface.h"
-#include "softbus_conn_interface.h"
 
 namespace OHOS {
 class LnnConnectInterface {
@@ -45,18 +50,40 @@ class LnnConnectInterfaceMock : public LnnConnectInterface {
 public:
     LnnConnectInterfaceMock();
     ~LnnConnectInterfaceMock() override;
-    MOCK_METHOD2(ConnGetConnectionInfo, int32_t (uint32_t, ConnectionInfo *));
-    MOCK_METHOD2(ConnSetConnectCallback, int32_t (ConnModule, const ConnectCallback *));
-    MOCK_METHOD1(ConnUnSetConnectCallback, void (ConnModule));
-    MOCK_METHOD3(ConnConnectDevice, int32_t (const ConnectOption *, uint32_t, const ConnectResult *));
-    MOCK_METHOD1(ConnDisconnectDevice, int32_t (uint32_t));
-    MOCK_METHOD0(ConnGetHeadSize, uint32_t (void));
-    MOCK_METHOD2(ConnPostBytes, int32_t (uint32_t, ConnPostData *));
-    MOCK_METHOD1(CheckActiveConnection, bool (const ConnectOption *));
-    MOCK_METHOD1(ConnStartLocalListening, int32_t (const LocalListenerInfo *));
-    MOCK_METHOD1(ConnStopLocalListening, int32_t (const LocalListenerInfo *));
-    MOCK_METHOD1(ConnGetNewRequestId, uint32_t (ConnModule));
-    MOCK_METHOD1(DiscDeviceInfoChanged, void (InfoTypeChanged));
+    MOCK_METHOD2(ConnGetConnectionInfo, int32_t(uint32_t, ConnectionInfo *));
+    MOCK_METHOD2(ConnSetConnectCallback, int32_t(ConnModule, const ConnectCallback *));
+    MOCK_METHOD1(ConnUnSetConnectCallback, void(ConnModule));
+    MOCK_METHOD3(ConnConnectDevice, int32_t(const ConnectOption *, uint32_t, const ConnectResult *));
+    MOCK_METHOD1(ConnDisconnectDevice, int32_t(uint32_t));
+    MOCK_METHOD0(ConnGetHeadSize, uint32_t(void));
+    MOCK_METHOD2(ConnPostBytes, int32_t(uint32_t, ConnPostData *));
+    MOCK_METHOD1(CheckActiveConnection, bool(const ConnectOption *));
+    MOCK_METHOD1(ConnStartLocalListening, int32_t(const LocalListenerInfo *));
+    MOCK_METHOD1(ConnStopLocalListening, int32_t(const LocalListenerInfo *));
+    MOCK_METHOD1(ConnGetNewRequestId, uint32_t(ConnModule));
+    MOCK_METHOD1(DiscDeviceInfoChanged, void(InfoTypeChanged));
+    static inline ConnectCallback g_conncallback;
+    static inline ConnectResult g_connresultcb;
+    static int32_t ActionofConnSetConnectCallback(ConnModule moduleId, const ConnectCallback *callback);
+    static int32_t ActionofOnConnectSuccessed(const ConnectOption *option, uint32_t requestId,
+        const ConnectResult *result);
+    static int32_t ActionofOnConnectFailed(const ConnectOption *option, uint32_t requestId,
+        const ConnectResult *result);
+    static int32_t ActionofConnGetConnectionInfo(uint32_t connectionId, ConnectionInfo *info);
+    static void ActionofConnUnSetConnectCallback(ConnModule moduleId);
+    static void OnVerifyPassed(uint32_t requestId, int64_t authId, const NodeInfo *info)
+    {
+        (void)requestId;
+        (void)authId;
+        (void)info;
+        return;
+    }
+    static void onVerifyFailed(uint32_t requestId, int32_t reason)
+    {
+        (void)requestId;
+        (void)reason;
+        return;
+    }
 };
 } // namespace OHOS
 #endif // AUTH_CONNECTION_MOCK_H
--- tests/core/bus_center/mock_common/src/lnn_hichain_mock.cpp
+++ tests/core/bus_center/mock_common/src/lnn_hichain_mock.cpp
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,72 +14,30 @@
  */
 
 #include "lnn_hichain_mock.h"
-
 #include "auth_interface.h"
+#include "softbus_adapter_log.h"
 #include "softbus_adapter_mem.h"
+#include "softbus_log.h"
 
 using namespace testing;
 using namespace testing::ext;
 
 namespace OHOS {
-
 const int32_t GRUOP_NUM1 = 10;
 const int32_t GRUOP_NUM2 = 12;
 const int32_t GRUOP_NUM3 = 100;
-
 void *g_hichainInterface;
+
 LnnHichainInterfaceMock::LnnHichainInterfaceMock()
 {
+    SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_INFO, "construction");
     g_hichainInterface = reinterpret_cast<void *>(this);
 }
 
 LnnHichainInterfaceMock::~LnnHichainInterfaceMock()
 {
+    SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_INFO, "delete");
     g_hichainInterface = nullptr;
-}
-
-int32_t LnnHichainInterfaceMock::InvokeAuthDevice(int32_t osAccountId, int64_t authReqId, const char *authParams,
-    const DeviceAuthCallback *gaCallback)
-{
-    return 0;
-}
-
-int32_t LnnHichainInterfaceMock::InvokeDataChangeListener(const char *appId, const DataChangeListener *listener)
-{
-    return 0;
-}
-
-int32_t LnnHichainInterfaceMock::InvokeGetJoinedGroups1(int32_t osAccountId, const char *appId, int groupType,
-    char **returnGroupVec, uint32_t *groupNum)
-{
-    (void)osAccountId;
-    (void)appId;
-    (void)groupType;
-    *groupNum = 1;
-
-    if (groupType == AUTH_IDENTICAL_ACCOUNT_GROUP) {
-        *groupNum = GRUOP_NUM1;
-    }
-    if (groupType == AUTH_PEER_TO_PEER_GROUP) {
-        *groupNum = GRUOP_NUM2;
-    }
-    *returnGroupVec = (char *)SoftBusCalloc(*groupNum);
-    if (*returnGroupVec == NULL) {
-        return -1;
-    }
-    return 0;
-}
-
-int32_t LnnHichainInterfaceMock::InvokeGetJoinedGroups2(int32_t osAccountId, const char *appId, int groupType,
-    char **returnGroupVec, uint32_t *groupNum)
-{
-    (void)osAccountId;
-    (void)appId;
-    (void)groupType;
-    (void)returnGroupVec;
-    *groupNum = GRUOP_NUM3;
-
-    return -1;
 }
 
 static LnnHichainInterface *GetHichainInterface()
@@ -105,7 +63,84 @@ const GroupAuthManager *GetGaInstance(void)
 
 const DeviceGroupManager *GetGmInstance(void)
 {
+    SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_INFO, "GetGmInstance");
     return GetHichainInterface()->GetGmInstance();
 }
 }
+
+int32_t LnnHichainInterfaceMock::ActionOfProcessData(int64_t authSeq, const uint8_t *data, uint32_t len,
+    const DeviceAuthCallback *gaCallback)
+{
+    (void)authSeq;
+    (void)data;
+    (void)len;
+    g_devAuthCb.onTransmit = gaCallback->onTransmit;
+    g_devAuthCb.onSessionKeyReturned = gaCallback->onSessionKeyReturned;
+    g_devAuthCb.onFinish = gaCallback->onFinish;
+    g_devAuthCb.onError = gaCallback->onError;
+    g_devAuthCb.onRequest = gaCallback->onRequest;
+    return HC_SUCCESS;
 }
+
+int32_t LnnHichainInterfaceMock::InvokeAuthDevice(int32_t osAccountId, int64_t authReqId, const char *authParams,
+    const DeviceAuthCallback *gaCallback)
+{
+    return HC_SUCCESS;
+}
+
+int32_t LnnHichainInterfaceMock::AuthDeviceConnSend(int32_t osAccountId, int64_t authReqId, const char *authParams,
+    const DeviceAuthCallback *gaCallback)
+{
+    SoftBusLog(SOFTBUS_LOG_COMM, SOFTBUS_LOG_INFO, "AuthDeviceConnSend");
+    (void)SoftBusCondSignal(&LnnHichainInterfaceMock::cond);
+    return HC_SUCCESS;
+}
+
+int32_t LnnHichainInterfaceMock::InvokeDataChangeListener(const char *appId, const DataChangeListener *listener)
+{
+    if (appId == nullptr || listener == nullptr) {
+        return SOFTBUS_INVALID_PARAM;
+    }
+    LnnHichainInterfaceMock::g_datachangelistener.emplace(appId, listener);
+    return SOFTBUS_OK;
+}
+
+int32_t LnnHichainInterfaceMock::InvokeGetJoinedGroups1(int32_t osAccountId, const char *appId, int groupType,
+    char **returnGroupVec, uint32_t *groupNum)
+{
+    (void)osAccountId;
+    (void)appId;
+    (void)groupType;
+    *groupNum = 1;
+
+    if (groupType == AUTH_IDENTICAL_ACCOUNT_GROUP) {
+        *groupNum = GRUOP_NUM1;
+    }
+    if (groupType == AUTH_PEER_TO_PEER_GROUP) {
+        *groupNum = GRUOP_NUM2;
+    }
+    *returnGroupVec = reinterpret_cast<char *>(SoftBusCalloc(*groupNum));
+    if (*returnGroupVec == NULL) {
+        return HC_ERR_INVALID_PARAMS;
+    }
+    return HC_SUCCESS;
+}
+
+int32_t LnnHichainInterfaceMock::InvokeGetJoinedGroups2(int32_t osAccountId, const char *appId, int groupType,
+    char **returnGroupVec, uint32_t *groupNum)
+{
+    (void)osAccountId;
+    (void)appId;
+    (void)groupType;
+    (void)returnGroupVec;
+    *groupNum = GRUOP_NUM3;
+
+    return HC_ERR_INVALID_PARAMS;
+}
+int32_t LnnHichainInterfaceMock::ActionofunRegDataChangeListener(const char *appId)
+{
+    (void)appId;
+    LnnHichainInterfaceMock::g_datachangelistener.clear();
+    return SOFTBUS_OK;
+}
+} // namespace OHOS
