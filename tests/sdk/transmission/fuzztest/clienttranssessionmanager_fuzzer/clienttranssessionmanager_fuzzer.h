f2b3bd54393eff772d6a705b9b783461874f08cc
wangyb0625 <wangyibo38@huawei.com>
2022-12-05 16:39:41 +0800

    新增sdk目录ut Signed-off-by: wangyb0625 <wangyibo38@huawei.com>

--- tests/sdk/transmission/fuzztest/clienttranssessionmanager_fuzzer/clienttranssessionmanager_fuzzer.h
+++ tests/sdk/transmission/fuzztest/clienttranssessionmanager_fuzzer/clienttranssessionmanager_fuzzer.h
@@ -12,6 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef CLIENTTRANSSESSIONMANAGER_FUZZER_H
+#define CLIENTTRANSSESSIONMANAGER_FUZZER_H
 
 #include <cstdint>
 #include <unistd.h>
@@ -19,9 +21,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <fcntl.h>
-
-#ifndef CLIENTTRANSSESSIONMANAGER_FUZZER_H
-#define CLIENTTRANSSESSIONMANAGER_FUZZER_H
 
 #define FUZZ_PROJECT_NAME "clienttranssessionmanager_fuzzer"
 
