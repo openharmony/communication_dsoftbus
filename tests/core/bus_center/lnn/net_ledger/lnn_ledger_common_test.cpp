7850aee41e43cf81efa1827504ce9cbcf0fba3a8
y30033032 <yuanmeng36@huawei.com>
2022-12-07 19:06:17 +0800

    Add ledger ut test and ut tests codex bug fix
    Signed-off-by: y30033032 <yuanmeng36@huawei.com>

--- tests/core/bus_center/lnn/net_ledger/lnn_ledger_common_test.cpp
+++ tests/core/bus_center/lnn/net_ledger/lnn_ledger_common_test.cpp
@@ -61,6 +61,18 @@ constexpr char LOCAL_UUID[] = "235999LOCAL";
 constexpr char LOCAL_BT_MAC[] = "56789TUT";
 constexpr char LOCAL_WLAN_IP[] = "10.146.181.134";
 constexpr int32_t DEFAULT_FD = 1;
+constexpr char LOCAL_DEVTYPE[] = "TYPE_WATCH";
+constexpr char LOCAL_NET_IF_NAME[] = "LOCAL";
+constexpr char MASTER_NODE_UDID[] = "234567LOCALTEST";
+constexpr char LOCAL_NODE_ADDR[] = "ADDR";
+constexpr char LOCAL_P2P_MAC[] = "11:22:33:44:55";
+constexpr char LOCAL_GO_MAC[] = "22:33:44:55:66";
+constexpr uint32_t LOCAL_SESSION_PORT = 5000;
+constexpr uint32_t LOCAL_AUTH_PORT = 6000;
+constexpr uint32_t LOCAL_PROXY_PORT = 7000;
+constexpr uint32_t LOCAL_CAPACITY = 3;
+constexpr int32_t MASTER_WEIGHT = 10;
+constexpr int32_t P2P_ROLE = 1;
 using namespace testing;
 class NetLedgerCommonTest : public testing::Test {
 public:
@@ -279,5 +291,148 @@ HWTEST_F(NetLedgerCommonTest, LNN_NET_LEDGER_Test_004, TestSize.Level1)
     SoftBusDumpBusCenterPrintInfo(DEFAULT_FD, nullptr);
     SoftBusDumpBusCenterPrintInfo(DEFAULT_FD, &nodeInfo);
     LnnDeinitNetLedger();
+}
+
+/*
+* @tc.name: LOCAL_LEDGER_Test_001
+* @tc.desc: lnn local key table test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerCommonTest, LOCAL_LEDGER_Test_001, TestSize.Level1)
+{
+    EXPECT_TRUE(LnnInitLocalLedger() == SOFTBUS_OK);
+    static InfoKey getLocalStringInfoKeyTable[] = {
+        STRING_KEY_HICE_VERSION,
+        STRING_KEY_DEV_UDID,
+        STRING_KEY_NETWORKID,
+        STRING_KEY_UUID,
+        STRING_KEY_DEV_TYPE,
+        STRING_KEY_DEV_NAME,
+        STRING_KEY_BT_MAC,
+        STRING_KEY_WLAN_IP,
+        STRING_KEY_NET_IF_NAME,
+        STRING_KEY_MASTER_NODE_UDID,
+        STRING_KEY_NODE_ADDR,
+        STRING_KEY_P2P_MAC,
+        STRING_KEY_P2P_GO_MAC,
+        STRING_KEY_OFFLINE_CODE
+    };
+    char buf[UDID_BUF_LEN] = {0};
+    int32_t ret;
+    uint32_t i;
+    LnnSetLocalStrInfo(STRING_KEY_DEV_UDID, LOCAL_UDID);
+    for (i = 0; i < sizeof(getLocalStringInfoKeyTable) / sizeof(InfoKey); i++) {
+        ret = LnnGetLocalStrInfo(getLocalStringInfoKeyTable[i], nullptr, UDID_BUF_LEN);
+        EXPECT_TRUE(ret == SOFTBUS_INVALID_PARAM);
+    }
+    for (i = 0; i < sizeof(getLocalStringInfoKeyTable) / sizeof(InfoKey); i++) {
+        (void)memset_s(buf, UDID_BUF_LEN, 0, UDID_BUF_LEN);
+        ret = LnnGetLocalStrInfo(getLocalStringInfoKeyTable[i], buf, UDID_BUF_LEN);
+        EXPECT_TRUE(ret == SOFTBUS_OK);
+    }
+    LnnDeinitLocalLedger();
+}
+
+/*
+* @tc.name: LOCAL_LEDGER_Test_002
+* @tc.desc: lnn local key table test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerCommonTest, LOCAL_LEDGER_Test_002, TestSize.Level1)
+{
+    EXPECT_TRUE(LnnInitLocalLedger() == SOFTBUS_OK);
+    int32_t ret = LnnSetLocalStrInfo(STRING_KEY_DEV_UDID, LOCAL_UDID);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_DEV_TYPE, LOCAL_DEVTYPE);
+    EXPECT_TRUE(ret == SOFTBUS_ERR);
+    ret = LnnSetLocalStrInfo(STRING_KEY_BT_MAC, LOCAL_BT_MAC);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_WLAN_IP, LOCAL_WLAN_IP);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_NET_IF_NAME, LOCAL_NET_IF_NAME);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_MASTER_NODE_UDID, MASTER_NODE_UDID);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_NODE_ADDR, LOCAL_NODE_ADDR);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_P2P_MAC, LOCAL_P2P_MAC);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalStrInfo(STRING_KEY_P2P_GO_MAC, LOCAL_GO_MAC);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    LnnDeinitLocalLedger();
+}
+
+/*
+* @tc.name: LOCAL_LEDGER_Test_003
+* @tc.desc: lnn local key table test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerCommonTest, LOCAL_LEDGER_Test_003, TestSize.Level1)
+{
+    EXPECT_TRUE(LnnInitLocalLedger() == SOFTBUS_OK);
+    static InfoKey getLocalNumInfoKeyTable[] = {
+        NUM_KEY_SESSION_PORT,
+        NUM_KEY_AUTH_PORT,
+        NUM_KEY_PROXY_PORT,
+        NUM_KEY_NET_CAP,
+        NUM_KEY_DISCOVERY_TYPE,
+        NUM_KEY_DEV_TYPE_ID,
+        NUM_KEY_MASTER_NODE_WEIGHT,
+        NUM_KEY_P2P_ROLE
+    };
+    int32_t ret, info;
+    LnnSetLocalNumInfo(NUM_KEY_AUTH_PORT, LOCAL_AUTH_PORT);
+    for (uint32_t i = 0; i < sizeof(getLocalNumInfoKeyTable) / sizeof(InfoKey); i++) {
+        info = 0;
+        ret = LnnGetLocalNumInfo(getLocalNumInfoKeyTable[i], &info);
+        EXPECT_TRUE(ret == SOFTBUS_OK);
+    }
+    LnnDeinitLocalLedger();
+}
+
+/*
+* @tc.name: LOCAL_LEDGER_Test_004
+* @tc.desc: lnn local key table test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerCommonTest, LOCAL_LEDGER_Test_004, TestSize.Level1)
+{
+    EXPECT_TRUE(LnnInitLocalLedger() == SOFTBUS_OK);
+    int32_t ret = LnnSetLocalNumInfo(NUM_KEY_SESSION_PORT, LOCAL_SESSION_PORT);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalNumInfo(NUM_KEY_AUTH_PORT, LOCAL_AUTH_PORT);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalNumInfo(NUM_KEY_PROXY_PORT, LOCAL_PROXY_PORT);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalNumInfo(NUM_KEY_NET_CAP, LOCAL_CAPACITY);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalNumInfo(NUM_KEY_MASTER_NODE_WEIGHT, MASTER_WEIGHT);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnSetLocalNumInfo(NUM_KEY_P2P_ROLE, P2P_ROLE);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    LnnDeinitLocalLedger();
+}
+
+/*
+* @tc.name: LOCAL_LEDGER_Test_005
+* @tc.desc: lnn local key table test
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(NetLedgerCommonTest, LOCAL_LEDGER_Test_005, TestSize.Level1)
+{
+    int64_t info;
+    EXPECT_TRUE(LnnInitLocalLedger() == SOFTBUS_OK);
+    int32_t ret = LnnGetLocalNum64Info(NUM_KEY_TRANS_PROTOCOLS, &info);
+    EXPECT_TRUE(ret == SOFTBUS_OK);
+    ret = LnnGetLocalDeviceInfo(nullptr);
+    EXPECT_TRUE(ret == SOFTBUS_INVALID_PARAM);
+    EXPECT_TRUE(LnnIsMasterNode() == true);
+    LnnDeinitLocalLedger();
+    LnnDeinitLocalLedger();
 }
 } // namespace OHOS
