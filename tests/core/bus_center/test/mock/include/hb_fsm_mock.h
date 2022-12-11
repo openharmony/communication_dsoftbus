fe7c2721fc2e3ac0d9b2f8dc608bf9ebb9834488
liangqi <liangqi22@huawei.com>
2022-12-11 16:30:57 +0800

    add heartbeat UT
    Signed-off-by: liangqi <liangqi22@huawei.com>

---
+++ tests/core/bus_center/test/mock/include/hb_fsm_mock.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HEARTBEAT_FSM_H
+#define HEARTBEAT_FSM_H
+
+#include "lnn_connection_addr_utils.h"
+#include "lnn_heartbeat_ctrl.h"
+#include "lnn_heartbeat_medium_mgr.h"
+#include "lnn_heartbeat_strategy.h"
+#include "lnn_net_builder.h"
+#include <gmock/gmock.h>
+#include <mutex>
+
+namespace OHOS {
+class HeartBeatFSMInterface {
+public:
+    HeartBeatFSMInterface() {};
+    virtual ~HeartBeatFSMInterface() {};
+
+    virtual int32_t LnnStartOfflineTimingStrategy(const char *networkId, ConnectionAddrType addrType) = 0;
+    virtual int32_t LnnStopOfflineTimingStrategy(const char *networkId, ConnectionAddrType addrType) = 0;
+    virtual int32_t LnnNotifyDiscoveryDevice(const ConnectionAddr *addr) = 0;
+    virtual int32_t LnnSetHbAsMasterNodeState(bool isMasterNode) = 0;
+    virtual int32_t LnnNotifyMasterElect(const char *networkId, const char *masterUdid, int32_t masterWeight) = 0;
+    virtual int32_t LnnStartHbByTypeAndStrategy(
+        LnnHeartbeatType hbType, LnnHeartbeatStrategyType strategyType, bool isRelay) = 0;
+    virtual int32_t LnnHbMediumMgrStop(LnnHeartbeatType *type) = 0;
+    virtual void LnnDumpHbMgrRecvList(void) = 0;
+    virtual void LnnDumpHbOnlineNodeList(void) = 0;
+    virtual bool LnnIsHeartbeatEnable(LnnHeartbeatType type) = 0;
+    virtual int32_t LnnGetGearModeBySpecificType(GearMode *mode, LnnHeartbeatType type) = 0;
+    virtual DiscoveryType LnnConvAddrTypeToDiscType(ConnectionAddrType type) = 0;
+    virtual int32_t LnnOfflineTimingByHeartbeat(const char *networkId, ConnectionAddrType addrType) = 0;
+    virtual int32_t LnnRequestLeaveSpecific(const char *networkId, ConnectionAddrType addrType) = 0;
+    virtual int32_t LnnHbMediumMgrSendBegin(LnnHeartbeatCustSendData *custData) = 0;
+    virtual int32_t LnnHbMediumMgrSendEnd(LnnHeartbeatType *type) = 0;
+    virtual int32_t LnnGetHbStrategyManager(
+        LnnHeartbeatStrategyManager *mgr, LnnHeartbeatType hbType, LnnHeartbeatStrategyType strategyType) = 0;
+    virtual int32_t LnnHbMediumMgrSetParam(const LnnHeartbeatMediumParam *param) = 0;
+    virtual int32_t LnnHbMediumMgrUpdateSendInfo(LnnHeartbeatUpdateInfoType type) = 0;
+};
+class HeartBeatFSMInterfaceMock : public HeartBeatFSMInterface {
+public:
+    HeartBeatFSMInterfaceMock();
+    ~HeartBeatFSMInterfaceMock() override;
+
+    MOCK_METHOD2(LnnStartOfflineTimingStrategy, int32_t(const char *, ConnectionAddrType));
+    MOCK_METHOD2(LnnStopOfflineTimingStrategy, int32_t(const char *, ConnectionAddrType));
+    MOCK_METHOD1(LnnNotifyDiscoveryDevice, int32_t(const ConnectionAddr *));
+    MOCK_METHOD3(LnnNotifyMasterElect, int32_t(const char *, const char *, int32_t));
+    MOCK_METHOD1(LnnSetHbAsMasterNodeState, int32_t(bool));
+    MOCK_METHOD3(LnnStartHbByTypeAndStrategy, int32_t(LnnHeartbeatType, LnnHeartbeatStrategyType, bool));
+    MOCK_METHOD1(LnnHbMediumMgrStop, int32_t(LnnHeartbeatType *));
+    MOCK_METHOD0(LnnDumpHbMgrRecvList, void(void));
+    MOCK_METHOD0(LnnDumpHbOnlineNodeList, void(void));
+    MOCK_METHOD1(LnnIsHeartbeatEnable, bool(LnnHeartbeatType));
+    MOCK_METHOD2(LnnGetGearModeBySpecificType, int32_t(GearMode *, LnnHeartbeatType));
+    MOCK_METHOD1(LnnConvAddrTypeToDiscType, DiscoveryType(ConnectionAddrType));
+    MOCK_METHOD2(LnnOfflineTimingByHeartbeat, int32_t(const char *, ConnectionAddrType));
+    MOCK_METHOD2(LnnRequestLeaveSpecific, int32_t(const char *, ConnectionAddrType));
+    MOCK_METHOD1(LnnHbMediumMgrSendBegin, int32_t(LnnHeartbeatCustSendData *));
+    MOCK_METHOD1(LnnHbMediumMgrSendEnd, int32_t(LnnHeartbeatType *));
+    MOCK_METHOD3(
+        LnnGetHbStrategyManager, int32_t(LnnHeartbeatStrategyManager *, LnnHeartbeatType, LnnHeartbeatStrategyType));
+    MOCK_METHOD1(LnnHbMediumMgrSetParam, int32_t(const LnnHeartbeatMediumParam *));
+    MOCK_METHOD1(LnnHbMediumMgrUpdateSendInfo, int32_t(LnnHeartbeatUpdateInfoType));
+};
+} // namespace OHOS
+#endif // AUTH_CONNECTION_MOCK_H
---
+++ tests/core/bus_center/test/mock/src/hb_strategy_mock.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hb_strategy_mock.h"
+#include "softbus_error_code.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+void *g_hbStrategyInterface;
+HeartBeatStategyInterfaceMock::HeartBeatStategyInterfaceMock()
+{
+    g_hbStrategyInterface = reinterpret_cast<void *>(this);
+}
+
+HeartBeatStategyInterfaceMock::~HeartBeatStategyInterfaceMock()
+{
+    g_hbStrategyInterface = nullptr;
+}
+
+static HeartBeatStategyInterface *HeartBeatStrategyInterface()
+{
+    return reinterpret_cast<HeartBeatStategyInterfaceMock *>(g_hbStrategyInterface);
+}
+
+extern "C" {
+int32_t LnnStartOfflineTimingStrategy(const char *networkId, ConnectionAddrType addrType)
+{
+    return HeartBeatStrategyInterface()->LnnStartOfflineTimingStrategy(networkId, addrType);
+}
+
+int32_t LnnStopOfflineTimingStrategy(const char *networkId, ConnectionAddrType addrType)
+{
+    return HeartBeatStrategyInterface()->LnnStopOfflineTimingStrategy(networkId, addrType);
+}
+
+int32_t LnnNotifyDiscoveryDevice(const ConnectionAddr *addr)
+{
+    return HeartBeatStrategyInterface()->LnnNotifyDiscoveryDevice(addr);
+}
+
+int32_t LnnNotifyMasterElect(const char *networkId, const char *masterUdid, int32_t masterWeight)
+{
+    return HeartBeatStrategyInterface()->LnnNotifyMasterElect(networkId, masterUdid, masterWeight);
+}
+
+int32_t LnnSetHbAsMasterNodeState(bool isMasterNode)
+{
+    return HeartBeatStrategyInterface()->LnnSetHbAsMasterNodeState(isMasterNode);
+}
+
+int32_t LnnStartHbByTypeAndStrategy(LnnHeartbeatType hbType, LnnHeartbeatStrategyType strategyType, bool isRelay)
+{
+    return HeartBeatStrategyInterface()->LnnStartHbByTypeAndStrategy(hbType, strategyType, isRelay);
+}
+}
+} // namespace OHOS
