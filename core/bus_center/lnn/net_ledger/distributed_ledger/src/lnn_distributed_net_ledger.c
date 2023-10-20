d8a922d01c0cefd46c34932d4e8bae404d9b6b2f
heyangbo <heyangbo1@huawei.com>
2023-10-20 18:22:34 +0800

    fix:solve capbility change problem
    Signed-off-by: heyangbo <heyangbo1@huawei.com>

--- core/bus_center/lnn/net_ledger/distributed_ledger/src/lnn_distributed_net_ledger.c
+++ core/bus_center/lnn/net_ledger/distributed_ledger/src/lnn_distributed_net_ledger.c
@@ -2310,6 +2310,11 @@ int32_t LnnSetDLConnCapability(const char *networkId, uint32_t connCapability)
         return SOFTBUS_ERR;
     }
     nodeInfo->netCapacity = connCapability;
+    if(LnnSaveRemoteDeviceInfo(nodeInfo) != SOFTBUS_OK) {
+        SoftBusLog(SOFTBUS_LOG_LNN, SOFTBUS_LOG_ERROR, "save remote netCapacity fail");
+        (void)SoftBusMutexUnlock(&g_distributedNetLedger.lock);
+        return SOFTBUS_ERR;
+    }
     (void)SoftBusMutexUnlock(&g_distributedNetLedger.lock);
     return SOFTBUS_OK;
 }
