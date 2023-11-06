e909c838caf4bc40ec976325c5d79bf8fe50b97b
huxianghao <huxianghao@huawei.com>
2023-11-06 15:52:34 +0000

    Modify the formatting
    Signed-off-by: huxianghao <huxianghao@huawei.com>

--- tests/core/discovery/coap/unittest/disc_nstackx_adapter_test.cpp
+++ tests/core/discovery/coap/unittest/disc_nstackx_adapter_test.cpp
@@ -477,8 +477,8 @@ HWTEST_F(DiscNstackxAdapterTest, TestDiscCoapAdapterParseDevInfo002, TestSize.Le
 
 /*
  * @tc.name: TestDiscCoapAdapterParseDevInfo003
- * @tc.desc: Test DiscParseDiscDevInfo should return SOFTBUS_ERR when given NSTACKX_DISCOVERY_TYPE_PASSIVE,
- *           should return SOFTBUS_OK when given NSTACKX_DISCOVERY_TYPE_ACTIVE NSTACKX_DeviceInfo.discoveryType,
+ * @tc.desc: Test DiscParseDiscDevInfo should return SOFTBUS_OK
+ *           when given NSTACKX_DISCOVERY_TYPE_ACTIVE NSTACKX_DeviceInfo.discoveryType,
  *           should return SOFTBUS_OK when given PUBLISH_MODE_PROACTIVE NSTACKX_DeviceInfo.mode
  * @tc.type: FUNC
  * @tc.require:
@@ -495,10 +495,6 @@ HWTEST_F(DiscNstackxAdapterTest, TestDiscCoapAdapterParseDevInfo003, TestSize.Le
         .discoveryType = NSTACKX_DISCOVERY_TYPE_PASSIVE,
     };
     DeviceInfo testDiscDevInfo;
-    ret = ParseDiscDevInfo(&testNstackxDevInfo, &testDiscDevInfo);
-    EXPECT_EQ(ret, SOFTBUS_ERR);
-
-    testNstackxDevInfo.discoveryType = NSTACKX_DISCOVERY_TYPE_ACTIVE;
     ret = ParseDiscDevInfo(&testNstackxDevInfo, &testDiscDevInfo);
     EXPECT_EQ(ret, SOFTBUS_OK);
 
