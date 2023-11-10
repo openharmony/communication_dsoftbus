043d4fd5e8102539c5eac122334d1d864ec89717
aqxyjay <zhangchunxin@huawei.com>
2023-11-10 17:29:27 +0800

    test: add module log tests
    Signed-off-by: aqxyjay <zhangchunxin@huawei.com>

--- core/common/dfx/interface/include/disc_log.h
+++ core/common/dfx/interface/include/disc_log.h
@@ -32,11 +32,11 @@ static const SoftBusLogLabel DISC_LABELS[MODULE_DOMAIN_MAX_LEN] = {
     { DISC_TEST, DOMAIN_ID_TEST, "DiscTest"},
 };
 
-#define DISC_LOGF(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_LOG_FATAL, DISC_LABELS[label], ##__VA_ARGS__)
-#define DISC_LOGE(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_LOG_ERROR, DISC_LABELS[label], ##__VA_ARGS__)
-#define DISC_LOGW(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_LOG_WARN, DISC_LABELS[label], ##__VA_ARGS__)
-#define DISC_LOGI(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_LOG_INFO, DISC_LABELS[label], ##__VA_ARGS__)
-#define DISC_LOGD(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_LOG_DEBUG, DISC_LABELS[label], ##__VA_ARGS__)
+#define DISC_LOGF(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_DFX_LOG_FATAL, DISC_LABELS[label], ##__VA_ARGS__)
+#define DISC_LOGE(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_DFX_LOG_ERROR, DISC_LABELS[label], ##__VA_ARGS__)
+#define DISC_LOGW(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_DFX_LOG_WARN, DISC_LABELS[label], ##__VA_ARGS__)
+#define DISC_LOGI(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_DFX_LOG_INFO, DISC_LABELS[label], ##__VA_ARGS__)
+#define DISC_LOGD(label, ...) (void)SOFTBUS_LOG_INNER(SOFTBUS_DFX_LOG_DEBUG, DISC_LABELS[label], ##__VA_ARGS__)
 
 #define DISC_CHECK_AND_RETURN_RET_LOGW(cond, ret, label, fmt, ...) \
     CHECK_AND_RETURN_RET_LOG_INNER(cond, ret, DISC_LOGW, label, fmt, ##__VA_ARGS__)
