f4ca869ddb9aca5eea425db1cd3deedf6387b157
liangjunhao <liangjunhao10@huawei.com>
2023-11-29 21:09:55 +0800

    fix: add some log
    Signed-off-by: liangjunhao <liangjunhao10@huawei.com>

--- sdk/transmission/session/src/client_trans_session_manager.c
+++ sdk/transmission/session/src/client_trans_session_manager.c
@@ -615,6 +615,7 @@ int32_t ClientDeleteSessionServer(SoftBusSecType type, const char *sessionName)
 
 int32_t ClientDeleteSession(int32_t sessionId)
 {
+    TRANS_LOGD(TRANS_SDK, "sessionId=%d", sessionId);
     if (sessionId < 0) {
         return SOFTBUS_ERR;
     }
