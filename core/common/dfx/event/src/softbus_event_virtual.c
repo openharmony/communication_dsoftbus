f5a7cb30297c28c4baf34ff96e118140aef704dd
aqxyjay <zhangchunxin@huawei.com>
2023-11-29 14:30:33 +0800

    build: event support liteos
    Signed-off-by: aqxyjay <zhangchunxin@huawei.com>

---
+++ core/common/dfx/event/src/softbus_event_virtual.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "conn_event.h"
+#include "disc_event.h"
+#include "lnn_event.h"
+#include "trans_event.h"
+
+void ConnEventInner(int32_t scene, int32_t stage, const char *func, int32_t line, ConnEventExtra *extra)
+{
+    (void)scene;
+    (void)stage;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void ConnAlarmInner(int32_t scene, const char *func, int32_t line, ConnAlarmExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void ConnStatsInner(int32_t scene, const char *func, int32_t line, ConnStatsExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void ConnAuditInner(int32_t scene, const char *func, int32_t line, ConnAuditExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void DiscEventInner(int32_t scene, int32_t stage, const char *func, int32_t line, DiscEventExtra *extra)
+{
+    (void)scene;
+    (void)stage;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void DiscAlarmInner(int32_t scene, const char *func, int32_t line, DiscAlarmExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void DiscStatsInner(int32_t scene, const char *func, int32_t line, DiscStatsExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void DiscAuditInner(int32_t scene, const char *func, int32_t line, DiscAuditExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void LnnEventInner(int32_t scene, int32_t stage, const char *func, int32_t line, LnnEventExtra *extra)
+{
+    (void)scene;
+    (void)stage;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void LnnAlarmInner(int32_t scene, const char *func, int32_t line, LnnAlarmExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void LnnStatsInner(int32_t scene, const char *func, int32_t line, LnnStatsExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void LnnAuditInner(int32_t scene, const char *func, int32_t line, LnnAuditExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void TransEventInner(int32_t scene, int32_t stage, const char *func, int32_t line, TransEventExtra *extra)
+{
+    (void)scene;
+    (void)stage;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void TransAlarmInner(int32_t scene, const char *func, int32_t line, TransAlarmExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void TransStatsInner(int32_t scene, const char *func, int32_t line, TransStatsExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
+
+void TransAuditInner(int32_t scene, const char *func, int32_t line, TransAuditExtra *extra)
+{
+    (void)scene;
+    (void)func;
+    (void)line;
+    (void)extra;
+}
