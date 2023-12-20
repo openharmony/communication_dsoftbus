576ccffb2bf4ee079f18cf31efe5f4e060254889
q30045852 <q30045852@notesmail.huawei.com/>
2023-12-20 12:08:19 +0800

    feat: Hidumper security ensure Signed-off-by: qinhao <qinhao43@huawei.com>

--- core/common/dfx/event/src/convert/softbus_event_converter.h
+++ core/common/dfx/event/src/convert/softbus_event_converter.h
@@ -27,8 +27,9 @@
 extern "C" {
 #endif
 
-#define INVALID_INT_VALUE          0
-#define PARAM_STRING_VALUE_MAX_LEN 256
+#define INVALID_INT_VALUE               0
+#define PARAM_STRING_VALUE_MAX_LEN      256
+#define PARAM_LONG_STRING_VALUE_MAX_LEN (256 * 1024)
 
 typedef struct {
     char name[MAX_LENGTH_OF_PARAM_NAME];
@@ -79,6 +80,17 @@ static inline bool AssignerString(const char *value, HiSysEventParam **param)
     }
     return InitString(&(*param)->v.s, PARAM_STRING_VALUE_MAX_LEN) &&
         CopyString((*param)->v.s, value, PARAM_STRING_VALUE_MAX_LEN);
+}
+
+/* Used by ASSIGNER macros */
+static inline bool AssignerLongString(const char *value, HiSysEventParam **param)
+{
+    if (value == NULL || strlen(value) == 0) {
+        (*param)->v.s = NULL;
+        return false;
+    }
+    return InitString(&(*param)->v.s, PARAM_LONG_STRING_VALUE_MAX_LEN) &&
+        CopyString((*param)->v.s, value, PARAM_LONG_STRING_VALUE_MAX_LEN);
 }
 
 /* Used by ASSIGNER macros */
