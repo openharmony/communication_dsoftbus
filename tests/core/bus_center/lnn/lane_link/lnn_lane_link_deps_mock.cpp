43c730cd6c3231681e40f8c74543908af52226b3
x30053509 <965446201@qq.com>
2023-12-29 09:36:02 +0800

    mdf
    Signed-off-by: x30053509 <965446201@qq.com>

--- tests/core/bus_center/lnn/lane_link/lnn_lane_link_deps_mock.cpp
+++ tests/core/bus_center/lnn/lane_link/lnn_lane_link_deps_mock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -38,7 +38,7 @@ static LaneLinkDepsInterface *GetLaneLinkDepsInterface()
 extern "C" {
 int32_t GetTransOptionByLaneId(uint32_t laneId, TransOption *reqInfo)
 {
-     return GetLaneLinkDepsInterface()->GetTransOptionByLaneId(laneId, reqInfo);
+    return GetLaneLinkDepsInterface()->GetTransOptionByLaneId(laneId, reqInfo);
 }
 }
 } // namespace OHOS
