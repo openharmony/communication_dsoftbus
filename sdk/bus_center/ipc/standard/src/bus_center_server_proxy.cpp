3202506d09369e662bc44fa71ed251f1cbaa0da1
fangdong7 <fangdong7@huawei.com>
2024-01-03 11:15:29 +0000

    fix:modify sdk death clear
    Signed-off-by: fangdong7 <fangdong7@huawei.com>

--- sdk/bus_center/ipc/standard/src/bus_center_server_proxy.cpp
+++ sdk/bus_center/ipc/standard/src/bus_center_server_proxy.cpp
@@ -81,6 +81,8 @@ int32_t BusCenterServerProxyInit(void)
 void BusCenterServerProxyDeInit(void)
 {
     LNN_LOGI(LNN_EVENT, "enter");
+    BusCenterExProxyDeInit();
+    std::lock_guard<std::mutex> lock(g_mutex);
     g_serverProxy.clear();
 }
 
