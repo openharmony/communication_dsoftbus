dca775b2f84069103b592c9bcf427a5d141dcbbb
范妮娜 <fannina2@huawei.com>
2024-01-05 09:20:49 +0000

    update tests/core/authentication/unittest/auth_other_test.cpp.
    Signed-off-by: 范妮娜 <fannina2@huawei.com>

--- tests/core/authentication/unittest/auth_other_test.cpp
+++ tests/core/authentication/unittest/auth_other_test.cpp
@@ -406,7 +406,7 @@ HWTEST_F(AuthOtherTest, AUTH_MANAGER_SET_AUTH_TIMEOUT_TEST_001, TestSize.Level1)
     int64_t authSeq = 0;
     AuthSessionInfo info;
     (void)memset_s(&info, sizeof(AuthSessionInfo), 0, sizeof(AuthSessionInfo));
-    info->connInfo.type = AUTH_LINK_TYPE_WIFI;
+    info.connInfo.type = AUTH_LINK_TYPE_WIFI;
     int32_t reason = SOFTBUS_AUTH_TIMEOUT;
     AuthManager *auth = NewAuthManager(authSeq, &info);
     EXPECT_TRUE(auth != nullptr);
