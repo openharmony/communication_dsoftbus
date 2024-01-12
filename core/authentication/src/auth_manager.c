e3f8202112568c74b0de05624512fc17de0c7a95
马昌友 <machangyou@huawei.com>
2024-01-12 11:03:03 +0800

    GetLatestIdByConnInfo添加auth是否认证通过判断逻辑
    Signed-off-by: 马昌友 <machangyou@huawei.com>

--- core/authentication/src/auth_manager.c
+++ core/authentication/src/auth_manager.c
@@ -461,7 +461,7 @@ static int64_t GetLatestIdByConnInfo(const AuthConnInfo *connInfo, AuthLinkType 
     int64_t latestAuthId = AUTH_INVALID_ID;
     uint64_t latestVerifyTime = 0;
     for (uint32_t i = 0; i < num; i++) {
-        if (auth[i] != NULL && auth[i]->lastVerifyTime > latestVerifyTime) {
+        if (auth[i] != NULL && auth[i]->lastVerifyTime > latestVerifyTime && auth[i]->hasAuthPassed) {
             latestAuthId = auth[i]->authId;
             latestVerifyTime = auth[i]->lastVerifyTime;
         }
