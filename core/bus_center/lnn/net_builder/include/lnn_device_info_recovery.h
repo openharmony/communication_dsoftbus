d7feffb4d4dd753a40b70c0ef69a038475382117
wanghaoxu <wanghaoxu1@huawei.com>
2024-01-14 10:15:08 +0000

    回退 'Pull Request !4992 : fix wifi no offline problem'

--- core/bus_center/lnn/net_builder/include/lnn_device_info_recovery.h
+++ core/bus_center/lnn/net_builder/include/lnn_device_info_recovery.h
@@ -28,7 +28,7 @@ int32_t LnnLoadRemoteDeviceInfo(void);
 int32_t LnnSaveLocalDeviceInfo(const NodeInfo *deviceInfo);
 int32_t LnnGetLocalDevInfo(NodeInfo *deviceInfo);
 int32_t LnnSaveRemoteDeviceInfo(const NodeInfo *deviceInfo);
-int32_t LnnUpdateRemoteDeviceInfo(NodeInfo *deviceInfo);
+int32_t LnnUpdateRemoteDeviceInfo(const NodeInfo *deviceInfo);
 int32_t LnnRetrieveDeviceInfo(const char *udid, NodeInfo *deviceInfo);
 NodeInfo *LnnRetrieveDeviceInfoByNetworkId(const char *networkId);
 void LnnDeleteDeviceInfo(const char *udid);
