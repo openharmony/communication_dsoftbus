b3cd0f631e772c18b2120a8b1c7a05f63f05d1f3
shufewhx <wanghaoxu1@huawei.com>
2024-01-25 03:51:03 +0000

    fixed 2c919f2 from https://gitee.com/shufewhx/communication_dsoftbus/pulls/5161 bug: delete repeat log
    Signed-off-by: shufewhx <wanghaoxu1@huawei.com>

--- adapter/common/kernel/posix/lnn_ip_utils_adapter.c
+++ adapter/common/kernel/posix/lnn_ip_utils_adapter.c
@@ -26,17 +26,14 @@
 static int32_t GetNetworkIfIp(int32_t fd, struct ifreq *req, char *ip, char *netmask, uint32_t len)
 {
     if (ioctl(fd, SIOCGIFFLAGS, (char *)req) < 0) {
-        COMM_LOGE(COMM_ADAPTER, "ioctl SIOCGIFFLAGS fail, errno=%{public}d", errno);
         return SOFTBUS_ERR;
     }
     if (!((uint16_t)req->ifr_flags & IFF_UP)) {
-        COMM_LOGE(COMM_ADAPTER, "interface is not up");
         return SOFTBUS_ERR;
     }
 
     /* get IP of this interface */
     if (ioctl(fd, SIOCGIFADDR, (char *)req) < 0) {
-        COMM_LOGE(COMM_ADAPTER, "ioctl SIOCGIFADDR fail, errno=%{public}d", errno);
         return SOFTBUS_ERR;
     }
     struct sockaddr_in *sockAddr = (struct sockaddr_in *)&(req->ifr_addr);
@@ -78,7 +75,6 @@ int32_t GetNetworkIpByIfName(const char *ifName, char *ip, char *netmask, uint32
         return SOFTBUS_ERR;
     }
     if (GetNetworkIfIp(fd, &ifr, ip, netmask, len) != SOFTBUS_OK) {
-        COMM_LOGE(COMM_ADAPTER, "GetNetworkIfIp fail. ifName=%{public}s", ifName);
         close(fd);
         return SOFTBUS_ERR;
     }
