79bb652e7625e9acd9d331e0c4bed4dcd05416c9
ma-haoyun <mahaoyun2@huawei.com>
2024-01-27 22:16:53 +0800

    add proxy get
    Signed-off-by: ma-haoyun <mahaoyun2@huawei.com>

--- sdk/bus_center/ipc/standard/include/bus_center_server_proxy_standard.h
+++ sdk/bus_center/ipc/standard/include/bus_center_server_proxy_standard.h
@@ -66,6 +66,8 @@ public:
     int32_t GetBusCenterExObj(sptr<IRemoteObject> &object) override;
     int32_t EvaluateQos(const char *peerNetworkId, TransDataType dataType, const QosTV *qos,
         uint32_t qosCount) override;
+    int32_t BusCenterServerProxyStandardInit(void);
+    void BusCenterServerProxyStandardDeInit(void);
 
 private:
     static inline BrokerDelegator<BusCenterServerProxy> delegator_;
