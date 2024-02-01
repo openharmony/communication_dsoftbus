d88073812b2c5170cdc9ebcd9c23076adf4a16d2
shufewhx <wanghaoxu1@huawei.com>
2024-02-01 08:05:40 +0000

    fixed 4ff2dc4 from https://gitee.com/shufewhx/communication_dsoftbus/pulls/5286 revert:fix:don't send adv when ondevicebound
    Signed-off-by: shufewhx <wanghaoxu1@huawei.com>

--- core/bus_center/lnn/lane_hub/heartbeat/src/lnn_heartbeat_ctrl_virtual.c
+++ core/bus_center/lnn/lane_hub/heartbeat/src/lnn_heartbeat_ctrl_virtual.c
@@ -58,13 +58,6 @@ void LnnUpdateHeartbeatInfo(LnnHeartbeatUpdateInfoType type)
     LNN_LOGI(LNN_HEART_BEAT, "heartbeat stub update send info");
 }
 
-void LnnHbOnTrustedRelationChanged(int32_t groupType)
-{
-    (void)groupType;
-
-    LNN_LOGI(LNN_HEART_BEAT, "heartbeat stub process auth OnDeviceBound");
-}
-
 void LnnHbOnTrustedRelationIncreased(int32_t groupType)
 {
     (void)groupType;
