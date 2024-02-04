aa49507b065104196d22a80c0f43543fe6dbba5b
shufewhx <wanghaoxu1@huawei.com>
2024-02-04 14:52:57 +0000

    fixed 9a8893c from https://gitee.com/shufewhx/communication_dsoftbus/pulls/5312 fix: recovery ptk before remotedeviceinfo
    Signed-off-by: shufewhx <wanghaoxu1@huawei.com>

--- core/bus_center/lnn/net_ledger/common/src/lnn_net_ledger.c
+++ core/bus_center/lnn/net_ledger/common/src/lnn_net_ledger.c
@@ -103,12 +103,12 @@ static void LnnRestoreLocalDeviceInfo()
         LnnNotifyNetworkIdChangeEvent(info.networkId);
     }
     AuthLoadDeviceKey();
+    LnnLoadPtkInfo();
     if (LnnLoadRemoteDeviceInfo() != SOFTBUS_OK) {
         LNN_LOGE(LNN_LEDGER, "load remote deviceInfo fail");
         return;
     }
     LoadBleBroadcastKey();
-    LnnLoadPtkInfo();
     LnnLoadLocalBroadcastCipherKey();
     LNN_LOGI(LNN_LEDGER, "load remote deviceInfo devicekey success");
 }
