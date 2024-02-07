81c0374a1875b02ab12198b3b2c9d97be8e0ce21
y30033032 <yuanmeng36@huawei.com>
2024-02-07 14:59:55 +0800

    bug fix: Lnn and auth tdd bug fix  Signed-off-by: y30033032 <yuanmeng36@huawei.com>

--- tests/core/authentication/unittest/auth_common_mock.h
+++ tests/core/authentication/unittest/auth_common_mock.h
@@ -44,7 +44,8 @@ public:
     virtual int32_t LnnEncryptAesGcm(AesGcmInputParam *in, int32_t keyIndex, uint8_t **out, uint32_t *outLen) = 0;
     virtual int32_t LnnDecryptAesGcm(AesGcmInputParam *in, uint8_t **out, uint32_t *outLen) = 0;
     virtual int32_t LnnGetTrustedDevInfoFromDb(char **udidArray, uint32_t *num) = 0;
-    virtual int32_t GetActiveOsAccountIds(void) = 0;
+    virtual int32_t LnnGetAllOnlineNodeNum(int32_t *nodeNum) = 0;
+    virtual int32_t LnnSetLocalStrInfo(InfoKey key, const char *info) = 0;
 };
 class AuthCommonInterfaceMock : public AuthCommonInterface {
 public:
@@ -60,7 +61,8 @@ public:
     MOCK_METHOD4(LnnEncryptAesGcm, int32_t (AesGcmInputParam *, int32_t, uint8_t **, uint32_t *));
     MOCK_METHOD3(LnnDecryptAesGcm, int32_t (AesGcmInputParam *, uint8_t **, uint32_t *));
     MOCK_METHOD2(LnnGetTrustedDevInfoFromDb, int32_t (char **, uint32_t *));
-    MOCK_METHOD0(GetActiveOsAccountIds, int32_t (void));
+    MOCK_METHOD1(LnnGetAllOnlineNodeNum, int32_t (int32_t *));
+    MOCK_METHOD2(LnnSetLocalStrInfo, int32_t (InfoKey, const char *));
 };
 } // namespace OHOS
 #endif // AUTH_COMMON_MOCK_H
--- tests/core/bus_center/test/mock/include/hb_fsm_strategy_mock.h
+++ tests/core/bus_center/test/mock/include/hb_fsm_strategy_mock.h
@@ -16,9 +16,13 @@
 #ifndef HEARTBEAT_FSM_STRATEGY_H
 #define HEARTBEAT_FSM_STRATEGY_H
 
+#include "bus_center_manager.h"
+#include "lnn_feature_capability.h"
 #include "lnn_heartbeat_fsm.h"
 #include "lnn_heartbeat_ctrl.h"
 #include "lnn_heartbeat_utils.h"
+#include "lnn_heartbeat_medium_mgr.h"
+#include "lnn_state_machine.h"
 #include <gmock/gmock.h>
 #include <mutex>
 
@@ -35,6 +39,33 @@ public:
     virtual SoftBusScreenState GetScreenState(void) = 0;
     virtual bool LnnCheckSupportedHbType(LnnHeartbeatType *srcType, LnnHeartbeatType *dstType) = 0;
     virtual int32_t LnnPostStartMsgToHbFsm(LnnHeartbeatFsm *hbFsm, uint64_t delayMillis) = 0;
+    virtual bool LnnVisitHbTypeSet(VisitHbTypeCb callback, LnnHeartbeatType *typeSet, void *data) = 0;
+    virtual LnnHeartbeatType LnnConvertConnAddrTypeToHbType(ConnectionAddrType addrType) = 0;
+    virtual int32_t LnnConvertHbTypeToId(LnnHeartbeatType type) = 0;
+    virtual bool LnnIsSupportBurstFeature(const char *networkId) = 0;
+    virtual int32_t LnnPostSetMediumParamMsgToHbFsm(LnnHeartbeatFsm *hbFsm,
+        const LnnHeartbeatMediumParam *para) = 0;
+    virtual int32_t LnnPostCheckDevStatusMsgToHbFsm(LnnHeartbeatFsm *hbFsm,
+        const LnnCheckDevStatusMsgPara *para, uint64_t delayMillis) = 0;
+    virtual int32_t LnnPostScreenOffCheckDevMsgToHbFsm(LnnHeartbeatFsm *hbFsm,
+        const LnnCheckDevStatusMsgPara *para, uint64_t delayMillis) = 0;
+    virtual LnnHeartbeatFsm *LnnCreateHeartbeatFsm(void) = 0;
+    virtual int32_t LnnStartHeartbeatFsm(LnnHeartbeatFsm *hbFsm) = 0;
+    virtual void LnnRemoveScreenOffCheckStatusMsg(LnnHeartbeatFsm *hbFsm, LnnCheckDevStatusMsgPara *msgPara) = 0;
+    virtual void LnnRemoveCheckDevStatusMsg(LnnHeartbeatFsm *hbFsm, LnnCheckDevStatusMsgPara *msgPara) = 0;
+    virtual int32_t LnnPostStopMsgToHbFsm(LnnHeartbeatFsm *hbFsm, LnnHeartbeatType type) = 0;
+    virtual int32_t LnnStopHeartbeatFsm(LnnHeartbeatFsm *hbFsm) = 0;
+    virtual void LnnRemoveSendEndMsg(LnnHeartbeatFsm *hbFsm, LnnHeartbeatType type,
+        bool wakeupFlag, bool isRelay, bool *isRemoved) = 0;
+    virtual int32_t LnnPostNextSendOnceMsgToHbFsm(LnnHeartbeatFsm *hbFsm,
+        const LnnProcessSendOnceMsgPara *para, uint64_t delayMillis) = 0;
+    virtual void LnnRemoveProcessSendOnceMsg(LnnHeartbeatFsm *hbFsm,
+        LnnHeartbeatType hbType, LnnHeartbeatStrategyType strategyType) = 0;
+    virtual void LnnHbClearRecvList(void) = 0;
+    virtual int32_t LnnFsmRemoveMessage(FsmStateMachine *fsm, int32_t msgType) = 0;
+    virtual int32_t LnnGetLocalNumU64Info(InfoKey key, uint64_t *info) = 0;
+    virtual int32_t LnnGetRemoteNumU64Info(const char *networkId, InfoKey key, uint64_t *info) = 0;
+    virtual bool IsFeatureSupport(uint64_t feature, FeatureCapability capaBit);
 };
 class HeartBeatFSMStrategyInterfaceMock : public HeartBeatFSMStrategyInterface {
 public:
@@ -47,6 +78,31 @@ public:
     MOCK_METHOD0(GetScreenState, SoftBusScreenState(void));
     MOCK_METHOD2(LnnCheckSupportedHbType, bool(LnnHeartbeatType *, LnnHeartbeatType *));
     MOCK_METHOD2(LnnPostStartMsgToHbFsm, int32_t(LnnHeartbeatFsm *, uint64_t));
+    MOCK_METHOD3(LnnVisitHbTypeSet, bool (VisitHbTypeCb, LnnHeartbeatType *, void *));
+    MOCK_METHOD1(LnnConvertConnAddrTypeToHbType, LnnHeartbeatType (ConnectionAddrType));
+    MOCK_METHOD1(LnnConvertHbTypeToId, int32_t (LnnHeartbeatType));
+    MOCK_METHOD1(LnnIsSupportBurstFeature, bool (const char *));
+    MOCK_METHOD2(LnnPostSetMediumParamMsgToHbFsm, int32_t (LnnHeartbeatFsm *, const LnnHeartbeatMediumParam *));
+    MOCK_METHOD3(LnnPostCheckDevStatusMsgToHbFsm, int32_t (LnnHeartbeatFsm *,
+        const LnnCheckDevStatusMsgPara *, uint64_t));
+    MOCK_METHOD3(LnnPostScreenOffCheckDevMsgToHbFsm, int32_t (LnnHeartbeatFsm *,
+        const LnnCheckDevStatusMsgPara *, uint64_t));
+    MOCK_METHOD0(LnnCreateHeartbeatFsm, LnnHeartbeatFsm * ());
+    MOCK_METHOD1(LnnStartHeartbeatFsm, int32_t (LnnHeartbeatFsm *));
+    MOCK_METHOD2(LnnRemoveScreenOffCheckStatusMsg, void (LnnHeartbeatFsm *, LnnCheckDevStatusMsgPara *));
+    MOCK_METHOD2(LnnRemoveCheckDevStatusMsg, void (LnnHeartbeatFsm *, LnnCheckDevStatusMsgPara *));
+    MOCK_METHOD2(LnnPostStopMsgToHbFsm, int32_t (LnnHeartbeatFsm *, LnnHeartbeatType));
+    MOCK_METHOD1(LnnStopHeartbeatFsm, int32_t (LnnHeartbeatFsm *));
+    MOCK_METHOD5(LnnRemoveSendEndMsg, void (LnnHeartbeatFsm *, LnnHeartbeatType, bool, bool, bool *));
+    MOCK_METHOD3(LnnPostNextSendOnceMsgToHbFsm, int32_t (LnnHeartbeatFsm *,
+        const LnnProcessSendOnceMsgPara *, uint64_t));
+    MOCK_METHOD3(LnnRemoveProcessSendOnceMsg, void (LnnHeartbeatFsm *,
+        LnnHeartbeatType, LnnHeartbeatStrategyType));
+    MOCK_METHOD0(LnnHbClearRecvList, void ());
+    MOCK_METHOD2(LnnFsmRemoveMessage, int32_t (FsmStateMachine *, int32_t));
+    MOCK_METHOD2(LnnGetLocalNumU64Info, int32_t (InfoKey, uint64_t *));
+    MOCK_METHOD3(LnnGetRemoteNumU64Info, int32_t (const char *, InfoKey, uint64_t *));
+    MOCK_METHOD2(IsFeatureSupport, bool (uint64_t, FeatureCapability));
 };
 } // namespace OHOS
 #endif // HEARTBEAT_FSM_STRATEGY_H
