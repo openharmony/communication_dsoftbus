77b838715bc4720b3385d57fdc197d6b6a31ff93
cuile4 <cuile4@huawei.com>
2024-02-18 18:43:28 +0800

    [Bug]: Log repetition rate
    Change-Id: Ie7619699035710fa99378d5001505cb9bfeb1c11
    Signed-off-by: cuile4 <cuile4@huawei.com>

--- sdk/transmission/session/src/client_trans_session_callback.c
+++ sdk/transmission/session/src/client_trans_session_callback.c
@@ -331,7 +331,8 @@ NO_SANITIZE("cfi") int32_t TransOnQosEvent(int32_t channelId, int32_t channelTyp
         return ret;
     }
     if (listener.OnQosEvent == NULL) {
-        TRANS_LOGE(TRANS_QOS, "listener OnQosEvent is NULL");
+        TRANS_LOGE(TRANS_QOS, "listener OnQosEvent is NULL, channelId=%{public}d, sessionId=%{public}d",
+                   channelId, sessionId);
         return SOFTBUS_ERR;
     }
     listener.OnQosEvent(sessionId, eventId, tvCount, tvList);
