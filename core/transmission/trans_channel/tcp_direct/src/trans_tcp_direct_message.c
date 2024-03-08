e7d806c617262154c11b7927f8d44f4783eb26eb
erliangqi <zhangjiaxiang7@huawei.com>
2024-03-08 11:31:39 +0800

    The return value added on the GetWifiDirectManager interface is null
    Signed-off-by: erliangqi <zhangjiaxiang7@huawei.com>

--- core/transmission/trans_channel/tcp_direct/src/trans_tcp_direct_message.c
+++ core/transmission/trans_channel/tcp_direct/src/trans_tcp_direct_message.c
@@ -333,10 +333,13 @@ static int32_t GetServerSideIpInfo(SessionConn *conn, char *ip, uint32_t len)
             TRANS_LOGW(TRANS_CTRL, "ServerSide wifi set peer ip fail");
         }
     } else if (conn->appInfo.routeType == WIFI_P2P) {
-        if (GetWifiDirectManager()->getLocalIpByRemoteIp(conn->appInfo.peerData.addr, myIp, sizeof(myIp)) !=
-            SOFTBUS_OK) {
-            TRANS_LOGE(TRANS_CTRL, "NotifyChannelOpened get p2p ip fail");
-            return SOFTBUS_TRANS_GET_P2P_INFO_FAILED;
+        struct WifiDirectManager *mgr = GetWifiDirectManager();
+        if (mgr != NULL && mgr->getLocalIpByRemoteIp != NULL) {
+            int32_t ret = mgr->getLocalIpByRemoteIp(conn->appInfo.peerData.addr, myIp, sizeof(myIp));
+            if (ret != SOFTBUS_OK) {
+                TRANS_LOGE(TRANS_CTRL, "get Local Ip fail, ret = %{public}d", ret);
+                return SOFTBUS_TRANS_GET_P2P_INFO_FAILED;
+            }
         }
         if (LnnSetLocalStrInfo(STRING_KEY_P2P_IP, myIp) != SOFTBUS_OK) {
             TRANS_LOGW(TRANS_CTRL, "ServerSide set local p2p ip fail");
@@ -369,10 +372,13 @@ static int32_t GetClientSideIpInfo(SessionConn *conn, char *ip, uint32_t len)
             TRANS_LOGW(TRANS_CTRL, "Client wifi set peer ip fail");
         }
     } else if (conn->appInfo.routeType == WIFI_P2P) {
-        if (GetWifiDirectManager()->getLocalIpByRemoteIp(conn->appInfo.peerData.addr, myIp, sizeof(myIp)) !=
-            SOFTBUS_OK) {
-            TRANS_LOGE(TRANS_CTRL, "NotifyChannelOpened get p2p ip fail");
-            return SOFTBUS_TRANS_GET_P2P_INFO_FAILED;
+        struct WifiDirectManager *mgr = GetWifiDirectManager();
+        if (mgr != NULL && mgr->getLocalIpByRemoteIp != NULL) {
+            int32_t ret = mgr->getLocalIpByRemoteIp(conn->appInfo.peerData.addr, myIp, sizeof(myIp));
+            if (ret != SOFTBUS_OK) {
+                TRANS_LOGE(TRANS_CTRL, "get Local Ip fail, ret = %{public}d", ret);
+                return SOFTBUS_TRANS_GET_P2P_INFO_FAILED;
+            }
         }
         if (LnnSetLocalStrInfo(STRING_KEY_P2P_IP, myIp) != SOFTBUS_OK) {
             TRANS_LOGW(TRANS_CTRL, "Client set local p2p ip fail");
@@ -959,14 +965,19 @@ static int64_t GetAuthIdByChannelInfo(int32_t channelId, uint64_t seq, uint32_t 
     }
     bool fromAuthServer = ((seq & AUTH_CONN_SERVER_SIDE) != 0);
     char uuid[UUID_BUF_LEN] = {0};
-    if (GetWifiDirectManager()->getRemoteUuidByIp(appInfo.peerData.addr, uuid, sizeof(uuid)) != SOFTBUS_OK) {
-        AuthConnInfo connInfo;
-        connInfo.type = AUTH_LINK_TYPE_WIFI;
-        if (strcpy_s(connInfo.info.ipInfo.ip, IP_LEN, appInfo.peerData.addr) != EOK) {
-            TRANS_LOGE(TRANS_CTRL, "copy ip addr fail");
-            return AUTH_INVALID_ID;
+    struct WifiDirectManager *mgr = GetWifiDirectManager();
+    if (mgr != NULL && mgr->getRemoteUuidByIp != NULL) {
+        int32_t ret = mgr->getRemoteUuidByIp(appInfo.peerData.addr, uuid, sizeof(uuid));
+        if (ret != SOFTBUS_OK) {
+            AuthConnInfo connInfo;
+            connInfo.type = AUTH_LINK_TYPE_WIFI;
+            if (strcpy_s(connInfo.info.ipInfo.ip, IP_LEN, appInfo.peerData.addr) != EOK) {
+                TRANS_LOGE(TRANS_CTRL, "copy ip addr fail");
+                return AUTH_INVALID_ID;
+            }
+            TRANS_LOGE(TRANS_CTRL, "get Local Ip fail, ret = %{public}d", ret);
+            return AuthGetIdByConnInfo(&connInfo, !fromAuthServer, false);
         }
-        return AuthGetIdByConnInfo(&connInfo, !fromAuthServer, false);
     }
 
     AuthLinkType linkType = SwitchCipherTypeToAuthLinkType(cipherFlag);
