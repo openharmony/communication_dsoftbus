054de57befeaa59a42128d38e812b82ff610bd15
范映欢 <fanyinghuan5@h-partners.com>
2024-03-15 02:18:28 +0000

    update core/bus_center/lnn/lane_hub/lane_manager/src/lnn_lane_link_p2p_virtual.c.
    Signed-off-by: 范映欢 <fanyinghuan5@h-partners.com>

--- core/bus_center/lnn/lane_hub/lane_manager/src/lnn_lane_link_p2p_virtual.c
+++ core/bus_center/lnn/lane_hub/lane_manager/src/lnn_lane_link_p2p_virtual.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,7 +24,7 @@ void LnnDisconnectP2p(const char *networkId, int32_t pid, uint32_t laneLinkReqId
 {
     return;
 }
-void LnnDestroyP2p(void);
+void LnnDestroyP2p(void)
 {
     return;
 }
