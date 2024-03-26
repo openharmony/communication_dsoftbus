e0cc2565458593015fad66ccc1663c4bb5442ec1
huobaobao <huobaobao@huawei.com>
2024-03-26 17:59:21 +0800

    Ignore address when checking for available ble connections
    Signed-off-by: huobaobao <huobaobao@huawei.com>

--- core/connection/ble/src/softbus_conn_ble_manager.c
+++ core/connection/ble/src/softbus_conn_ble_manager.c
@@ -81,7 +81,7 @@ enum BleConnectionCompareType {
     BLE_CONNECTION_COMPARE_TYPE_CONNECTION_ID,
     BLE_CONNECTION_COMPARE_TYPE_ADDRESS,
     BLE_CONNECTION_COMPARE_TYPE_UNDERLAY_HANDLE,
-    BLE_CONNECTION_COMPARE_TYPE_UDID_DIFF_ADDRESS,
+    BLE_CONNECTION_COMPARE_TYPE_UDID_IGNORE_ADDRESS,
     BLE_CONNECTION_COMPARE_TYPE_UDID_CLIENT_SIDE,
 };
 
@@ -1237,11 +1237,13 @@ static bool ConnectionCompareByUnderlayHandle(ConnBleConnection *connection, con
                 connection->protocol == (BleProtocolType)option->underlayerHandleOption.protocol);
 }
 
-static bool ConnectionCompareByUdidDiffAddress(ConnBleConnection *connection, const BleConnectionCompareOption *option)
+static bool ConnectionCompareByUdidIgnoreAddress(ConnBleConnection *connection, const BleConnectionCompareOption *option)
 {
     ConnBleInnerComplementDeviceId(connection);
-    return StrCmpIgnoreCase(connection->addr, option->udidAddressOption.addr) != 0 &&
-        IsSameDevice(connection->udid, option->udidAddressOption.udid) &&
+    if (StrCmpIgnoreCase(connection->addr, option->udidAddressOption.addr) != 0) {
+        CONN_LOGD(CONN_BLE, "ble mac address are different. connectionId=%{public}u", connection->connectionId);
+    }
+    return IsSameDevice(connection->udid, option->udidAddressOption.udid) &&
         ((BleProtocolType)option->udidAddressOption.protocol == BLE_PROTOCOL_ANY ?
                 true :
                 connection->protocol == (BleProtocolType)option->udidAddressOption.protocol);
@@ -1269,8 +1271,8 @@ static ConnBleConnection *GetConnectionByOption(const BleConnectionCompareOption
         case BLE_CONNECTION_COMPARE_TYPE_UNDERLAY_HANDLE:
             compareFunc = ConnectionCompareByUnderlayHandle;
             break;
-        case BLE_CONNECTION_COMPARE_TYPE_UDID_DIFF_ADDRESS:
-            compareFunc = ConnectionCompareByUdidDiffAddress;
+        case BLE_CONNECTION_COMPARE_TYPE_UDID_IGNORE_ADDRESS:
+            compareFunc = ConnectionCompareByUdidIgnoreAddress;
             break;
         case BLE_CONNECTION_COMPARE_TYPE_UDID_CLIENT_SIDE:
             compareFunc = ConnectionCompareByUdidClientSide;
@@ -1344,7 +1346,7 @@ ConnBleConnection *ConnBleGetConnectionByHandle(int32_t underlayerHandle, ConnSi
 ConnBleConnection *ConnBleGetConnectionByUdid(const char *addr, const char *udid, BleProtocolType protocol)
 {
     BleConnectionCompareOption option = {
-        .type = BLE_CONNECTION_COMPARE_TYPE_UDID_DIFF_ADDRESS,
+        .type = BLE_CONNECTION_COMPARE_TYPE_UDID_IGNORE_ADDRESS,
         .udidAddressOption = {
             .addr = addr,
             .udid = udid,
