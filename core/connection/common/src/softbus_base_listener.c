6c79974987a49a64ec0a1340cd232152e622247a
黄凯 <huangkai71@huawei.com>
2024-04-03 03:26:23 +0000

    update core/connection/common/src/softbus_base_listener.c.
    Signed-off-by: 黄凯 <huangkai71@huawei.com>

--- core/connection/common/src/softbus_base_listener.c
+++ core/connection/common/src/softbus_base_listener.c
@@ -208,7 +208,6 @@ static SoftbusListenerNode *CreateSpecifiedListenerModule(ListenerModule module)
 
 int32_t InitBaseListener(void)
 {
-    // This function is invoked by both the client and server.
     // flag : if the client and server are in the same process, this function can be executed only once.
     static bool flag = false;
     if (flag) {
