329ad9bf515f6d0b6115437a42e5362d6e4b3176
songzhengteng <songzhengteng@huawei.com>
2024-04-18 19:40:48 +0800

    refactor: optimize p2p inner link with cxx
    Signed-off-by: songzhengteng <songzhengteng@huawei.com>

---
+++ tests/core/connection/wifi_direct_cpp/data/link_manager_test.cpp
@@ -0,0 +1,159 @@
+/*
+* Copyright (c) 2024 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#include <gtest/gtest.h>
+
+#include <set>
+
+#include "data/link_manager.h"
+
+using namespace testing::ext;
+
+namespace OHOS::SoftBus {
+class LinkManagerTest : public testing::Test {
+public:
+   static void SetUpTestCase() { }
+   static void TearDownTestCase() { }
+   void SetUp() override { }
+   void TearDown() override { }
+};
+
+/*
+ * @tc.name: AllocateLinkId
+ * @tc.desc: check AllocateLinkId method
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(LinkManagerTest, AllocateLinkId, TestSize.Level1)
+{
+    std::set<int> ids;
+    for (int i = 0; i < 100; ++i) {
+        auto id = LinkManager::GetInstance().AllocateLinkId();
+        EXPECT_TRUE(ids.find(id) == ids.end());
+        ids.insert(id);
+    }
+
+}
+
+/*
+ * @tc.name: ProcessIfXXXByRemoteDeviceId
+ * @tc.desc: check ProcessIfAbsent and ProcessIfPresent by remote device id
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(LinkManagerTest, ProcessIfXXXByRemoteDeviceId, TestSize.Level1)
+{
+    std::string remoteDeviceId("7001005458323933328a013ce3153800");
+
+    bool execute = false;
+    auto result = LinkManager::GetInstance().ProcessIfPresent(InnerLink::LinkType::HML, remoteDeviceId, [&execute](InnerLink &innerLink){
+        execute = true;
+    });
+    EXPECT_FALSE(execute);
+    EXPECT_FALSE(result);
+
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfAbsent(InnerLink::LinkType::HML, remoteDeviceId, [&execute, remoteDeviceId](InnerLink &innerLink){
+        innerLink.SetRemoteDeviceId(remoteDeviceId);
+        execute = true;
+    });
+    EXPECT_TRUE(execute);
+    EXPECT_TRUE(result);
+
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfPresent(InnerLink::LinkType::HML, remoteDeviceId, [&execute](InnerLink &innerLink){
+        execute = true;
+    });
+    EXPECT_TRUE(execute);
+    EXPECT_TRUE(result);
+
+    auto innerLink = LinkManager::GetInstance().GetReuseLink(WIFI_DIRECT_CONNECT_TYPE_BLE_TRIGGER_HML, remoteDeviceId);
+    EXPECT_NE(innerLink, nullptr);
+
+    LinkManager::GetInstance().RemoveLink(InnerLink::LinkType::HML, remoteDeviceId);
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfPresent(InnerLink::LinkType::HML, remoteDeviceId, [&execute](InnerLink &innerLink){
+        execute = true;
+    });
+    EXPECT_FALSE(execute);
+    EXPECT_FALSE(result);
+
+    innerLink = LinkManager::GetInstance().GetReuseLink(WIFI_DIRECT_CONNECT_TYPE_BLE_TRIGGER_HML, remoteDeviceId);
+    EXPECT_EQ(innerLink, nullptr);
+}
+
+/*
+ * @tc.name: ProcessIfXXXByRemoteMac
+ * @tc.desc: check ProcessIfAbsent and ProcessIfPresent by remote mac
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(LinkManagerTest, ProcessIfXXXByRemoteMac, TestSize.Level1)
+{
+    std::string remoteMac("11:22:33:44:55:66");
+    bool execute = false;
+    bool result = LinkManager::GetInstance().ProcessIfPresent(remoteMac, [&execute](InnerLink &innerLink){
+        execute = true;
+    });
+    EXPECT_FALSE(execute);
+    EXPECT_FALSE(result);
+
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfAbsent(remoteMac, [&execute, remoteMac](InnerLink &innerLink){
+        innerLink.SetRemoteBaseMac(remoteMac);
+        execute = true;
+    });
+    EXPECT_TRUE(execute);
+    EXPECT_TRUE(result);
+
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfPresent(remoteMac, [&execute](InnerLink &innerLink){
+        execute = true;
+    });
+    EXPECT_TRUE(execute);
+    EXPECT_TRUE(result);
+
+    WifiDirectLink link{};
+    std::string localIp("192.168.0.1");
+    std::string remoteIp("192.168.0.2");
+    LinkManager::GetInstance().ProcessIfPresent(remoteMac, [localIp, remoteIp, &link](InnerLink &innerLink){
+        innerLink.SetLocalIpv4(localIp);
+        innerLink.SetRemoteIpv4(remoteIp);
+        innerLink.SetLinkType(InnerLink::LinkType::HML);
+
+        innerLink.GenerateLink(888, 666, link);
+    });
+    EXPECT_NE(link.linkId, 0);
+    EXPECT_EQ(link.localIp, localIp);
+    EXPECT_EQ(link.remoteIp, remoteIp);
+    EXPECT_EQ(link.linkType, WIFI_DIRECT_LINK_TYPE_HML);
+
+    execute = false;
+    result = LinkManager::GetInstance().ProcessIfPresent(link.linkId, [&execute](InnerLink &innerLink) {
+        execute = true;
+    });
+    EXPECT_TRUE(execute);
+    EXPECT_TRUE(result);
+
+    auto innerLink = LinkManager::GetInstance().GetLinkById(link.linkId);
+    EXPECT_NE(innerLink, nullptr);
+
+    LinkManager::GetInstance().ProcessIfPresent(link.linkId, [link](InnerLink &innerLink) {
+        innerLink.RemoveId(link.linkId);
+    });
+    innerLink = LinkManager::GetInstance().GetLinkById(link.linkId);
+    EXPECT_EQ(innerLink, nullptr);
+}
+
+}
