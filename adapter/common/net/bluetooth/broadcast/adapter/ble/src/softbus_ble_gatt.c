a48c62b4862e1d6549f7c6f60f286d61733849ab
guowen666 <guowen31@huawei.com>
2024-05-11 11:32:00 +0800

    change advId when stop adv
    Signed-off-by: guowen666 <guowen31@huawei.com>

--- adapter/common/net/bluetooth/broadcast/adapter/ble/src/softbus_ble_gatt.c
+++ adapter/common/net/bluetooth/broadcast/adapter/ble/src/softbus_ble_gatt.c
@@ -509,7 +509,7 @@ static int32_t StartAdv(int32_t advId, const SoftbusBroadcastParam *param, const
         DISC_LOGE(DISC_BLE_ADAPTER, "lock failed, advId=%{public}d, btAdvId=%{public}d", advId, btAdvId);
         return SOFTBUS_LOCK_ERR;
     }
-    g_advChannel[advId].advId = btAdvId;
+    g_advChannel[advId].advId = (ret == SOFTBUS_OK) ? btAdvId : -1;
     g_advChannel[advId].isAdvertising = (ret == SOFTBUS_OK);
     SoftBusMutexUnlock(&g_advLock);
     if (ret != SOFTBUS_OK) {
