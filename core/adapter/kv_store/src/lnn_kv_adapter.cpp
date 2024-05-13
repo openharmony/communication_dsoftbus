fab6cc29bb188de73ca6cef36231432f3397cdb7
wzhhhh <wangzhaohao@huawei.com>
2024-05-13 08:47:22 +0000

    update core/adapter/kv_store/src/lnn_kv_adapter.cpp.
    Signed-off-by: wzhhhh <wangzhaohao@huawei.com>

--- core/adapter/kv_store/src/lnn_kv_adapter.cpp
+++ core/adapter/kv_store/src/lnn_kv_adapter.cpp
@@ -309,7 +309,7 @@ DistributedKv::Status KVAdapter::GetKvStorePtr()
         .baseDir = DATABASE_DIR,
         .isPublic = true,
         .cloudConfig = {
-            .enableCloud = true,
+            .enableCloud = true
         }
     };
     DistributedKv::Status status;
