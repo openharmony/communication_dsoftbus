0b42b7d3c3dd50e7d9bf1dfc940cdfce2b599fee
杨可欣 <yangkexin6@h-partners.com>
2024-05-27 19:22:17 +0800

    Clear alarms
    Signed-off-by: 杨可欣 <yangkexin6@h-partners.com>

--- core/bus_center/lnn/net_builder/src/lnn_devicename_info.c
+++ core/bus_center/lnn/net_builder/src/lnn_devicename_info.c
@@ -111,8 +111,8 @@ static void OnReceiveDeviceName(LnnSyncInfoType type, const char *networkId, con
         return;
     }
     char deviceName[DEVICE_NAME_BUF_LEN + 1] = {0};
-    if (memcpy_s(deviceName, DEVICE_NAME_BUF_LEN, msg, len) != EOK) {
-        LNN_LOGE(LNN_BUILDER, "memcpy fail");
+    if (strcpy_s(deviceName, DEVICE_NAME_BUF_LEN + 1, (char *)msg) != EOK) {
+        LNN_LOGE(LNN_BUILDER, "strcpy fail");
         return;
     }
     char *anonyNetworkId = NULL;
