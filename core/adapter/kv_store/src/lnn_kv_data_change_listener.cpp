67d067e8606a0fda60746f3af61d8aede44a14c3
wangzhaohao <wangzhaohao@huawei.com>
2024-05-29 15:45:55 +0800

    HandleAddChange中使用基于范围的for循环替代传统的for循环
    Signed-off-by: wangzhaohao <wangzhaohao@huawei.com>

--- core/adapter/kv_store/src/lnn_kv_data_change_listener.cpp
+++ core/adapter/kv_store/src/lnn_kv_data_change_listener.cpp
@@ -120,11 +120,13 @@ void KvDataChangeListener::HandleAddChange(const std::vector<DistributedKv::Entr
         return;
     }
 
-    for (int32_t i = 0; i < insertSize; ++i) {
-        std::string dbKey = insertRecords[i].key.ToString();
-        std::string dbValue = insertRecords[i].value.ToString();
+    int32_t i = 0;
+    for (const auto &item : insertRecords) {
+        std::string dbKey = item.key.ToString();
+        std::string dbValue = item.value.ToString();
         keys[i] = strdup(dbKey.c_str());
         values[i] = strdup(dbValue.c_str());
+        ++i;
     }
     LnnDBDataAddChangeSyncToCache(const_cast<const char **>(keys), const_cast<const char **>(values), insertSize);
 }
