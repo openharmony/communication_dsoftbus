05f2593fbad55e5bdb95f3651299a1fa4ef19c99
wangzhaohao <wangzhaohao@huawei.com>
2024-06-07 10:32:59 +0800

    监听到DB进程启动后，初始化端云模块
    Signed-off-by: wangzhaohao <wangzhaohao@huawei.com>

--- core/adapter/subscribe_kv_store_sa/src/lnn_kv_store_launch_listener.cpp
+++ core/adapter/subscribe_kv_store_sa/src/lnn_kv_store_launch_listener.cpp
@@ -16,14 +16,15 @@
 #include "lnn_kv_store_launch_listener.h"
 #include "lnn_log.h"
 #include "system_ability_definition.h"
+#include "lnn_data_cloud_sync.h"
 
 namespace OHOS {
 
 void KvStoreStatusChangeListener::OnAddSystemAbility(int32_t systemAbilityId, const std::string& deviceId)
 {
     if (systemAbilityId == DISTRIBUTED_KV_DATA_SERVICE_ABILITY_ID) {
-        // do something
         LNN_LOGI(LNN_LEDGER, "kv store SA launch.");
+        LnnInitCloudSyncModule();
     }
 }
 
