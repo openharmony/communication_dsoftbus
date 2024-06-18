34fae2c768fee4db1cdbae5dc27b300be158affd
王瑞峰 <wangruifeng14@huawei.com>
2024-06-18 01:10:49 +0000

    update core/discovery/manager/src/disc_manager.c.
    Signed-off-by: 王瑞峰 <wangruifeng14@huawei.com>

--- core/discovery/manager/src/disc_manager.c
+++ core/discovery/manager/src/disc_manager.c
@@ -241,16 +241,36 @@ static int32_t CallSpecificInterfaceFunc(const InnerOption *option,
     }
 }
 
+static void DfxCallInterfaceByMedium(const ExchangeMedium medium, const DiscoveryFuncInterface *interface,
+    const DiscoverMode mode, InterfaceFuncType type, int32_t reason)
+{
+        DiscEventExtra extra = { 0 };
+        DiscEventExtraInit(&extra);
+        extra.errcode = reason;
+        extra.discType = medium;
+        extra.interFuncType = type + 1;
+        extra.discMode = mode;
+        extra.result = (reason == SOFTBUS_OK) ? EVENT_STAGE_RESULT_OK : EVENT_STAGE_RESULT_FAILED;
+        DISC_EVENT(EVENT_SCENE_DISC, EVENT_STAGE_CALL_INTERFACE, extra);
+}
+
 static int32_t CallInterfaceByMedium(const DiscInfo *info, const char *packageName, const InterfaceFuncType type)
 {
+    int ret = SOFTBUS_OK;
     switch (info->medium) {
         case COAP:
-            return CallSpecificInterfaceFunc(&(info->option), g_discCoapInterface, info->mode, type);
+            ret = CallSpecificInterfaceFunc(&(info->option), g_discCoapInterface, info->mode, type);
+            DfxCallInterfaceByMedium(COAP, g_discCoapInterface, info->mode, type, ret);
+            return ret;
         case BLE:
-            return CallSpecificInterfaceFunc(&(info->option), g_discBleInterface, info->mode, type);
+            ret = CallSpecificInterfaceFunc(&(info->option), g_discBleInterface, info->mode, type);
+            DfxCallInterfaceByMedium(BLE, g_discBleInterface, info->mode, type, ret);
+            return ret;
         case AUTO: {
             int coapRes = CallSpecificInterfaceFunc(&(info->option), g_discCoapInterface, info->mode, type);
+            DfxCallInterfaceByMedium(COAP, g_discCoapInterface, info->mode, type, coapRes);
             int bleRes = CallSpecificInterfaceFunc(&(info->option), g_discBleInterface, info->mode, type);
+            DfxCallInterfaceByMedium(BLE, g_discBleInterface, info->mode, type, bleRes);
             DISC_CHECK_AND_RETURN_RET_LOGE(coapRes == SOFTBUS_OK || bleRes == SOFTBUS_OK,
                 SOFTBUS_DISCOVER_MANAGER_INNERFUNCTION_FAIL, DISC_CONTROL, "all medium failed");
             return SOFTBUS_OK;
