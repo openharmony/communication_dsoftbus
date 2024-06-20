15cef9f8a83b35a49683cae8b466dbffb7acf86e
wangboyi <wangboyi4@huawei.com>
2024-06-20 15:57:48 +0800

    feature:modify coap fwk
    Signed-off-by: wangboyi <wangboyi4@huawei.com>

---
+++ core/discovery/coap/common/src/disc_coap_parser_virtual.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "disc_coap_parser.h"
+#include "softbus_error_code.h"
+
+int32_t DiscCoapFillServiceData(const PublishOption *option, char *outData, uint32_t outDataLen, uint32_t allCap)
+{
+    (void *)option;
+    (void *)outData;
+    (void)outDataLen;
+    (void)allCap;
+    return SOFTBUS_OK;
+}
