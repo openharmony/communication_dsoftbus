1d25276d41559eddf5ecb6c9a6bd026952996332
liwenqiang <liwenqiang45@huawei.com>
2024-06-28 20:13:39 +0800

    add p2p ut
    Signed-off-by: liwenqiang <liwenqiang45@huawei.com>

--- tests/core/connection/wifi_direct_cpp/entity/p2p_entity_test.cpp
+++ tests/core/connection/wifi_direct_cpp/entity/p2p_entity_test.cpp
@@ -20,6 +20,7 @@
 #include "softbus_error_code.h"
 #include "data/interface_info.h"
 #include "data/interface_manager.h"
+#include "entity/p2p_connect_state.h"
 #include "entity/p2p_entity.h"
 #include "wifi_direct_mock.h"
 
@@ -93,6 +94,32 @@ HWTEST_F(P2pEntityTest, CreateGroupTest002, TestSize.Level1)
 }
 
 /*
+* @tc.name: CreateGroupTest003
+* @tc.desc: check create group
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(P2pEntityTest, CreateGroupTest003, TestSize.Level1)
+{
+    InterfaceManager::GetInstance().UpdateInterface(
+        InterfaceInfo::InterfaceType::P2P, [](InterfaceInfo &info) {
+        info. SetReuseCount(1); // 1 Indicates the number of reference counting .
+        return SOFTBUS_OK;
+    });
+    std::shared_ptr<P2pOperationWrapper<P2pDestroyGroupParam>> destroyGroupParam;
+    int result = P2pCreateGroupState::Instance()->RemoveLink(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    P2pConnectParam param{"123\n01:02:03:04:05:06\n555\n16\n1", false, false};
+    auto operation = std::make_shared<P2pOperationWrapper<P2pConnectParam>>(param, P2pOperationType::CONNECT);
+    result = P2pCreateGroupState::Instance()->Connect(operation);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    result = P2pCreateGroupState::Instance()->DestroyGroup(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+}
+
+/*
 * @tc.name: ConnectTest001
 * @tc.desc: check connect
 * @tc.type: FUNC
@@ -137,6 +164,28 @@ HWTEST_F(P2pEntityTest, ConnectTest002, TestSize.Level1)
     P2pConnectParam param{"123\n01:02:03:04:05:06\n555\n16\n1", false, false};
     P2pOperationResult result = P2pEntity::GetInstance().Connect(param);
     EXPECT_EQ(result.errorCode_, ToSoftBusErrorCode(ERROR_WIFI_UNKNOWN));
+}
+
+
+/*
+* @tc.name: ConnectTest003
+* @tc.desc: check connect
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(P2pEntityTest, ConnectTest003, TestSize.Level1)
+{
+    InterfaceManager::GetInstance().UpdateInterface(
+        InterfaceInfo::InterfaceType::P2P, [](InterfaceInfo &info) {
+        info. SetReuseCount(1); // 1 Indicates the number of reference counting .
+        return SOFTBUS_OK;
+    });
+    std::shared_ptr<P2pOperationWrapper<P2pDestroyGroupParam>> destroyGroupParam;
+    int result = P2pConnectState::Instance()->RemoveLink(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    result = P2pConnectState::Instance()->DestroyGroup(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
 }
 
 /*
@@ -221,6 +270,38 @@ HWTEST_F(P2pEntityTest, DestroyGroupTest002, TestSize.Level1)
         P2pOperationResult result = P2pEntity::GetInstance().DestroyGroup(param);
         EXPECT_EQ(result.errorCode_, ToSoftBusErrorCode(ERROR_WIFI_UNKNOWN));
     }
+}
+
+/*
+* @tc.name: DestroyGroupTest003
+* @tc.desc: check destroy group
+* @tc.type: FUNC
+* @tc.require:
+*/
+HWTEST_F(P2pEntityTest, DestroyGroupTest003, TestSize.Level1)
+{
+    InterfaceManager::GetInstance().UpdateInterface(
+        InterfaceInfo::InterfaceType::P2P, [](InterfaceInfo &info) {
+        info. SetReuseCount(1); // 1 Indicates the number of reference counting .
+        return SOFTBUS_OK;
+    });
+    std::shared_ptr<P2pOperationWrapper<P2pDestroyGroupParam>> destroyGroupParam;
+    int result = P2pDestroyGroupState::Instance()->RemoveLink(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    P2pConnectParam param{"123\n01:02:03:04:05:06\n555\n16\n1", false, false};
+    auto operation = std::make_shared<P2pOperationWrapper<P2pConnectParam>>(param, P2pOperationType::CONNECT);
+    result = P2pDestroyGroupState::Instance()->Connect(operation);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    result = P2pDestroyGroupState::Instance()->DestroyGroup(destroyGroupParam);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
+
+    P2pCreateGroupParam createGroupParam{5180, true};
+    auto createGroupOp =
+        std::make_shared<P2pOperationWrapper<P2pCreateGroupParam>>(createGroupParam, P2pOperationType::CREATE_GROUP);
+    result = P2pDestroyGroupState::Instance()->CreateGroup(createGroupOp);
+    EXPECT_EQ(result, SOFTBUS_CONN_NOT_SUPPORT_FAILED);
 }
 
 /*
