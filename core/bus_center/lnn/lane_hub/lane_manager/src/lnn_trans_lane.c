d389955dc6a86c7df0d64b4435a34a5d400d1869
wang_mingxu <wangmingxu9@huawei.com>
2024-07-05 14:05:56 +0800

    fix SpecifiedLink
    Signed-off-by: wang_mingxu <wangmingxu9@huawei.com>

--- core/bus_center/lnn/lane_hub/lane_manager/src/lnn_trans_lane.c
+++ core/bus_center/lnn/lane_hub/lane_manager/src/lnn_trans_lane.c
@@ -557,38 +557,51 @@ static int32_t AllocTargetLane(uint32_t laneHandle, const LaneAllocInfoExt *allo
     return BuildTargetLink(laneHandle, allocInfo, listener);
 }
 
-static bool SpecifiedLinkConvert(LaneSpecifiedLink link, LanePreferredLinkList *preferLink)
+static int32_t SpecifiedLinkConvert(const char *networkId, LaneSpecifiedLink link, LanePreferredLinkList *preferLink)
 {
+    LaneLinkType optionalLink[LANE_LINK_TYPE_BUTT];
+    (void)memset_s(optionalLink, sizeof(optionalLink), 0, sizeof(optionalLink));
     uint32_t linkNum = 0;
     switch (link) {
         case LANE_LINK_TYPE_WIFI_WLAN:
-            preferLink->linkType[linkNum++] = LANE_WLAN_5G;
-            preferLink->linkType[linkNum++] = LANE_WLAN_2P4G;
+            optionalLink[linkNum++] = LANE_WLAN_5G;
+            optionalLink[linkNum++] = LANE_WLAN_2P4G;
             break;
         case LANE_LINK_TYPE_WIFI_P2P:
-            preferLink->linkType[linkNum++] = LANE_P2P;
+            optionalLink[linkNum++] = LANE_P2P;
             break;
         case LANE_LINK_TYPE_BR:
-            preferLink->linkType[linkNum++] = LANE_BR;
+            optionalLink[linkNum++] = LANE_BR;
             break;
         case LANE_LINK_TYPE_COC_DIRECT:
-            preferLink->linkType[linkNum++] = LANE_COC_DIRECT;
+            optionalLink[linkNum++] = LANE_COC_DIRECT;
             break;
         case LANE_LINK_TYPE_BLE_DIRECT:
-            preferLink->linkType[linkNum++] = LANE_BLE_DIRECT;
+            optionalLink[linkNum++] = LANE_BLE_DIRECT;
             break;
         case LANE_LINK_TYPE_HML:
-            preferLink->linkType[linkNum++] = LANE_HML;
+            optionalLink[linkNum++] = LANE_HML;
             break;
         default:
             LNN_LOGE(LNN_LANE, "unexpected link=%{public}d", link);
             break;
     }
     if (linkNum == 0) {
-        return false;
+        return SOFTBUS_LANE_NO_AVAILABLE_LINK;
+    }
+    uint32_t i = 0;
+    for (i = 0; i < linkNum; i++) {
+        if (LaneCapCheck(networkId, optionalLink[i]) != SOFTBUS_OK) {
+            LNN_LOGE(LNN_LANE, "SpecifiedLink capcheck fail=%{public}d", optionalLink[i]);
+            continue;
+        }
+        preferLink->linkType[0] = optionalLink[i];
+    }
+    if (i == 0) {
+        return GetErrCodeOfLink(networkId, optionalLink[0]);
     }
     preferLink->linkTypeNum = linkNum;
-    return true;
+    return SOFTBUS_OK;
 }
 
 static int32_t ProcessSpecifiedLink(uint32_t laneHandle, const LaneAllocInfo *allocInfo,
@@ -596,7 +609,8 @@ static int32_t ProcessSpecifiedLink(uint32_t laneHandle, const LaneAllocInfo *al
 {
     LaneAllocInfoExt info;
     (void)memset_s(&info, sizeof(info), 0, sizeof(info));
-    if (!SpecifiedLinkConvert(allocInfo->extendInfo.linkType, &info.linkList)) {
+    if (SpecifiedLinkConvert((const char *)allocInfo->networkId, allocInfo->extendInfo.linkType, &info.linkList)
+        != SOFTBUS_OK) {
         return SOFTBUS_LANE_SELECT_FAIL;
     }
     info.type = allocInfo->type;
