59007286bb40ef9aa7e754d25848a946fb8132a4
18392170496 <magentang4@huawei.com>
2024-07-15 19:45:15 +0800

    fix wifi net cap
    Signed-off-by: 18392170496 <magentang4@huawei.com>

--- tests/core/bus_center/lnn/lane/include/lnn_lane_deps_mock.h
+++ tests/core/bus_center/lnn/lane/include/lnn_lane_deps_mock.h
@@ -99,6 +99,8 @@ public:
     virtual struct WifiDirectManager* GetWifiDirectManager(void) = 0;
     virtual int32_t LnnGetRemoteNumU32Info(const char *networkId, InfoKey key, uint32_t *info) = 0;
     virtual int32_t LnnGetLocalNumU32Info(InfoKey key, uint32_t *info) = 0;
+    virtual int32_t LnnSetLocalNumU32Info(InfoKey key, uint32_t info) = 0;
+    virtual int32_t LnnSetNetCapability(uint32_t *capability, NetCapability type) = 0;
     virtual void LnnDumpLocalBasicInfo(void) = 0;
     virtual void LnnDumpOnlineDeviceInfo(void) = 0;
     virtual int32_t LnnConvertDlId(const char *srcId, IdCategory srcIdType, IdCategory dstIdType,
@@ -159,6 +161,8 @@ public:
     MOCK_METHOD0(GetWifiDirectManager, struct WifiDirectManager* (void));
     MOCK_METHOD3(LnnGetRemoteNumU32Info, int32_t (const char *networkId, InfoKey key, uint32_t *info));
     MOCK_METHOD2(LnnGetLocalNumU32Info, int32_t (InfoKey key, uint32_t *info));
+    MOCK_METHOD2(LnnSetLocalNumU32Info, int32_t (InfoKey key, uint32_t info));
+    MOCK_METHOD2(LnnSetNetCapability, int32_t (uint32_t *capability, NetCapability type));
     MOCK_METHOD0(LnnDumpLocalBasicInfo, void (void));
     MOCK_METHOD0(LnnDumpOnlineDeviceInfo, void (void));
     MOCK_METHOD5(LnnConvertDlId, int32_t (const char *srcId, IdCategory srcIdType, IdCategory dstIdType,
