a1f5615d3721d2d323caf5f66629a250cf9d1710
xiawenteng <xiawenteng@huawei.com>
2024-07-18 10:43:26 +0800

    coap interface for lnn
    Signed-off-by: xiawenteng <xiawenteng@huawei.com>

---
+++ core/connection/coap/include/conn_coap.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CONN_COAP_H
+#define CONN_COAP_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t ConnCoapStartServerListen(void);
+void ConnCoapStopServerListen(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CONN_COAP_H
