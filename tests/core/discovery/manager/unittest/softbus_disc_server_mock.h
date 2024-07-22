7eaff67cb9c6680a7b39d198a4a37dd49e19fc27
yanghuiqing <yanghuiqing6@huawei.com>
2024-07-22 16:27:36 +0800

    fix softbus_disc_server.c ut
    Signed-off-by: yanghuiqing <yanghuiqing6@huawei.com>

--- tests/core/discovery/manager/unittest/softbus_disc_server_mock.h
+++ tests/core/discovery/manager/unittest/softbus_disc_server_mock.h
@@ -21,6 +21,7 @@
 #include "disc_manager.h"
 #include "gmock/gmock.h"
 #include "softbus_common.h"
+#include "softbus_hisysevt_discreporter.h"
 
 namespace OHOS {
 class DiscMockInterface {
@@ -33,6 +34,13 @@ public:
     virtual int32_t DiscStartDiscovery(const char *packageName, const SubscribeInfo *info,
                                        const IServerDiscInnerCallback *cb) = 0;
     virtual int32_t DiscStopDiscovery(const char *packageName, int32_t subscribeId) = 0;
+    virtual int32_t ClientIpcOnPublishFail(const char *pkgName, int publishId, int reason) = 0;
+    virtual int32_t ClientIpcOnPublishSuccess(const char *pkgName, int publishId) = 0;
+    virtual int32_t ClientIpcOnDiscoverFailed(const char *pkgName, int subscribeId, int failReason) = 0;
+    virtual int32_t ClientIpcDiscoverySuccess(const char *pkgName, int subscribeId) = 0;
+    virtual int32_t SoftbusReportDiscFault(SoftBusDiscMedium medium, int32_t errCode) = 0;
+    virtual int32_t ClientIpcOnDeviceFound(const char *pkgName, const DeviceInfo *device,
+                                           const InnerDeviceInfoAddtions *additions) = 0;
 };
 
 class DiscMock : public DiscMockInterface {
@@ -49,6 +57,13 @@ public:
     MOCK_METHOD(int32_t, DiscStartDiscovery,
                (const char *packageName, const SubscribeInfo *info, const IServerDiscInnerCallback *cb), (override));
     MOCK_METHOD(int32_t, DiscStopDiscovery, (const char *packageName, int32_t subscribeId), (override));
+    MOCK_METHOD(int32_t, ClientIpcOnPublishFail, (const char *pkgName, int publishId, int reason), (override));
+    MOCK_METHOD(int32_t, ClientIpcOnPublishSuccess, (const char *pkgName, int publishId), (override));
+    MOCK_METHOD(int32_t, ClientIpcOnDiscoverFailed, (const char *pkgName, int subscribeId, int failReason), (override));
+    MOCK_METHOD(int32_t, ClientIpcDiscoverySuccess, (const char *pkgName, int subscribeId), (override));
+    MOCK_METHOD(int32_t, SoftbusReportDiscFault, (SoftBusDiscMedium medium, int32_t errCode), (override));
+    MOCK_METHOD(int32_t, ClientIpcOnDeviceFound,
+               (const char *pkgName, const DeviceInfo *device, const InnerDeviceInfoAddtions *additions), (override));
 
 private:
     static inline std::atomic<DiscMock*> mock = nullptr;
