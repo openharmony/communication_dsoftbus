ea3140bf918b6c47a0e62e1cbad31fd4a0a3641d
张晗 <z464772357@163.com>
2024-07-23 02:41:16 +0000

    add set MultiLayer
    Signed-off-by: 张晗 <z464772357@163.com>

---
+++ sdk/transmission/trans_channel/udp/stream/libsoftbus_stream/vtp_stream_opt.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vtp_stream_opt.h"
+
+#include "softbus_errcode.h"
+
+int32_t VtpSetSocketMultiLayer(int fd, OnFrameEvt *cb, const void *para)
+{
+    (void)fd;
+    (void)cb;
+    (void)para;
+    return SOFTBUS_FUNC_NOT_SUPPORT;
+}
+
+bool IsVtpFrameSentEvt(const FtEventCbkInfo *info)
+{
+    (void)info;
+    return false;
+}
+
+int HandleVtpFrameEvt(int fd, OnFrameEvt cb, const FtEventCbkInfo *info)
+{
+    (void)fd;
+    (void)cb;
+    (void)info;
+    return SOFTBUS_FUNC_NOT_SUPPORT;
+}
