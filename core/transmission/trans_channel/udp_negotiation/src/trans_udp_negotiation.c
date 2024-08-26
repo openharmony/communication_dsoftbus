ea4251e10cc9f48bfcbf762ced36d16440987882
Zhangzi <zhangjiaxiang7@huawei.com>
2024-08-26 09:16:37 +0000

    pkgName and sessionName anonymous printing correction
    Signed-off-by: erliangqi <zhangjiaxiang7@huawei.com>

--- core/transmission/trans_channel/udp_negotiation/src/trans_udp_negotiation.c
+++ core/transmission/trans_channel/udp_negotiation/src/trans_udp_negotiation.c
@@ -1183,10 +1183,7 @@ void TransUdpDeathCallback(const char *pkgName, int32_t pid)
         TRANS_LOGE(TRANS_CTRL, "param invalid");
         return;
     }
-    char *anonymizePkgName = NULL;
-    Anonymize(pkgName, &anonymizePkgName);
-    TRANS_LOGW(TRANS_CTRL, "TransUdpDeathCallback: pkgName=%{public}s, pid=%{public}d", anonymizePkgName, pid);
-    AnonymizeFree(anonymizePkgName);
+
     if (GetUdpChannelLock() != SOFTBUS_OK) {
         TRANS_LOGE(TRANS_CTRL, "lock failed");
         return;
@@ -1205,7 +1202,10 @@ void TransUdpDeathCallback(const char *pkgName, int32_t pid)
             }
             *tempNode = *udpChannelNode;
             ListAdd(&destroyList, &tempNode->node);
-            TRANS_LOGI(TRANS_CTRL, "add pkgName = %{public}s", pkgName);
+            char *anonymizePkgName = NULL;
+            Anonymize(pkgName, &anonymizePkgName);
+            TRANS_LOGW(TRANS_CTRL, "add pkgName=%{public}s, pid=%{public}d", anonymizePkgName, pid);
+            AnonymizeFree(anonymizePkgName);
         }
     }
     (void)ReleaseUdpChannelLock();
@@ -1218,6 +1218,6 @@ void TransUdpDeathCallback(const char *pkgName, int32_t pid)
         ListDelete(&udpChannelNode->node);
         SoftBusFree(udpChannelNode);
     }
-    TRANS_LOGI(TRANS_CTRL, "TransUdpDeathCallback end pkgName=%{public}s", pkgName);
+
     return;
 }
