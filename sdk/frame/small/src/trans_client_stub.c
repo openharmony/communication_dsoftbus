f348f4c8a26a95b290091bc8f4da537fc0c113b3
姚望 <yaopwang@huawei.com>
2024-08-27 14:11:27 +0000

    update sdk/frame/small/src/trans_client_stub.c.
    Signed-off-by: 姚望 <yaopwang@huawei.com>

--- sdk/frame/small/src/trans_client_stub.c
+++ sdk/frame/small/src/trans_client_stub.c
@@ -46,7 +46,7 @@ int32_t ClientOnChannelOpened(IpcIo *data, IpcIo *reply)
     }
     if (channel.channelType == CHANNEL_TYPE_TCP_DIRECT) {
         channel.myIp = (char *)ReadString(data, &size);
-        TRANS_CHECK_AND_RETURN_RET_LOGE(channel.myIP != NULL, SOFTBUS_IPC_ERR, TRANS_LOGE, "pointer null error.");
+        TRANS_CHECK_AND_RETURN_RET_LOGE(channel.myIp != NULL, SOFTBUS_IPC_ERR, TRANS_LOGE, "pointer null error.");
         channel.fd = ReadFileDescriptor(data);
     }
     ReadInt32(data, &(channel.businessType));
