83f569918386985b5a0f46f4a95ef9d5ea71cc38
姚望 <yaopwang@huawei.com>
2024-08-27 14:26:08 +0000

    update sdk/frame/small/src/trans_client_stub.c.
    Signed-off-by: 姚望 <yaopwang@huawei.com>

--- sdk/frame/small/src/trans_client_stub.c
+++ sdk/frame/small/src/trans_client_stub.c
@@ -23,7 +23,10 @@
 
 int32_t ClientOnChannelOpened(IpcIo *data, IpcIo *reply)
 {
-    TRANS_CHECK_AND_RETURN_RET_LOGE(reply != NULL, SOFTBUS_INVALID_PARAM, TRANS_LOGE, "invalid param.");
+    if (reply == NULL) {
+        TRANS_LOGE(TRANS_CTRL, "invalid param.");
+        return SOFTBUS_INVALID_PARAM;
+    }
     size_t size = 0;
     ChannelInfo channel = {0};
     const char *sessionName = (const char *)ReadString(data, &size);
@@ -46,7 +49,10 @@ int32_t ClientOnChannelOpened(IpcIo *data, IpcIo *reply)
     }
     if (channel.channelType == CHANNEL_TYPE_TCP_DIRECT) {
         channel.myIp = (char *)ReadString(data, &size);
-        TRANS_CHECK_AND_RETURN_RET_LOGE(channel.myIp != NULL, SOFTBUS_IPC_ERR, TRANS_LOGE, "pointer null error.");
+        if (channel.myIp == NULL) {
+            TRANS_LOGE(TRANS_CTRL, "pointer null error.");
+            return SOFTBUS_IPC_ERR;
+        }
         channel.fd = ReadFileDescriptor(data);
     }
     ReadInt32(data, &(channel.businessType));
