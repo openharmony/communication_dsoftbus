c520db3417fe2d8339ed4a6bfa5a0bcafc7f0912
Agrant <luojuheng@huawei.com>
2024-08-30 07:44:01 +0000

    update components/nstackx/fillp/src/fillp_lib/src/fillp/fillp_pcb.c.
    Signed-off-by: Agrant <luojuheng@huawei.com>

--- components/nstackx/fillp/src/fillp_lib/src/fillp/fillp_pcb.c
+++ components/nstackx/fillp/src/fillp_lib/src/fillp/fillp_pcb.c
@@ -31,8 +31,7 @@ extern "C" {
 #define FILLP_PCB_GET_CONN(pcb) (struct FtNetconn *)((struct SpungePcb *)((pcb)->spcb))->conn
 #define FILLP_UNRECV_THRESHOLD 2
 
-__attribute__((no_sanitize("unsigned-integer-overflow")))
-static inline FILLP_INT FillpSkiplistCmp(void *t1, void *t2)
+IGNORE_OVERFLOW static inline FILLP_INT FillpSkiplistCmp(void *t1, void *t2)
 {
     struct FillpPcbItem *value1 = (struct FillpPcbItem *)t1;
     struct FillpPcbItem *value2 = (struct FillpPcbItem *)t2;
@@ -44,8 +43,7 @@ static inline FILLP_INT FillpSkiplistCmp(void *t1, void *t2)
     return ((FILLP_INT32)(value1->seqNum - value2->seqNum)) > 0;
 }
 
-__attribute__((no_sanitize("unsigned-integer-overflow")))
-static inline FILLP_INT FillpSkiplistRecvcmp(void *t1, void *t2)
+IGNORE_OVERFLOW static inline FILLP_INT FillpSkiplistRecvcmp(void *t1, void *t2)
 {
     struct FillpPcbItem *value1 = (struct FillpPcbItem *)t1;
     struct FillpPcbItem *value2 = (struct FillpPcbItem *)t2;
