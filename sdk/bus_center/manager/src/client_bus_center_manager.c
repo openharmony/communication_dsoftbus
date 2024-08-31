ea899cd523a75c1859f4026293b2dd1c729ff1dc
wudj2110 <wudaijun2@huawei.com>
2024-08-31 11:37:43 +0800

    fix: Resolved the problems described in the review comments
    Signed-off-by: wudj2110 <wudaijun2@huawei.com>

--- sdk/bus_center/manager/src/client_bus_center_manager.c
+++ sdk/bus_center/manager/src/client_bus_center_manager.c
@@ -1212,7 +1212,7 @@ int32_t LnnOnNodeStatusChanged(const char *pkgName, void *info, int32_t type)
         type, nodeStatus->reserved[0]);
     LIST_FOR_EACH_ENTRY(item, &dupList, NodeStateCallbackItem, node) {
         if (((strcmp(item->pkgName, pkgName) == 0) || (strlen(pkgName) == 0)) &&
-            (item->cb.events & EVENT_NODE_STATUS_CHANGED) != 0) {
+            (item->cb.events & EVENT_NODE_STATUS_CHANGED) != 0 && item->cb.onNodeStatusChanged != NULL) {
             LNN_LOGI(LNN_STATE, "LnnOnNodeStatusChanged, pkgName=%{public}s, type=%{public}d, screen=%{public}d", pkgName, type,
                 nodeStatus->reserved[0]);
             item->cb.onNodeStatusChanged((NodeStatusType)type, nodeStatus);
