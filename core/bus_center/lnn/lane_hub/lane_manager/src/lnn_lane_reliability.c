ba6e39d6a8410dffa9a2808d15121b9f03af3634
wang_mingxu <wangmingxu9@huawei.com>
2024-09-05 19:52:29 +0800

    WiFi detects real IP addresses v4
    Signed-off-by: wang_mingxu <wangmingxu9@huawei.com>

--- core/bus_center/lnn/lane_hub/lane_manager/src/lnn_lane_reliability.c
+++ core/bus_center/lnn/lane_hub/lane_manager/src/lnn_lane_reliability.c
@@ -88,7 +88,7 @@ static int32_t ClientConnectTcp(LaneDetectInfo *infoItem)
         return SOFTBUS_MEM_ERR;
     }
     char localIp[IP_LEN] = {0};
-    int32_t fd = 0;
+    int32_t fd = SOFTBUS_INVALID_FD;
     if (LnnGetLocalStrInfo(STRING_KEY_WLAN_IP, localIp, IP_LEN) != SOFTBUS_OK) {
         LNN_LOGE(LNN_LANE, "get local ip fail");
         fd = ConnOpenClientSocket(&option, BIND_ADDR_ALL, true);
