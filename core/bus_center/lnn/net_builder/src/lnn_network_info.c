0f287d2c0024e5134fedc1aa351dc679fd920473
18392170496 <magentang4@huawei.com>
2024-10-15 11:09:55 +0800

    sync code Signed-off-by: 18392170496 <magentang4@huawei.com>

--- core/bus_center/lnn/net_builder/src/lnn_network_info.c
+++ core/bus_center/lnn/net_builder/src/lnn_network_info.c
@@ -293,6 +293,25 @@ static void LnnSetP2pNetCapability(uint32_t *capability)
     }
 }
 
+static void ProcessApEnabled(uint32_t *capability, bool *needSync)
+{
+    g_isApEnable = true;
+    LnnSetNetworkCapability(capability);
+    if (IsP2pAvailable(true)) {
+        (void)LnnSetNetCapability(capability, BIT_WIFI_P2P);
+    }
+    *needSync = true;
+}
+
+static void ProcessApDisabled(uint32_t *capability, bool *needSync)
+{
+    g_isApEnable = false;
+    if (IsP2pAvailable(false)) {
+        (void)LnnSetNetCapability(capability, BIT_WIFI_P2P);
+    }
+    *needSync = true;
+}
+
 static void GetNetworkCapability(SoftBusWifiState wifiState, uint32_t *capability, bool *needSync)
 {
     switch (wifiState) {
@@ -325,18 +344,14 @@ static void GetNetworkCapability(SoftBusWifiState wifiState, uint32_t *capabilit
             *needSync = true;
             break;
         case SOFTBUS_AP_ENABLED:
-            g_isApEnable = true;
-            LnnSetNetworkCapability(capability);
-            if (IsP2pAvailable(true)) {
-                (void)LnnSetNetCapability(capability, BIT_WIFI_P2P);
-            }
-            *needSync = true;
+            ProcessApEnabled(capability, needSync);
             break;
         case SOFTBUS_AP_DISABLED:
-            g_isApEnable = false;
-            if (IsP2pAvailable(false)) {
-                (void)LnnSetNetCapability(capability, BIT_WIFI_P2P);
-            }
+            ProcessApDisabled(capability, needSync);
+            break;
+        case SOFTBUS_WIFI_SEMI_ACTIVE:
+            g_isWifiEnable = true;
+            (void)LnnSetNetCapability(capability, BIT_WIFI_P2P);
             *needSync = true;
             break;
         default:
