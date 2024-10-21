9b421d6c5342103b03e48635052d8cfe8c763023
wurui <wurui51@huawei.com>
2024-10-21 15:26:12 +0800

    config static ip for winpc
    Signed-off-by: wurui <wurui51@huawei.com>

--- core/connection/wifi_direct_cpp/processor/p2p_v1_processor.cpp
+++ core/connection/wifi_direct_cpp/processor/p2p_v1_processor.cpp
@@ -1657,15 +1657,11 @@ int P2pV1Processor::UpdateWhenConnectSuccess(std::string groupConfig, const Nego
     CONN_CHECK_AND_RETURN_RET_LOGW(
         ret == SOFTBUS_OK, ret, CONN_WIFI_DIRECT, "update interface failed, error=%{public}d", ret);
     std::string remoteMac = msg.GetLegacyP2pMac();
-    std::string remoteIp = msg.GetLegacyP2pGoIp();
-    auto success = LinkManager::GetInstance().ProcessIfAbsent(
-        InnerLink::LinkType::P2P, msg.GetRemoteDeviceId(), [localMac, localIp, remoteMac, remoteIp](InnerLink &link) {
-            link.SetRemoteBaseMac(remoteMac);
-            link.SetLocalBaseMac(localMac);
-            link.SetRemoteIpv4(remoteIp);
-            link.SetLocalIpv4(localIp);
-            link.SetState(InnerLink::LinkState::CONNECTED);
-        });
+    auto success = LinkManager::GetInstance().ProcessIfPresent(remoteMac, [localMac, localIp](InnerLink &link) {
+        link.SetLocalBaseMac(localMac);
+        link.SetLocalIpv4(localIp);
+        link.SetState(InnerLink::LinkState::CONNECTED);
+    });
     CONN_CHECK_AND_RETURN_RET_LOGW(
         success, SOFTBUS_CONN_PV1_INTERNAL_ERR0R, CONN_WIFI_DIRECT, "update inner link failed");
     WifiDirectUtils::SyncLnnInfoForP2p(WifiDirectUtils::ToWifiDirectRole(myRole), localMac, msg.GetLegacyP2pGoMac());
@@ -1690,6 +1686,15 @@ int P2pV1Processor::ConnectGroup(const NegotiateMessage &msg, const std::shared_
     int coexCode = P2pAdapter::GetCoexConflictCode(IF_NAME_P2P0, WifiDirectUtils::FrequencyToChannel(freq));
     CONN_CHECK_AND_RETURN_RET_LOGE(
         coexCode == SOFTBUS_OK, coexCode, CONN_WIFI_DIRECT, "coex conflict, errorcode=%{public}d", coexCode);
+    std::string remoteMac = msg.GetLegacyP2pMac();
+    std::string remoteIp = msg.GetLegacyP2pGoIp();
+    auto success = LinkManager::GetInstance().ProcessIfAbsent(
+        InnerLink::LinkType::P2P, msg.GetRemoteDeviceId(), [remoteMac, remoteIp](InnerLink &link) {
+            link.SetRemoteBaseMac(remoteMac);
+            link.SetRemoteIpv4(remoteIp);
+        });
+    CONN_CHECK_AND_RETURN_RET_LOGW(
+        success, SOFTBUS_CONN_PV1_INTERNAL_ERR0R, CONN_WIFI_DIRECT, "update inner link failed");
     P2pAdapter::ConnectParam params {};
     params.isNeedDhcp = IsNeedDhcp(gcIp, groupConfig);
     params.groupConfig = groupConfig;
