f8533d3faaa6c193a291fda350d0a8329a6e4e85
weiqian <weiqian22@huawei.com>
2024-10-31 11:43:15 +0800

    feat:auth proof info report
    Signed-off-by: weiqian <weiqian22@huawei.com>

--- core/bus_center/ipc/standard/include/bus_center_client_proxy_standard.h
+++ core/bus_center/ipc/standard/include/bus_center_client_proxy_standard.h
@@ -44,6 +44,8 @@ public:
     int32_t OnNodeStatusChanged(const char *pkgName, void *info, uint32_t infoTypeLen, int32_t type) override;
     int32_t OnLocalNetworkIdChanged(const char *pkgName) override;
     int32_t OnNodeDeviceNotTrusted(const char *pkgName, const char *msg) override;
+	int32_t OnHichainProofException(const char *pkgName, const char *proofInfo, uint32_t proofLen,
+        uint16_t deviceTypeId, int32_t errCode) override;
     int32_t OnTimeSyncResult(const void *info, uint32_t infoTypeLen, int32_t retCode) override;
     void OnPublishLNNResult(int32_t publishId, int32_t reason) override;
     void OnRefreshLNNResult(int32_t refreshId, int32_t reason) override;
