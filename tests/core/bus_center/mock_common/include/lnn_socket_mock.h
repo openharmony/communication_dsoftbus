4a44285fd15b65e3c333ec5657febafde9c987a0
shenjiahao5 <shenjiahao5@huawei.com>
2024-11-22 17:13:32 +0800

    fix tests\core\bus_center\mock_common
    Signed-off-by: shenjiahao5 <shenjiahao5@huawei.com>

--- tests/core/bus_center/mock_common/include/lnn_socket_mock.h
+++ tests/core/bus_center/mock_common/include/lnn_socket_mock.h
@@ -16,17 +16,18 @@
 #ifndef DSOFTBUS_LNN_SOCKET_MOCK_H
 #define DSOFTBUS_LNN_SOCKET_MOCK_H
 
-#include "softbus_socket.h"
-#include "auth_tcp_connection.h"
 #include <gmock/gmock.h>
+
+#include "auth_tcp_connection.h"
+#include "softbus_socket.h"
 
 namespace OHOS {
 class LnnSocketInterface {
 public:
     LnnSocketInterface() {};
     virtual ~LnnSocketInterface() {};
-    virtual const SocketInterface* GetSocketInterface(ProtocolType protocolType) = 0;
-    virtual int32_t RegistSocketProtocol(const SocketInterface* interface) = 0;
+    virtual const SocketInterface *GetSocketInterface(ProtocolType protocolType) = 0;
+    virtual int32_t RegistSocketProtocol(const SocketInterface *interface) = 0;
     virtual int32_t ConnOpenClientSocket(const ConnectOption *option, const char *bindAddr, bool isNonBlock) = 0;
     virtual ssize_t ConnSendSocketData(int32_t fd, const char *buf, size_t len, int32_t timeout) = 0;
     virtual void ConnShutdownSocket(int32_t fd) = 0;
@@ -44,17 +45,17 @@ public:
     LnnSocketInterfaceMock();
     ~LnnSocketInterfaceMock() override;
 
-    MOCK_METHOD1(GetSocketInterface, const SocketInterface*(ProtocolType));
-    MOCK_METHOD1(RegistSocketProtocol, int32_t (const SocketInterface*));
-    MOCK_METHOD3(ConnOpenClientSocket, int32_t (const ConnectOption *, const char *, bool));
-    MOCK_METHOD4(ConnSendSocketData, ssize_t (int32_t, const char *, size_t, int32_t));
-    MOCK_METHOD1(ConnShutdownSocket, void (int32_t));
-    MOCK_METHOD4(ConnSetTcpKeepalive, int32_t (int32_t, int32_t, int32_t, int32_t));
-    MOCK_METHOD2(ConnSetTcpUserTimeOut, int32_t (int32_t, uint32_t));
-    MOCK_METHOD2(ConnToggleNonBlockMode, int32_t (int32_t, bool));
-    MOCK_METHOD1(ConnGetSocketError, int32_t (int32_t));
-    MOCK_METHOD1(ConnGetLocalSocketPort, int32_t (int32_t));
-    MOCK_METHOD2(ConnGetPeerSocketAddr, int32_t (int32_t, SocketAddr *));
+    MOCK_METHOD1(GetSocketInterface, const SocketInterface *(ProtocolType));
+    MOCK_METHOD1(RegistSocketProtocol, int32_t(const SocketInterface *));
+    MOCK_METHOD3(ConnOpenClientSocket, int32_t(const ConnectOption *, const char *, bool));
+    MOCK_METHOD4(ConnSendSocketData, ssize_t(int32_t, const char *, size_t, int32_t));
+    MOCK_METHOD1(ConnShutdownSocket, void(int32_t));
+    MOCK_METHOD4(ConnSetTcpKeepalive, int32_t(int32_t, int32_t, int32_t, int32_t));
+    MOCK_METHOD2(ConnSetTcpUserTimeOut, int32_t(int32_t, uint32_t));
+    MOCK_METHOD2(ConnToggleNonBlockMode, int32_t(int32_t, bool));
+    MOCK_METHOD1(ConnGetSocketError, int32_t(int32_t));
+    MOCK_METHOD1(ConnGetLocalSocketPort, int32_t(int32_t));
+    MOCK_METHOD2(ConnGetPeerSocketAddr, int32_t(int32_t, SocketAddr *));
 };
 } // namespace OHOS
 #endif // DSOFTBUS_LNN_SOCKET_MOCK_H
