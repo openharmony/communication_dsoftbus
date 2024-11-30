72d175d760ea15d8803cedd30833186cbee5994c
liangjunhao <liangjunhao10@huawei.com>
2024-11-30 10:31:26 +0800

    fix: select authtype
    Signed-off-by: liangjunhao <liangjunhao10@huawei.com>

--- core/transmission/trans_channel/udp_negotiation/src/trans_udp_negotiation_exchange.c
+++ core/transmission/trans_channel/udp_negotiation/src/trans_udp_negotiation_exchange.c
@@ -33,7 +33,7 @@ typedef enum {
 
 #define ISHARE_SESSION_NAME "IShare*"
 
-static inline bool IsIShareSession(const char* sessionName)
+bool IsIShareSession(const char* sessionName)
 {
     regex_t regComp;
     if (regcomp(&regComp, ISHARE_SESSION_NAME, REG_EXTENDED | REG_NOSUB) != 0) {
