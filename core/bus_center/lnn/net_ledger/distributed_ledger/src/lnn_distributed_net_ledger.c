85dacb2a10f9dfe0d7a8139a785af36ad8f7f1f1
房冬 <fangdong7@huawei.com>
2024-12-10 11:33:50 +0000

    fix:clear device info key
    Signed-off-by: 房冬 <fangdong7@huawei.com>

--- core/bus_center/lnn/net_ledger/distributed_ledger/src/lnn_distributed_net_ledger.c
+++ core/bus_center/lnn/net_ledger/distributed_ledger/src/lnn_distributed_net_ledger.c
@@ -1051,7 +1051,7 @@ static void BleDirectlyOnlineProc(NodeInfo *info)
         AnonymizeFree(anonyDevNetworkId);
         AnonymizeFree(anonyNetworkId);
         GetAndSaveRemoteDeviceInfo(&deviceInfo, info);
-        (void)memset_s(&deviceInfo, sizeof(NodeInfo), 0, sizeof(NodeInfo);
+        (void)memset_s(&deviceInfo, sizeof(NodeInfo), 0, sizeof(NodeInfo));
         return;
     }
     if (LnnHasDiscoveryType(info, DISCOVERY_TYPE_WIFI)) {
