52104ed2e889cd543ed9efc04f6a6d04eeb9b33c
xzh457 <xiaozihan4@h-partners.com>
2024-12-15 17:36:56 +0800

    add datatype when on bind
    Signed-off-by: xzh457 <xiaozihan4@h-partners.com>
    Change-Id: Ia9d94fabceeb2f98c4b3bdff9bfa92d675acf95b

--- sdk/transmission/trans_channel/tcp_direct/src/client_trans_tcp_direct_callback.c
+++ sdk/transmission/trans_channel/tcp_direct/src/client_trans_tcp_direct_callback.c
@@ -39,7 +39,19 @@ int32_t ClientTransTdcSetCallBack(const IClientSessionCallBack *cb)
 
 int32_t ClientTransTdcOnSessionOpened(const char *sessionName, const ChannelInfo *channel)
 {
-    return g_sessionCb.OnSessionOpened(sessionName, channel, TYPE_BYTES);
+    SessionType type = TYPE_BUTT;
+    switch (channel->businessType) {
+        case BUSINESS_TYPE_MESSAGE:
+            type = TYPE_MESSAGE;
+            break;
+        case BUSINESS_TYPE_BYTE:
+            type = TYPE_BYTES;
+            break;
+        default:
+            TRANS_LOGE(TRANS_SDK, "unsupport businessType=%{public}d", channel->businessType);
+            return SOFTBUS_TRANS_BUSINESS_TYPE_NOT_MATCH;
+    }
+    return g_sessionCb.OnSessionOpened(sessionName, channel, type);
 }
 
 int32_t ClientTransTdcOnSessionClosed(int32_t channelId, ShutdownReason reason)
