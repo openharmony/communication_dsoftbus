6503872de807ec80fe281177182e164d308432db
姚望 <yaopwang@huawei.com>
2024-12-19 01:15:35 +0000

    update /unittest/trans_tcp_direct_message_static_test.cpp.
    Signed-off-by: 姚望 <yaopwang@huawei.com>

--- tests/core/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_message_static_test.cpp
+++ tests/core/transmission/trans_channel/tcp_direct/unittest/trans_tcp_direct_message_static_test.cpp
@@ -645,7 +645,7 @@ HWTEST_F(TransTcpDirectMessageStaticTest, OpenDataBusRequestOutSessionNameTest00
 {
     char *mySessionName = nullptr;
     char *peerSessionName = nullptr;
-    EXPECT_NO_THROW(OpenDataBusRequestOutSessionName(mySessionName, peerSessionName));
+    EXPECT_NO_FATAL_FAILURE(OpenDataBusRequestOutSessionName(mySessionName, peerSessionName));
 }
 
 /**
