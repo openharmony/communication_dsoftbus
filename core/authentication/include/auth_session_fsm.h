d91aec712d2ffb5c978e754a960fadc8e46dbd7b
song-chenxi050 <songchenxi2@huawei.com>
2024-12-27 06:36:58 +0000

    bug:modify fd double free
    Signed-off-by: song-chenxi050 <songchenxi2@huawei.com>

--- core/authentication/include/auth_session_fsm.h
+++ core/authentication/include/auth_session_fsm.h
@@ -134,7 +134,7 @@ int32_t AuthSessionProcessDevInfoDataByConnId(uint64_t connId, bool isServer, co
 int32_t AuthSessionProcessCloseAckByConnId(uint64_t connId, bool isServer, const uint8_t *data, uint32_t len);
 int32_t AuthSessionProcessCancelAuthByConnId(uint64_t connId, bool isConnectServer, const uint8_t *data, uint32_t len);
 int32_t AuthSessionHandleDeviceNotTrusted(const char *udid);
-int32_t AuthSessionHandleDeviceDisconnected(uint64_t connId);
+int32_t AuthSessionHandleDeviceDisconnected(uint64_t connId, bool isNeedDisconnect);
 int32_t AuthNotifyRequestVerify(int64_t authSeq);
 AuthFsm *GetAuthFsmByConnId(uint64_t connId, bool isServer, bool isConnectSide);
 void AuthSessionFsmExit(void);
