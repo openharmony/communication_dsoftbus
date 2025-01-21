bb12eb772eb2f03c0a37c95f4dde1de7cd7b5fc4
wangboyi <wangboyi4@huawei.com>
2025-01-21 10:19:49 +0800

    fix:modify disc alarm
    Signed-off-by: wangboyi <wangboyi4@huawei.com>

--- core/broadcast/common/src/broadcast_dfx_event.c
+++ core/broadcast/common/src/broadcast_dfx_event.c
@@ -25,7 +25,7 @@ void BroadcastDiscEvent(int32_t eventScene, int32_t eventStage, DiscEventExtra *
     }
  
     DiscEventExtra extra = { 0 };
-    uint64_t stamptime = SoftBusGetSysTimeMs();
+    int32_t stamptime = (int32_t)SoftBusGetSysTimeMs();
     for (int32_t i = 0; i < size; i++) {
         extra.capabilityBit = discExtra[i].capabilityBit;
         extra.discType = discExtra[i].discType;
