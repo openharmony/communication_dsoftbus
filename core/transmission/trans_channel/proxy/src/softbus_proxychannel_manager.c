c0b53f920a8713c4eb63562a74b5c131cf513d1e
wangyipeng <wangyipeng11@huawei.com>
2025-02-20 19:42:20 +0800

    fix:RegisterTimeoutCallback needs to be optimized for deregistering
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- core/transmission/trans_channel/proxy/src/softbus_proxychannel_manager.c
+++ core/transmission/trans_channel/proxy/src/softbus_proxychannel_manager.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -1977,9 +1977,8 @@ static void TransProxyManagerDeinitInner(void)
 
 void TransProxyManagerDeinit(void)
 {
-    TransProxyManagerDeinitInner();
-
     (void)RegisterTimeoutCallback(SOFTBUS_PROXYCHANNEL_TIMER_FUN, NULL);
+    TransProxyManagerDeinitInner();
 }
 
 static void TransProxyDestroyChannelList(const ListNode *destroyList)
