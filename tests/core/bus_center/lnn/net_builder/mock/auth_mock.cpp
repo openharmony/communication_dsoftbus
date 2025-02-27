1b4374c351e8da2203bf0bc83b3c02a67e14f31e
michael4096 <xusai4@huawei.com>
2025-02-27 08:19:14 +0000

    fix:add auth test mock func
    Signed-off-by: michael4096 <xusai4@huawei.com>

--- tests/core/bus_center/lnn/net_builder/mock/auth_mock.cpp
+++ tests/core/bus_center/lnn/net_builder/mock/auth_mock.cpp
@@ -81,13 +81,143 @@ int64_t AuthGetIdByConnInfo(const AuthConnInfo *connInfo, bool isServer, bool is
 {
     return GetAuthInterface()->AuthGetIdByConnInfo(connInfo, isServer, isMeta);
 }
+
 int32_t AuthFlushDevice(const char *uuid)
 {
     return GetAuthInterface()->AuthFlushDevice(uuid);
 }
+
 int32_t AuthSendKeepaliveOption(const char *uuid, ModeCycle cycle)
 {
     return GetAuthInterface()->AuthSendKeepaliveOption(uuid, cycle);
+}
+
+int32_t AuthStartConnVerify(const AuthConnInfo *connInfo, uint32_t requestId, const AuthConnCallback *connCallback,
+    AuthVerifyModule module, bool isFastAuth)
+{
+    return GetAuthInterface()->AuthStartConnVerify(connInfo, requestId, connCallback, module, isFastAuth);
+}
+
+int32_t AuthMetaStartVerify(uint32_t connectionId, const AuthKeyInfo *authKeyInfo, uint32_t requestId,
+    int32_t callingPid, const AuthVerifyCallback *callBack)
+{
+    return GetAuthInterface()->AuthMetaStartVerify(connectionId, authKeyInfo, requestId, callingPid, callBack);
+}
+
+void AuthMetaReleaseVerify(int64_t authId)
+{
+    return GetAuthInterface()->AuthMetaReleaseVerify(authId);
+}
+
+void AuthMetaStartVerify(const char *pkgName, int32_t pid)
+{
+    return GetAuthInterface()->AuthMetaStartVerify(pkgName, pid);
+}
+
+int32_t RegGroupChangeListener(const GroupChangeListener *listener)
+{
+    return GetAuthInterface()->RegGroupChangeListener(listener);
+}
+
+void UnregGroupChangeListener(void)
+{
+    return GetAuthInterface()->UnregGroupChangeListener();
+}
+
+bool AuthIsPotentialTrusted(const DeviceInfo *device)
+{
+    return GetAuthInterface()->AuthIsPotentialTrusted(device);
+}
+
+bool IsAuthHasTrustedRelation(void)
+{
+    return GetAuthInterface()->IsAuthHasTrustedRelation();
+}
+
+bool IsSameAccountDevice(const DeviceInfo *device)
+{
+    return GetAuthInterface()->IsSameAccountDevice(device);
+}
+
+bool AuthHasSameAccountGroup(void)
+{
+    return GetAuthInterface()->AuthHasSameAccountGroup();
+}
+
+int32_t AuthStartListening(AuthLinkType type, const char *ip, int32_t port)
+{
+    return GetAuthInterface()->AuthStartListening(type, ip, port);
+}
+
+void AuthStopListening(AuthLinkType type)
+{
+    return GetAuthInterface()->AuthStopListening(type);
+}
+
+int32_t AuthStartListeningForWifiDirect(AuthLinkType type, const char *ip, int32_t port, ListenerModule *moduleId)
+{
+    return GetAuthInterface()->AuthStartListeningForWifiDirect(type, ip, port, moduleId);
+}
+
+void AuthStopListeningForWifiDirect(AuthLinkType type, ListenerModule moduleId)
+{
+    return GetAuthInterface()->AuthStopListeningForWifiDirect(type, moduleId);
+}
+
+int32_t AuthOpenConn(const AuthConnInfo *info, uint32_t requestId, const AuthConnCallback *callback, bool isMeta)
+{
+    return GetAuthInterface()->AuthOpenConn(info, requestId, callback, isMeta);
+}
+
+void AuthCloseConn(AuthHandle authHandle)
+{
+    return GetAuthInterface()->AuthCloseConn(authHandle);
+}
+
+int32_t AuthGetPreferConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta)
+{
+    return GetAuthInterface()->AuthGetPreferConnInfo(uuid, connInfo, isMeta);
+}
+
+int32_t AuthGetConnInfoByType(const char *uuid, AuthLinkType type, AuthConnInfo *connInfo, bool isMeta)
+{
+    return GetAuthInterface()->AuthGetConnInfoByType(uuid, type, connInfo, isMeta);
+}
+
+int32_t AuthGetP2pConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta)
+{
+    return GetAuthInterface()->AuthGetP2pConnInfo(uuid, connInfo, isMeta);
+}
+
+int32_t AuthGetHmlConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta)
+{
+    return GetAuthInterface()->AuthGetHmlConnInfo(uuid, connInfo, isMeta);
+}
+
+int32_t AuthGetLatestAuthSeqList(const char *udid, int64_t *seqList, uint32_t num)
+{
+    return GetAuthInterface()->AuthGetLatestAuthSeqList(udid, seqList, num);
+}
+
+int32_t AuthGetLatestAuthSeqListByType(const char *udid, int64_t *seqList, uint64_t *authVerifyTime,
+    DiscoveryType type)
+{
+    return GetAuthInterface()->AuthGetLatestAuthSeqListByType(udid, seqList, authVerifyTime, type);
+}
+
+void AuthGetLatestIdByUuid(const char *uuid, AuthLinkType type, bool isMeta, AuthHandle *authHandle)
+{
+    return GetAuthInterface()->AuthGetLatestIdByUuid(uuid, type, isMeta, authHandle);
+}
+
+int32_t AuthGetAuthHandleByIndex(const AuthConnInfo *connInfo, bool isServer, int32_t index, AuthHandle *authHandle)
+{
+    return GetAuthInterface()->AuthGetAuthHandleByIndex(connInfo, isServer, index, authHandle);
+}
+
+int64_t AuthGetIdByUuid(const char *uuid, AuthLinkType type, bool isServer, bool isMeta)
+{
+    return GetAuthInterface()->AuthGetIdByUuid(uuid, type, isServer, isMeta);
 }
 }
 }
