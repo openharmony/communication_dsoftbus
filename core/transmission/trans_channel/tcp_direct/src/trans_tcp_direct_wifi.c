0ddb33512efb3ca0421e2bd4e5e49f642f53a66b
wangyipeng <wangyipeng11@huawei.com>
2025-03-06 09:55:46 +0800

    fix:fd leak optimization
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- core/transmission/trans_channel/tcp_direct/src/trans_tcp_direct_wifi.c
+++ core/transmission/trans_channel/tcp_direct/src/trans_tcp_direct_wifi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -131,6 +131,7 @@ int32_t OpenTcpDirectChannel(const AppInfo *appInfo, const ConnectOption *connIn
 
     int32_t ret = AddTcpConnAndSessionInfo(newchannelId, fd, newConn, module);
     if (ret != SOFTBUS_OK) {
+        ConnShutdownSocket(fd);
         return ret;
     }
     *channelId = newchannelId;
