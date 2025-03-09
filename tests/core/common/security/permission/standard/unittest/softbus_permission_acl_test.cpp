901e86fedf085919716525f740b7ee92c3670508
jiazhenyu <jiazhenyu1@huawei.com>
2025-03-09 16:24:07 +0800

    软总线传输安全加固UT代码
    Signed-off-by: jiazhenyu <jiazhenyu1@huawei.com>

--- tests/core/common/security/permission/standard/unittest/softbus_permission_acl_test.cpp
+++ tests/core/common/security/permission/standard/unittest/softbus_permission_acl_test.cpp
@@ -168,7 +168,7 @@ HWTEST_F(SoftbusPermissionACLTest, TransCheckClientAccessControl006, TestSize.Le
     EXPECT_CALL(mockIpc, GetCallingFullTokenID)
         .WillRepeatedly(Return(HAP_TOKENID));
     EXPECT_CALL(mockIpc, GetCallingUid)
-        .WillRepeatedly(Return(NATIVE_PID)); 
+        .WillRepeatedly(Return(NATIVE_PID));
 
     SoftbusPermissionACLInterfaceMock mockSPACL;
     EXPECT_CALL(mockSPACL, SoftBusGetAccessTokenType)
