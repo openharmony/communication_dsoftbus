ed59c84b905755a64e70619cff806be5767f6a2b
wangyipeng <wangyipeng11@huawei.com>
2025-03-15 16:33:01 +0800

    fix:UT Coverage
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- tests/sdk/transmission/trans_channel/udp/stream/libsoftbus_stream/vtp_stream_socket_test/softbus_stream_test_mock.h
+++ tests/sdk/transmission/trans_channel/udp/stream/libsoftbus_stream/vtp_stream_socket_test/softbus_stream_test_mock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,6 +22,8 @@
 #include "fillpinc.h"
 #include "fillptypes.h"
 #include "softbus_def.h"
+#include "stream_common.h"
+#include "stream_packetizer.h"
 
 namespace OHOS {
 class SoftBusStreamTestInterface {
@@ -40,6 +42,15 @@ public:
     virtual FILLP_INT FtEpollCreate(void) = 0;
     virtual FILLP_INT FtSendFrame(FILLP_INT fd, FILLP_CONST void *data, size_t size, FILLP_INT flag,
         FILLP_CONST struct FrameInfo *frame) = 0;
+    virtual FILLP_INT FtListen(FILLP_INT fd, FILLP_INT backLog) = 0;
+    virtual int32_t SoftBusGetTime(SoftBusSysTime *sysTime) = 0;
+    virtual bool Connect(const Communication::SoftBus::IpAndPort &remote) = 0;
+    virtual std::unique_ptr<char[]> PacketizeStream() = 0;
+    virtual ssize_t GetPacketLen() = 0;
+    virtual int SetSocketEpollMode(int fd) = 0;
+    virtual bool CreateClient(
+        Communication::SoftBus::IpAndPort &local, int streamType, std::pair<uint8_t *, uint32_t> sessionKey) = 0;
+    virtual int CreateAndBindSocket(Communication::SoftBus::IpAndPort &local, bool isServer) = 0;
 };
 
 class SoftBusStreamTestInterfaceMock : public SoftBusStreamTestInterface {
@@ -59,6 +70,15 @@ public:
     MOCK_METHOD0(FtEpollCreate, FILLP_INT (void));
     MOCK_METHOD5(FtSendFrame, FILLP_INT (FILLP_INT fd, FILLP_CONST void *data, size_t size,
         FILLP_INT flag, FILLP_CONST struct FrameInfo *frame));
+    MOCK_METHOD1(SoftBusGetTime, int32_t (SoftBusSysTime *sysTime));
+    MOCK_METHOD1(Connect, bool (const Communication::SoftBus::IpAndPort &remote));
+    MOCK_METHOD2(FtListen, FILLP_INT (FILLP_INT fd, FILLP_INT backLog));
+    MOCK_METHOD0(PacketizeStream, std::unique_ptr<char[]> ());
+    MOCK_METHOD0(GetPacketLen, ssize_t ());
+    MOCK_METHOD1(SetSocketEpollMode, int (int fd));
+    MOCK_METHOD3(CreateClient,
+        bool (Communication::SoftBus::IpAndPort &local, int streamType, std::pair<uint8_t *, uint32_t> sessionKey));
+    MOCK_METHOD2(CreateAndBindSocket, int (Communication::SoftBus::IpAndPort &local, bool isServer));
 };
 } // namespace OHOS
 #endif // SOFTBUS_STREAM_TEST_MOCK_H
