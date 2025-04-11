61b1fb589afb553cab45580414cc8c158d9a22c8
xingchu <weiqian22@huawei.com>
2025-04-11 10:11:38 +0000

    update core/authentication/src/auth_identity_service_adapter.c.
    Signed-off-by: xingchu <weiqian22@huawei.com>

--- core/authentication/src/auth_identity_service_adapter.c
+++ core/authentication/src/auth_identity_service_adapter.c
@@ -31,6 +31,7 @@
 #define FIELD_AUTHORIZED_SCOPE "authorizedScope"
 #define AUTH_APPID "softbus_auth"
 #define SCOPE_USER 2
+SHORT_UDID_HASH_LEN 8
 
 enum SoftbusCredType {
     ACCOUNT_RELATED = 1,
