cc06f6f009be8125786e6e187297728dbb174e08
jieyifeng <jieyifeng@huawei.com>
2025-04-18 19:25:18 +0800

    fix some quote
    Signed-off-by: jieyifeng <jieyifeng@huawei.com>

--- core/authentication/src/auth_manager.c
+++ core/authentication/src/auth_manager.c
@@ -745,14 +745,17 @@ bool RawLinkNeedUpdateAuthManager(char *uuid, bool isServer)
     bool haveEnhanceP2pAuth = false;
     AuthManager *item = FindAuthManagerByUuid(uuid, AUTH_LINK_TYPE_SESSION_KEY, isServer);
     if (item == NULL) {
+        AUTH_LOGI(AUTH_FSM, "not found session key auth manager");
         return false;
     }
     haveSessionAuth = true;
     if (item->connInfo[AUTH_LINK_TYPE_ENHANCED_P2P].type == AUTH_LINK_TYPE_ENHANCED_P2P &&
         item->hasAuthPassed[AUTH_LINK_TYPE_ENHANCED_P2P]) {
+        AUTH_LOGI(AUTH_FSM, "have enhance p2p auth, skip");
         haveEnhanceP2pAuth = true;
     }
     if (haveSessionAuth && !haveEnhanceP2pAuth) {
+        AUTH_LOGI(AUTH_FSM, "need add auth");
         return true;
     }
     return false;
