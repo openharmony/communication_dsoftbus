e7d97680283404eb24d67f9bef674a9ec75a2120
RichardHuang9527 <huangyiping8@huawei.com>
2025-05-20 20:14:39 +0800

    fix: optimize the code
    Signed-off-by: RichardHuang9527 <huangyiping8@huawei.com>

--- core/adapter/bus_center/src/lnn_oobe_manager_virtual.cpp
+++ core/adapter/bus_center/src/lnn_oobe_manager_virtual.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "lnn_oobe_manager.h"
 
+#include "lnn_oobe_manager.h"
 #include "softbus_error_code.h"
 
 int32_t LnnGetOOBEState(SoftBusOOBEState *state)
@@ -34,3 +34,4 @@ void RegisterOOBEMonitor(void *para)
 {
     (void)para;
 }
+
