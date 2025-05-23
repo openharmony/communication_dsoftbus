1d8d2f173698a9a887982d518dc07340b13aa5ec
yaowang <yaopwang@huawei.com>
2025-05-23 16:15:53 +0800

    0523
    Signed-off-by: yaowang <yaopwang@huawei.com>

--- core/transmission/session/src/softbus_scenario_manager_virtual.c
+++ core/transmission/session/src/softbus_scenario_manager_virtual.c
@@ -21,7 +21,7 @@ int32_t ScenarioManagerGetInstance(void)
 }
 
 // update scenarios based on the command and determine whether to deliver the driver.
-int32_t AddScenario(const char *localMac, const char *peerMac, int localPid, int businessType)
+int32_t AddScenario(const char *localMac, const char *peerMac, int32_t localPid, int32_t businessType)
 {
     (void)localMac;
     (void)peerMac;
@@ -30,7 +30,7 @@ int32_t AddScenario(const char *localMac, const char *peerMac, int localPid, int
     return 0;
 }
 
-int32_t DelScenario(const char *localMac, const char *peerMac, int localPid, int businessType)
+int32_t DelScenario(const char *localMac, const char *peerMac, int32_t localPid, int32_t businessType)
 {
     (void)localMac;
     (void)peerMac;
