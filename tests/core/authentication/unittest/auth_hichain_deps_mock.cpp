f34c416696b52eb38d1d315eed91107ec558167a
RichardHuang9527 <huangyiping8@huawei.com>
2025-05-28 17:33:44 +0800

    fix: code optimization
    Signed-off-by: RichardHuang9527 <huangyiping8@huawei.com>

--- tests/core/authentication/unittest/auth_hichain_deps_mock.cpp
+++ tests/core/authentication/unittest/auth_hichain_deps_mock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,6 +21,7 @@ using namespace testing::ext;
 namespace OHOS {
 constexpr int64_t TEST_AUTH_SEQ = 1;
 void *g_hichainMockInterface;
+
 AuthHichainInterfaceMock::AuthHichainInterfaceMock()
 {
     g_hichainMockInterface = reinterpret_cast<void *>(this);
