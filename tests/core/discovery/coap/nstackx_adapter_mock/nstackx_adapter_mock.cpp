300f6e66f3a4c0e20f011d5c00233930d8c18693
王勃懿 <wangboyi4@huawei.com>
2025-05-29 11:15:32 +0000

    update tests/core/discovery/coap/nstackx_adapter_mock/nstackx_adapter_mock.cpp.
    Signed-off-by: 王勃懿 <wangboyi4@huawei.com>

--- tests/core/discovery/coap/nstackx_adapter_mock/nstackx_adapter_mock.cpp
+++ tests/core/discovery/coap/nstackx_adapter_mock/nstackx_adapter_mock.cpp
@@ -15,13 +15,14 @@
 #include <securec.h>
 #include "nstackx_adapter_mock.h"
 #include "softbus_error_code.h"
+#include "disc_log.h"
 
-#define MOCK_PHONE_DEVICE_TYPE        14
-#define MOCK_ACCOUNT_ID               1234567890
-#define MOCK_DEVICE_ID                "ABCDEF11223344556677889900"
-#define MOCK_DEVICE_NAME              "deviceName"
-#define MOCK_AUTH_PORT                1234
-#define MOCK_WLAN_IP                  "192.168.0.1"
+const int32_t MOCK_PHONE_DEVICE_TYPE = 14;
+const int32_t MOCK_ACCOUNT_ID = 1234567890;
+const int32_t MOCK_AUTH_PORT = 1234;
+const char *MOCK_DEVICE_ID = "ABCDEF11223344556677889900";
+const char *MOCK_DEVICE_NAME = "deviceName";
+const char *MOCK_WLAN_IP = "192.168.0.1";
 
 AdapterMock::AdapterMock()
 {
@@ -33,37 +34,63 @@ AdapterMock::~AdapterMock()
     mock.store(nullptr);
 }
 
+int32_t NSTACKX_Init(const NSTACKX_Parameter *parameter)
+{
+    return AdapterMock::ActionOfNstackInit(parameter);
+}
+
 int32_t NSTACKX_RegisterServiceDataV2(const struct NSTACKX_ServiceData *param, uint32_t cnt)
 {
-    return AdapterMock::ActionRegisterServiceDataV2(param, cnt);
+    return AdapterMock::ActionOfRegisterServiceDataV2(param, cnt);
+}
+
+int32_t NSTACKX_RegisterCapability(uint32_t capabilityBitmapNum, uint32_t capabilityBitmap[])
+{
+    return SOFTBUS_OK;
+}
+
+int32_t NSTACKX_SetFilterCapability(uint32_t capabilityBitmapNum, uint32_t capabilityBitmap[])
+{
+    return SOFTBUS_OK;
+}
+
+int32_t NSTACKX_SendDiscoveryRsp(const NSTACKX_ResponseSettings *responseSettings)
+{
+    return SOFTBUS_OK;
 }
 
 int32_t LnnGetLocalNumInfo(InfoKey key, int32_t *info)
 {
-    return AdapterMock::ActionLnnGetLocalNumInfo(key, info);
+    return AdapterMock::ActionOfLnnGetLocalNumInfo(key, info);
 }
 
 int32_t LnnGetLocalNum64Info(InfoKey key, int64_t *info)
 {
-    return AdapterMock::ActionLnnGetLocalNum64Info(key, info);
+    return AdapterMock::ActionOfLnnGetLocalNum64Info(key, info);
 }
 
 int32_t LnnGetLocalStrInfo(InfoKey key, char *info, uint32_t len)
 {
-    return AdapterMock::ActionLnnGetLocalStrInfo(key, info, len);
+    return AdapterMock::ActionOfLnnGetLocalStrInfo(key, info, len);
 }
 
 int32_t LnnGetLocalNumInfoByIfnameIdx(InfoKey key, int32_t *info, int32_t ifIdx)
 {
-    return AdapterMock::ActionLnnGetLocalNumInfoByIfnameIdx(key, info, ifIdx);
+    return AdapterMock::ActionOfLnnGetLocalNumInfoByIfnameIdx(key, info, ifIdx);
 }
 
 int32_t LnnGetLocalStrInfoByIfnameIdx(InfoKey key, char *info, uint32_t len, int32_t ifIdx)
 {
-    return AdapterMock::ActionLnnGetLocalStrInfoByIfnameIdx(key, info, len, ifIdx);
+    return AdapterMock::ActionOfLnnGetLocalStrInfoByIfnameIdx(key, info, len, ifIdx);
 }
 
-int32_t AdapterMock::ActionRegisterServiceDataV2(const struct NSTACKX_ServiceData *param, uint32_t cnt)
+int32_t AdapterMock::ActionOfNstackInit(const NSTACKX_Parameter *parameter)
+{
+    deviceFoundCallback_ = *parameter;
+    return SOFTBUS_OK;
+}
+
+int32_t AdapterMock::ActionOfRegisterServiceDataV2(const struct NSTACKX_ServiceData *param, uint32_t cnt)
 {
     if (cnt == 0) {
         return SOFTBUS_INVALID_PARAM;
@@ -71,83 +98,87 @@ int32_t AdapterMock::ActionRegisterServiceDataV2(const struct NSTACKX_ServiceDat
     return SOFTBUS_OK;
 }
 
-int32_t AdapterMock::ActionLnnGetLocalNumInfo(InfoKey key, int32_t *info)
+int32_t AdapterMock::ActionOfLnnGetLocalNumInfo(InfoKey key, int32_t *info)
 {
     if (info == NULL) {
         return SOFTBUS_INVALID_PARAM;
     }
 
-    int32_t deviceType = MOCK_PHONE_DEVICE_TYPE;
     if (key == NUM_KEY_DEV_TYPE_ID) {
-        *info = deviceType;
+        *info = MOCK_PHONE_DEVICE_TYPE;
     }
     return SOFTBUS_OK;
 }
 
-int32_t AdapterMock::ActionLnnGetLocalNum64Info(InfoKey key, int64_t *info)
+int32_t AdapterMock::ActionOfLnnGetLocalNum64Info(InfoKey key, int64_t *info)
 {
     if (info == NULL) {
         return SOFTBUS_INVALID_PARAM;
     }
 
-    int64_t accountId = MOCK_ACCOUNT_ID;
     if (key == NUM_KEY_ACCOUNT_LONG) {
-        *info = accountId;
+        *info = MOCK_ACCOUNT_ID;
     }
     return SOFTBUS_OK;
 }
 
-int32_t AdapterMock::ActionLnnGetLocalStrInfo(InfoKey key, char *info, uint32_t len)
+int32_t AdapterMock::ActionOfLnnGetLocalStrInfo(InfoKey key, char *info, uint32_t len)
 {
     if (info == NULL) {
         return SOFTBUS_INVALID_PARAM;
     }
 
     if (key == STRING_KEY_DEV_UDID) {
-        (void)strncpy_s(info, len, MOCK_DEVICE_ID, len);
+        (void)strncpy_s(info, len, MOCK_DEVICE_ID, strlen(MOCK_DEVICE_ID));
     }
     if (key == STRING_KEY_DEV_NAME) {
-        (void)strncpy_s(info, len, MOCK_DEVICE_NAME, len);
+        (void)strncpy_s(info, len, MOCK_DEVICE_NAME, strlen(MOCK_DEVICE_NAME));
     }
     return SOFTBUS_OK;
 }
 
-int32_t AdapterMock::ActionLnnGetLocalNumInfoByIfnameIdx(InfoKey key, int32_t *info, int32_t ifIdx)
+int32_t AdapterMock::ActionOfLnnGetLocalNumInfoByIfnameIdx(InfoKey key, int32_t *info, int32_t ifIdx)
 {
     if (info == NULL) {
         return SOFTBUS_INVALID_PARAM;
     }
 
-    int32_t port = MOCK_AUTH_PORT;
     if (key == NUM_KEY_AUTH_PORT) {
-        *info = port;
+        *info = MOCK_AUTH_PORT;
     }
     return SOFTBUS_OK;
 }
 
-int32_t AdapterMock::ActionLnnGetLocalStrInfoByIfnameIdx(InfoKey key, char *info, uint32_t len, int32_t ifIdx)
+int32_t AdapterMock::ActionOfLnnGetLocalStrInfoByIfnameIdx(InfoKey key, char *info, uint32_t len, int32_t ifIdx)
 {
     if (info == NULL) {
         return SOFTBUS_INVALID_PARAM;
     }
 
     if (key == STRING_KEY_IP) {
-        (void)strncpy_s(info, len, MOCK_WLAN_IP, len);
+        (void)strncpy_s(info, len, MOCK_WLAN_IP, strlen(MOCK_WLAN_IP));
     }
     if (key == STRING_KEY_NET_IF_NAME) {
-        (void)strncpy_s(info, len, "wlan0", len);
+        (void)strncpy_s(info, len, "wlan0", strlen("wlan0"));
     }
     return SOFTBUS_OK;
 }
 
+void AdapterMock::InjectDeviceFoundEvent(const NSTACKX_DeviceInfo *deviceInfo, uint32_t deviceCount)
+{
+    if (deviceFoundCallback_.onDeviceListChanged) {
+        deviceFoundCallback_.onDeviceListChanged(deviceInfo, deviceCount);
+    }
+}
+
 void AdapterMock::SetupSuccessStub()
 {
-    EXPECT_CALL(*this, NSTACKX_RegisterServiceDataV2).WillRepeatedly(AdapterMock::ActionRegisterServiceDataV2);
-    EXPECT_CALL(*this, LnnGetLocalNumInfo).WillRepeatedly(AdapterMock::ActionLnnGetLocalNumInfo);
-    EXPECT_CALL(*this, LnnGetLocalNum64Info).WillRepeatedly(AdapterMock::ActionLnnGetLocalNum64Info);
-    EXPECT_CALL(*this, LnnGetLocalStrInfo).WillRepeatedly(AdapterMock::ActionLnnGetLocalStrInfo);
+    EXPECT_CALL(*this, NSTACKX_RegisterServiceDataV2).WillRepeatedly(AdapterMock::ActionOfRegisterServiceDataV2);
+    EXPECT_CALL(*this, LnnGetLocalNumInfo).WillRepeatedly(AdapterMock::ActionOfLnnGetLocalNumInfo);
+    EXPECT_CALL(*this, LnnGetLocalNum64Info).WillRepeatedly(AdapterMock::ActionOfLnnGetLocalNum64Info);
+    EXPECT_CALL(*this, LnnGetLocalStrInfo).WillRepeatedly(AdapterMock::ActionOfLnnGetLocalStrInfo);
     EXPECT_CALL(*this, LnnGetLocalNumInfoByIfnameIdx).
-        WillRepeatedly(AdapterMock::ActionLnnGetLocalNumInfoByIfnameIdx);
+        WillRepeatedly(AdapterMock::ActionOfLnnGetLocalNumInfoByIfnameIdx);
     EXPECT_CALL(*this, LnnGetLocalStrInfoByIfnameIdx).
-        WillRepeatedly(AdapterMock::ActionLnnGetLocalStrInfoByIfnameIdx);
+        WillRepeatedly(AdapterMock::ActionOfLnnGetLocalStrInfoByIfnameIdx);
 }
