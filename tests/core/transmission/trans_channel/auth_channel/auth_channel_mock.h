83904ff50241cd155bd8c0bdd812946e0bf0cd09
wangyipeng <wangyipeng11@huawei.com>
2025-06-13 10:31:58 +0800

    fix:UT Coverage Increase
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- tests/core/transmission/trans_channel/auth_channel/auth_channel_mock.h
+++ tests/core/transmission/trans_channel/auth_channel/auth_channel_mock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,10 +17,10 @@
 #define AUTH_CHANNEL_MOCK_H
 
 #include <gmock/gmock.h>
-#include <mutex>
 
-#include "lnn_settingdata_event_monitor.h"
 #include "lnn_net_builder.h"
+#include "softbus_json_utils.h"
+#include "softbus_utils.h"
 
 namespace OHOS {
 class AuthChannelInterface {
@@ -28,6 +28,10 @@ public:
     AuthChannelInterface() {};
     virtual ~AuthChannelInterface() {};
     virtual int32_t LnnServerJoinExt(ConnectionAddr *addr, LnnServerJoinExtCallBack *callback) = 0;
+    virtual bool AddNumberToJsonObject(cJSON *json, const char *const string, int32_t num) = 0;
+    virtual int32_t GenerateRandomStr(char *str, uint32_t size) = 0;
+    virtual bool AddStringToJsonObject(cJSON *json, const char * const string, const char *value) = 0;
+    virtual char *cJSON_PrintUnformatted(const cJSON *json) = 0;
 };
 
 class AuthChannelInterfaceMock : public AuthChannelInterface {
@@ -35,6 +39,10 @@ public:
     AuthChannelInterfaceMock();
     ~AuthChannelInterfaceMock() override;
     MOCK_METHOD2(LnnServerJoinExt, int32_t (ConnectionAddr *, LnnServerJoinExtCallBack *));
+    MOCK_METHOD3(AddNumberToJsonObject, bool (cJSON *, const char * const, int32_t));
+    MOCK_METHOD2(GenerateRandomStr, int32_t (char *, uint32_t));
+    MOCK_METHOD3(AddStringToJsonObject, bool (cJSON *json, const char * const string, const char *value));
+    MOCK_METHOD1(cJSON_PrintUnformatted, char * (const cJSON *json));
 };
 } // namespace OHOS
 #endif // AUTH_CHANNEL_MOCK_H
