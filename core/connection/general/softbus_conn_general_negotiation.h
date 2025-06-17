c4dcdee480f26e46f8b931f95f85fc67b6efeb71
wufengshan <wufengshan1@huawei.com>
2025-06-17 22:31:55 +0800

    fix gereral bug and add ut
    Signed-off-by: wufengshan <wufengshan1@huawei.com>

--- core/connection/general/softbus_conn_general_negotiation.h
+++ core/connection/general/softbus_conn_general_negotiation.h
@@ -25,7 +25,7 @@
 extern "C" {
 #endif
 
-#define GENERAL_NAME_LEN (30)
+#define GENERAL_NAME_LEN (256)
 #define BUNDLE_NAME_MAX (256)
 
 typedef enum {
@@ -41,7 +41,7 @@ typedef struct {
     int32_t msgType;
     int32_t localId;
     int32_t peerId;
-    int32_t headLen;
+    uint32_t headLen;
 } GeneralConnectionHead;
 
 typedef uint32_t GeneralConnAbilityBitSet;
