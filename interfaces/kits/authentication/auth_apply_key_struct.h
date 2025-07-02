ae464fb7d7a5940fefc005fb0ddd292c80881467
song-chenxi050 <songchenxi2@huawei.com>
2025-07-02 12:21:16 +0000

    feature:d2d apply key generate
    Signed-off-by: song-chenxi050 <songchenxi2@huawei.com>

---
+++ interfaces/kits/authentication/auth_apply_key_struct.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AUTH_APPLY_KEY_STRUCT_H
+#define AUTH_APPLY_KEY_STRUCT_H
+
+#include <stdint.h>
+
+#include "common_list.h"
+#include "softbus_adapter_crypto.h"
+#include "softbus_def.h"
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif
+
+#define D2D_UDID_HASH_STR_LEN    11
+#define D2D_ACCOUNT_HASH_STR_LEN 11
+#define D2D_APPLY_KEY_LEN        32
+
+typedef enum {
+    BUSINESS_TYPE_D2D = 0,
+    BUSINESS_TYPE_MAX,
+} RequestBusinessType;
+
+typedef struct {
+    RequestBusinessType type;
+    char udidHash[D2D_UDID_HASH_STR_LEN];
+    char accountHash[D2D_ACCOUNT_HASH_STR_LEN];
+} RequestBusinessInfo;
+
+typedef struct {
+    void (*onGenSuccess)(uint32_t requestId, uint8_t *applyKey, uint32_t applyKeyLen);
+    void (*onGenFailed)(uint32_t requestId, int32_t reason);
+} GenApplyKeyCallback;
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif
+#endif /* AUTH_APPLY_KEY_STRUCT_H */
