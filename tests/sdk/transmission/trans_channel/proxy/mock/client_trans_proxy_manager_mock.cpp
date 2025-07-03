7baecabc4683273434b28d45a467f4f822e37852
万俊豪 <xiao_021019@163.com>
2025-07-03 22:21:57 +0800

    add UT test.
    Signed-off-by: 万俊豪 <xiao_021019@163.com>

--- tests/sdk/transmission/trans_channel/proxy/mock/client_trans_proxy_manager_mock.cpp
+++ tests/sdk/transmission/trans_channel/proxy/mock/client_trans_proxy_manager_mock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,71 +40,87 @@ int32_t ClinetTransProxyFileManagerInit(void)
 {
     return GetClientTransProxyManagerInterface()->ClinetTransProxyFileManagerInit();
 }
+
 SoftBusList *CreateSoftBusList(void)
 {
     return GetClientTransProxyManagerInterface()->CreateSoftBusList();
 }
+
 int32_t PendingInit(int32_t type)
 {
     return GetClientTransProxyManagerInterface()->PendingInit(type);
 }
-uint32_t SoftBusHtoL1(uint32_t value)
+
+uint32_t SoftBusHtoLl(uint32_t value)
 {
-    return GetClientTransProxyManagerInterface()->SoftBusHtoL1(value);
+    return GetClientTransProxyManagerInterface()->SoftBusHtoLl(value);
 }
-uint32_t SoftBusHtoN1(uint32_t value)
+
+uint32_t SoftBusHtoNl(uint32_t value)
 {
-    return GetClientTransProxyManagerInterface()->SoftBusHtoN1(value);
+    return GetClientTransProxyManagerInterface()->SoftBusHtoNl(value);
 }
+
 uint32_t SoftBusNtoHl(uint32_t value)
 {
     return GetClientTransProxyManagerInterface()->SoftBusNtoHl(value);
 }
+
 int32_t ClientGetSessionIdByChannelId(int32_t channelId, int32_t channelType, int32_t *sessionId, bool isClosing)
 {
     return GetClientTransProxyManagerInterface()->ClientGetSessionIdByChannelId(
         channelId, channelType, sessionId, isClosing);
 }
+
 int32_t ClientGetSessionCallbackAdapterById(int32_t sessionId, SessionListenerAdapter *callbackAdapter, bool *isServer)
 {
     return GetClientTransProxyManagerInterface()->ClientGetSessionCallbackAdapterById(
         sessionId, callbackAdapter, isServer);
 }
+
 int32_t DeleteDataSeqInfoList(uint32_t dataSeq, int32_t channelId)
 {
     return GetClientTransProxyManagerInterface()->DeleteDataSeqInfoList(dataSeq, channelId);
 }
+
 int32_t GetSupportTlvAndNeedAckById(int32_t channelId, int32_t channelType, bool *supportTlv, bool *needAck)
 {
     return GetClientTransProxyManagerInterface()->GetSupportTlvAndNeedAckById(
         channelId, channelType, supportTlv, needAck);
 }
+
 int32_t ServerIpcSendMessage(int32_t channelId, int32_t channelType, const void *data, uint32_t len, int32_t msgType)
 {
     return GetClientTransProxyManagerInterface()->ServerIpcSendMessage(channelId, channelType, data, len, msgType);
 }
+
 int32_t TransProxySessionDataLenCheck(uint32_t dataLen, SessionPktType type)
 {
     return GetClientTransProxyManagerInterface()->TransProxySessionDataLenCheck(dataLen, type);
 }
-int32_t TransProxyPackTlvBytes(ProxyDataInfo *dataInfo, const char *sessionKey,
-    SessionPktType flag, int32_t seq, DataHeadTlvPacketHead *info)
+
+int32_t TransProxyPackTlvBytes(
+    ProxyDataInfo *dataInfo, const char *sessionKey, SessionPktType flag, int32_t seq, DataHeadTlvPacketHead *info)
 {
     return GetClientTransProxyManagerInterface()->TransProxyPackTlvBytes(dataInfo, sessionKey, flag, seq, info);
 }
-uint8_t *TransProxyPackData(ProxyDataInfo *dataInfo, uint32_t sliceNum,
-    SessionPktType pktType, uint32_t cnt, uint32_t *dataLen)
+
+uint8_t *TransProxyPackData(
+    ProxyDataInfo *dataInfo, uint32_t sliceNum, SessionPktType pktType, uint32_t cnt, uint32_t *dataLen)
 {
     return GetClientTransProxyManagerInterface()->TransProxyPackData(dataInfo, sliceNum, pktType, cnt, dataLen);
 }
+
 int32_t ProcPendingPacket(int32_t channelId, int32_t seqNum, int32_t type)
 {
     return GetClientTransProxyManagerInterface()->ProcPendingPacket(channelId, seqNum, type);
 }
+
 int32_t AddPendingPacket(int32_t channelId, int32_t seqNum, int32_t type)
 {
     return GetClientTransProxyManagerInterface()->AddPendingPacket(channelId, seqNum, type);
 }
+
 int32_t TransProxyDecryptPacketData(int32_t seq, ProxyDataInfo *dataInfo, const char *sessionKey)
 {
     return GetClientTransProxyManagerInterface()->TransProxyDecryptPacketData(seq, dataInfo, sessionKey);
