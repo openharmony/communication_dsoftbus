de4af9fa710cee8fe31aa5ed894130cdef8bcf2d
Lby <liboyu12@huawei.com>
2025-07-04 15:35:33 +0800

    传输补充fuzz
    Signed-off-by: Lby <liboyu12@huawei.com>

--- tests/br_proxy/fuzztest/brproxy_fuzzer/brproxy_fuzzer.h
+++ tests/br_proxy/fuzztest/brproxy_fuzzer/brproxy_fuzzer.h
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef BR_PROXY_FUZZER_H
-#define BR_PROXY_FUZZER_H
+#ifndef BRPROXY_FUZZER_H
+#define BRPROXY_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "br_proxy_fuzzer"
+#define FUZZ_PROJECT_NAME "brproxy_fuzzer"
 
 #endif
