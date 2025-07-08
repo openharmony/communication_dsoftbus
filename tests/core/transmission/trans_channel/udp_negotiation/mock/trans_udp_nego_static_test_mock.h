97a2347d3a8b65d5860496e918e57a62fd6aea1f
万俊豪 <xiao_021019@163.com>
2025-07-08 09:07:06 +0800

    add UT test
    Signed-off-by: 万俊豪 <xiao_021019@163.com>

---
+++ tests/core/transmission/trans_channel/udp_negotiation/mock/trans_udp_nego_static_test_mock.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TRANS_UDP_NEGO_STATIC_TEST_MOCK_H
+#define TRANS_UDP_NEGO_STATIC_TEST_MOCK_H
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "stdint.h"
+#include "auth_interface_struct.h"
+#include "bus_center_manager.h"
+#include "trans_channel_common.h"
+#include "softbus_scenario_manager.h"
+#include "trans_udp_negotiation_exchange.h"
+#include "lnn_distributed_net_ledger.h"
+#include "trans_udp_channel_manager.h"
+
+namespace OHOS {
+class TransUdpNegoStaticInterface {
+public:
+    TransUdpNegoStaticInterface() {};
+    virtual ~TransUdpNegoStaticInterface() {};
+    virtual int32_t LnnGetLocalStrInfoByIfnameIdx(InfoKey key, char *info, uint32_t len, int32_t ifIdx) = 0;
+    virtual int32_t AddScenario(const char *localMac, const char *peerMac, int32_t localPid, int32_t businessType)= 0;
+    virtual int32_t TransUnpackRequestUdpInfo(const cJSON *msg, AppInfo *appInfo) = 0;
+    virtual int32_t AuthGetDeviceUuid(int64_t authId, char *uuid, uint16_t size) = 0;
+    virtual int32_t TransSetUdpChannelStatus(int64_t seq, UdpChannelStatus status, bool isReply) = 0;
+    virtual int32_t TransGetUdpChannelBySeq(int64_t seq, UdpChannelInfo *channel, bool isReply) = 0;
+    virtual int32_t LnnGetRemoteNodeInfoById(const char *id, IdCategory type, NodeInfo *info) = 0;
+    virtual int32_t LnnGetLocalStrInfo(InfoKey key, char *info, uint32_t len) = 0;
+    virtual int32_t AuthGetConnInfoBySide(const char *uuid, AuthConnInfo *connInfo, bool isMeta, bool isClient) = 0;
+    virtual int32_t AuthGetP2pConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta) = 0;
+    virtual int32_t AuthGetHmlConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta) = 0;
+    virtual int32_t AuthGetUsbConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta) = 0;
+    virtual int32_t AuthGetPreferConnInfo(const char *uuid, AuthConnInfo *connInfo, bool isMeta) = 0;
+    virtual int32_t AuthOpenConn(
+        const AuthConnInfo *info, uint32_t requestId, const AuthConnCallback *callback, bool isMeta) = 0;
+    virtual int32_t TransGetUdpChannelByRequestId(uint32_t requestId, UdpChannelInfo *channel) = 0;
+    virtual int32_t TransGetUdpChannelById(int32_t channelId, UdpChannelInfo *channel);
+};
+
+class TransUdpNegoStaticInterfaceMock : public TransUdpNegoStaticInterface {
+public:
+    TransUdpNegoStaticInterfaceMock();
+    ~TransUdpNegoStaticInterfaceMock() override;
+    MOCK_METHOD4(LnnGetLocalStrInfoByIfnameIdx, int32_t (InfoKey key, char *info, uint32_t len, int32_t ifIdx));
+    MOCK_METHOD4(AddScenario, int32_t (
+        const char *localMac, const char *peerMac, int32_t localPid, int32_t businessType));
+    MOCK_METHOD2(TransUnpackRequestUdpInfo, int32_t (const cJSON *msg, AppInfo *appInfo));
+    MOCK_METHOD3(AuthGetDeviceUuid, int32_t (int64_t authId, char *uuid, uint16_t size));
+    MOCK_METHOD3(TransSetUdpChannelStatus, int32_t (int64_t seq, UdpChannelStatus status, bool isReply));
+    MOCK_METHOD3(TransGetUdpChannelBySeq, int32_t (int64_t seq, UdpChannelInfo *channel, bool isReply));
+    MOCK_METHOD3(LnnGetRemoteNodeInfoById, int32_t (const char *id, IdCategory type, NodeInfo *info));
+    MOCK_METHOD3(LnnGetLocalStrInfo, int32_t (InfoKey key, char *info, uint32_t len));
+    MOCK_METHOD4(AuthGetConnInfoBySide, int32_t (
+        const char *uuid, AuthConnInfo *connInfo, bool isMeta, bool isClient));
+    MOCK_METHOD3(AuthGetP2pConnInfo, int32_t (const char *uuid, AuthConnInfo *connInfo, bool isMeta));
+    MOCK_METHOD3(AuthGetHmlConnInfo, int32_t (const char *uuid, AuthConnInfo *connInfo, bool isMeta));
+    MOCK_METHOD3(AuthGetUsbConnInfo, int32_t (const char *uuid, AuthConnInfo *connInfo, bool isMeta));
+    MOCK_METHOD3(AuthGetPreferConnInfo, int32_t (const char *uuid, AuthConnInfo *connInfo, bool isMeta));
+    MOCK_METHOD4(AuthOpenConn, int32_t (
+        const AuthConnInfo *info, uint32_t requestId, const AuthConnCallback *callback, bool isMeta));
+    MOCK_METHOD2(TransGetUdpChannelByRequestId, int32_t (uint32_t requestId, UdpChannelInfo *channel));
+    MOCK_METHOD2(TransGetUdpChannelById, int32_t (int32_t channelId, UdpChannelInfo *channel));
+};
+} // namespace OHOS
+#endif // TRANS_UDP_NEGO_STATIC_TEST_MOCK_H
