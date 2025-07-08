c86e2307177dedc05f7694cacb6b87321f89ad6a
RichardHuang9527 <huangyiping8@huawei.com>
2025-07-08 18:12:26 +0800

    Fix header file issues.
    Signed-off-by: RichardHuang9527 <huangyiping8@huawei.com>

--- interfaces/kits/lnn/enhance/lnn_fast_offline_struct.h
+++ interfaces/kits/lnn/enhance/lnn_fast_offline_struct.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,7 @@
 
 #include <stdint.h>
 
-#include "../lnn_node_info_struct.h"
+#include "lnn_node_info_struct.h"
 #include "softbus_common.h"
 #ifdef __cplusplus
 extern "C" {
