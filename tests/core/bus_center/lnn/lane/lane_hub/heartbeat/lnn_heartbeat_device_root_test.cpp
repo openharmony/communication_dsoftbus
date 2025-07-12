8ec49025216a734e5942f6b6355cd854e083717e
wang_mingxu <wangmingxu9@huawei.com>
2025-07-12 08:05:52 +0000

    update tests/core/bus_center/lnn/lane/lane_hub/heartbeat/lnn_heartbeat_device_root_test.cpp.
    Signed-off-by: wang_mingxu <wangmingxu9@huawei.com>

--- tests/core/bus_center/lnn/lane/lane_hub/heartbeat/lnn_heartbeat_device_root_test.cpp
+++ tests/core/bus_center/lnn/lane/lane_hub/heartbeat/lnn_heartbeat_device_root_test.cpp
@@ -12,3 +12,99 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <securec.h>
+#include <thread>
+
+#include "bus_center_event.h"
+#include "bus_center_manager.h"
+#include "lnn_connection_fsm.h"
+#include "lnn_device_info.h"
+#include "lnn_heartbeat_ctrl.c"
+#include "lnn_heartbeat_satic_mock.h"
+#include "lnn_heartbeat_strategy.h"
+#include "lnn_node_info.h"
+#include "message_handler.h"
+#include "softbus_adapter_bt_common_struct.h"
+#include "softbus_error_code.h"
+
+namespace OHOS {
+using namespace testing;
+using namespace testing::ext;
+
+NodeInfo nodeinfo1;
+
+class LnnHeartBeatStaticTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+
+    static void TearDownTestCase();
+
+    void SetUp();
+
+    void TearDown();
+};
+
+void LnnHeartBeatStaticTest::SetUpTestCase()
+{
+}
+
+void LnnHeartBeatStaticTest::TearDownTestCase()
+{
+}
+
+void LnnHeartBeatStaticTest::SetUp()
+{}
+
+void LnnHeartBeatStaticTest::TearDown()
+{}
+
+/*
+ * @tc.name: HbRootDeviceLeaveLnnTest01
+ * @tc.desc: use abnomal parameter
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(LnnHeartBeatStaticTest, HbRootDeviceLeaveLnnTest01, TestSize.Level1)
+{
+    int ret = 0;
+    NiceMock<LnnHeatbeatStaticInterfaceMock> LnnMock;
+
+    EXPECT_CALL(LnnMock, LnnGetAllOnlineNodeInfo).WillRepeatedly(Return(SOFTBUS_ERR));
+    ret = HbRootDeviceLeaveLnn();
+    EXPECT_EQ(ret, SOFTBUS_NETWORK_GET_ALL_NODE_INFO_ERR);
+
+    EXPECT_CALL(LnnMock, LnnGetAllOnlineNodeInfo).WillRepeatedly(Return(SOFTBUS_OK));
+    ret = HbRootDeviceLeaveLnn();
+    EXPECT_EQ(ret, SOFTBUS_NO_ONLINE_DEVICE);
+}
+
+/*
+ * @tc.name: HbDeviceRootStateEventHandlerTest01
+ * @tc.desc: use abnomal parameter
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(LnnHeartBeatStaticTest, HbDeviceRootStateEventHandlerTest01, TestSize.Level1)
+{
+    LnnDeviceRootStateChangeEvent *event =
+        reinterpret_cast<LnnDeviceRootStateChangeEvent*>(SoftBusCalloc(sizeof(LnnDeviceRootStateChangeEvent)));
+    event->basic.event = LNN_EVENT_TYPE_MAX;
+    event->status = SOFTBUS_DEVICE_IS_ROOT;
+    g_hbConditionState.deviceRootState = SOFTBUS_DEVICE_IS_ROOT;
+    HbDeviceRootStateEventHandler(NULL);
+
+    HbDeviceRootStateEventHandler(&event->basic);
+
+    event->basic.event = LNN_EVENT_DEVICE_ROOT_STATE_CHANGED;
+    HbDeviceRootStateEventHandler(&event->basic);
+
+    g_hbConditionState.deviceRootState = SOFTBUS_DEVICE_NOT_ROOT;
+    event->status = SOFTBUS_DEVICE_NOT_ROOT;
+    HbDeviceRootStateEventHandler(&event->basic);
+
+    EXPECT_EQ(event->status, 0);
+}
+}  // namespace OHOS
