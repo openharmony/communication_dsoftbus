98004f0be2f24bab334efc46445a8b490e5f735d
law <liyuhang47@huawei.com>
2025-07-13 07:37:08 +0000

    update interfaces/kits/connect/wifi_direct_types_struct.h.
    Signed-off-by: law <liyuhang47@huawei.com>

--- interfaces/kits/connect/wifi_direct_types_struct.h
+++ interfaces/kits/connect/wifi_direct_types_struct.h
@@ -13,231 +13,231 @@
  * limitations under the License.
  */
 
- #ifndef WIFI_DIRECT_TYPES_STRUCT_H
- #define WIFI_DIRECT_TYPES_STRUCT_H
- 
- #include <stdint.h>
- #include <stdbool.h>
- #include <stddef.h>
- 
- #include "softbus_common.h"
- #include "wifi_direct_defines_struct.h"
- #include "wifi_direct_error_code_struct.h"
- 
- #ifndef NULL
- #define NULL 0
- #endif
- 
- enum WifiDirectConnectType {
-     WIFI_DIRECT_CONNECT_TYPE_AUTH_NEGO_P2P = 0,
-     WIFI_DIRECT_CONNECT_TYPE_AUTH_NEGO_HML = 1,
- 
-     WIFI_DIRECT_CONNECT_TYPE_BLE_TRIGGER_HML = 2,
-     WIFI_DIRECT_CONNECT_TYPE_AUTH_TRIGGER_HML = 3,
-     WIFI_DIRECT_CONNECT_TYPE_ACTION_TRIGGER_HML = 4,
-     WIFI_DIRECT_CONNECT_TYPE_SPARKLINK_TRIGGER_HML = 5,
- };
- 
- enum WifiDirectRole {
-     WIFI_DIRECT_ROLE_AUTO = 1,
-     WIFI_DIRECT_ROLE_GO = 2,
-     WIFI_DIRECT_ROLE_GC = 3,
-     WIFI_DIRECT_ROLE_BRIDGE_GC = 4,
-     WIFI_DIRECT_ROLE_NONE = 5,
-     WIFI_DIRECT_ROLE_HML = 6,
-     WIFI_DIRECT_ROLE_INVALID = 7,
- };
- 
- enum WifiDirectApiRole {
-     WIFI_DIRECT_API_ROLE_NONE = 0,
-     WIFI_DIRECT_API_ROLE_STA = 1,
-     WIFI_DIRECT_API_ROLE_AP = 2,
-     WIFI_DIRECT_API_ROLE_GO = 4,
-     WIFI_DIRECT_API_ROLE_GC = 8,
-     WIFI_DIRECT_API_ROLE_HML = 16,
- };
- 
- enum P2pGroupConfigIndex {
-     P2P_GROUP_CONFIG_INDEX_SSID = 0,
-     P2P_GROUP_CONFIG_INDEX_BSSID = 1,
-     P2P_GROUP_CONFIG_INDEX_SHARE_KEY = 2,
-     P2P_GROUP_CONFIG_INDEX_FREQ = 3,
-     P2P_GROUP_CONFIG_INDEX_MODE = 4,
-     P2P_GROUP_CONFIG_INDEX_MAX,
- };
- 
- enum WifiDirectLinkType {
-     WIFI_DIRECT_LINK_TYPE_INVALID = -1,
-     WIFI_DIRECT_LINK_TYPE_P2P,
-     WIFI_DIRECT_LINK_TYPE_HML,
- };
- 
- enum WifiDirectBandWidth {
-     BAND_WIDTH_RANDOM = 0x0,
-     BAND_WIDTH_20M,
-     BAND_WIDTH_40M,
-     BAND_WIDTH_80M,
-     BAND_WIDTH_80P80M,
-     BAND_WIDTH_160M,
-     BAND_WIDTH_BUTT = 0xFF,
- };
- 
- typedef enum {
-     CONN_HML_CAP_UNKNOWN = -1,
-     CONN_HML_SUPPORT = 0,
-     CONN_HML_NOT_SUPPORT = 1,
- } HmlCapabilityCode;
- 
- typedef enum {
-     CONN_VSP_CAP_UNKNOWN = -1,
-     CONN_VSP_SUPPORT = 0,
-     CONN_VSP_NOT_SUPPORT = 1,
- } VspCapabilityCode;
- 
- typedef enum {
-     CONN_HML_ENABLED,
-     CONN_HML_DISABLED,
-     CONN_HML_STATE_MAX,
- } SoftBusHmlState;
- 
- typedef enum {
-     INVALID_POWER,
-     LOW_POWER,
-     DEFAULT_POWER,
- } LinkPowerMode;
- 
- struct WifiDirectLink {
-     int32_t linkId;
-     char localIp[IP_STR_MAX_LEN];
-     char remoteIp[IP_STR_MAX_LEN];
-     enum WifiDirectLinkType linkType;
-     enum WifiDirectBandWidth bandWidth;
-     bool isReuse;
-     int channelId;
-     // localIpv6 and remoteIpv6 is only used with remotePort
-     char localIpv6[IP_STR_MAX_LEN];
-     char remoteIpv6[IP_STR_MAX_LEN];
-     int32_t remotePort;
- };
- 
- struct WifiDirectSinkLink {
-     char remoteUuid[UUID_BUF_LEN];
-     int channelId;
-     enum WifiDirectBandWidth bandWidth;
-     enum WifiDirectLinkType linkType;
- 
-     char localIp[IP_STR_MAX_LEN];
-     char remoteIp[IP_STR_MAX_LEN];
- 
-     char remoteMac[MAC_ADDR_STR_LEN];
- };
- 
- enum WifiDirectNegoChannelType {
-     NEGO_CHANNEL_NULL = 0,
-     NEGO_CHANNEL_AUTH = 1,
-     NEGO_CHANNEL_COC = 2,
-     NEGO_CHANNEL_ACTION = 3,
-     NEGO_CHANNEL_SHARE = 4,
- };
- 
- struct WifiDirectNegotiateChannel {
-     enum WifiDirectNegoChannelType type;
-     union {
-         AuthHandle authHandle;
-         int32_t channelId;
-         uint32_t actionAddr;
-     } handle;
- };
- 
- enum IpAddrType {
-     IPV4,
-     IPV6
- };
- 
- enum StatisticLinkType {
-     STATISTIC_P2P = 0,
-     STATISTIC_HML = 1,
-     STATISTIC_TRIGGER_HML = 2,
-     STATISTIC_LINK_TYPE_NUM = 3,
- };
- 
- enum StatisticBootLinkType {
-     STATISTIC_NONE = 0,
-     STATISTIC_WLAN = 1,
-     STATISTIC_BLE = 2,
-     STATISTIC_BR = 3,
-     STATISTIC_COC = 4,
-     STATISTIC_ACTION = 5,
-     STATISTIC_BLE_AND_ACTION = 6,
-     STATISTIC_BLE_TRIGGER = 7,
-     STATISTIC_RENEGOTIATE = 8,
-     STATISTIC_BOOT_LINK_TYPE_NUM = 9,
- };
- 
- struct WifiDirectDfxInfo {
-     enum StatisticLinkType linkType;
-     enum StatisticBootLinkType bootLinkType;
-     int renegotiate;
-     int reuse;
-     int costTime;
-     uint16_t challengeCode;
-     int frequency;
-     int staChannel;
-     int hmlChannel;
-     int p2pChannel;
-     int apChannel;
- };
- 
- struct WifiDirectConnectInfo {
-     uint32_t requestId;
-     int32_t pid;
-     enum WifiDirectConnectType connectType;
-     bool isStrictProtocol; // for HMLv1 | P2Pv2
-     struct WifiDirectNegotiateChannel negoChannel;
-     bool reuseOnly;
-     uint32_t expectApiRole;
-     bool isStrict;
-     char remoteNetworkId[NETWORK_ID_BUF_LEN];
-     char remoteMac[MAC_ADDR_STR_LEN];
-     bool isNetworkDelegate;
-     bool ratePreference;
-     int32_t bandWidth;
-     enum IpAddrType ipAddrType;
- 
-     struct WifiDirectDfxInfo dfxInfo;
-     bool isVirtualLink;
- };
- 
- struct WifiDirectDisconnectInfo {
-     uint32_t requestId;
-     int32_t pid;
-     int32_t linkId;
-     struct WifiDirectNegotiateChannel negoChannel;
- };
- 
- struct WifiDirectForceDisconnectInfo {
-     uint32_t requestId;
-     int32_t pid;
-     char remoteUuid[UUID_BUF_LEN];
-     enum WifiDirectLinkType linkType;
-     struct WifiDirectNegotiateChannel negoChannel;
- };
- 
- struct WifiDirectConnectCallback {
-     void (*onConnectSuccess)(uint32_t requestId, const struct WifiDirectLink *link);
-     void (*onConnectFailure)(uint32_t requestId, int32_t reason);
- };
- 
- struct WifiDirectDisconnectCallback {
-     void (*onDisconnectSuccess)(uint32_t requestId);
-     void (*onDisconnectFailure)(uint32_t requestId, int32_t reason);
- };
- 
- #ifdef __cplusplus
- extern "C" {
- #endif
- 
- #ifdef __cplusplus
- }
- #endif
- #endif
+#ifndef WIFI_DIRECT_TYPES_STRUCT_H
+#define WIFI_DIRECT_TYPES_STRUCT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "softbus_common.h"
+#include "wifi_direct_defines_struct.h"
+#include "wifi_direct_error_code_struct.h"
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+enum WifiDirectConnectType {
+    WIFI_DIRECT_CONNECT_TYPE_AUTH_NEGO_P2P = 0,
+    WIFI_DIRECT_CONNECT_TYPE_AUTH_NEGO_HML = 1,
+
+    WIFI_DIRECT_CONNECT_TYPE_BLE_TRIGGER_HML = 2,
+    WIFI_DIRECT_CONNECT_TYPE_AUTH_TRIGGER_HML = 3,
+    WIFI_DIRECT_CONNECT_TYPE_ACTION_TRIGGER_HML = 4,
+    WIFI_DIRECT_CONNECT_TYPE_SPARKLINK_TRIGGER_HML = 5,
+};
+
+enum WifiDirectRole {
+    WIFI_DIRECT_ROLE_AUTO = 1,
+    WIFI_DIRECT_ROLE_GO = 2,
+    WIFI_DIRECT_ROLE_GC = 3,
+    WIFI_DIRECT_ROLE_BRIDGE_GC = 4,
+    WIFI_DIRECT_ROLE_NONE = 5,
+    WIFI_DIRECT_ROLE_HML = 6,
+    WIFI_DIRECT_ROLE_INVALID = 7,
+};
+
+enum WifiDirectApiRole {
+    WIFI_DIRECT_API_ROLE_NONE = 0,
+    WIFI_DIRECT_API_ROLE_STA = 1,
+    WIFI_DIRECT_API_ROLE_AP = 2,
+    WIFI_DIRECT_API_ROLE_GO = 4,
+    WIFI_DIRECT_API_ROLE_GC = 8,
+    WIFI_DIRECT_API_ROLE_HML = 16,
+};
+
+enum P2pGroupConfigIndex {
+    P2P_GROUP_CONFIG_INDEX_SSID = 0,
+    P2P_GROUP_CONFIG_INDEX_BSSID = 1,
+    P2P_GROUP_CONFIG_INDEX_SHARE_KEY = 2,
+    P2P_GROUP_CONFIG_INDEX_FREQ = 3,
+    P2P_GROUP_CONFIG_INDEX_MODE = 4,
+    P2P_GROUP_CONFIG_INDEX_MAX,
+};
+
+enum WifiDirectLinkType {
+    WIFI_DIRECT_LINK_TYPE_INVALID = -1,
+    WIFI_DIRECT_LINK_TYPE_P2P,
+    WIFI_DIRECT_LINK_TYPE_HML,
+};
+
+enum WifiDirectBandWidth {
+    BAND_WIDTH_RANDOM = 0x0,
+    BAND_WIDTH_20M,
+    BAND_WIDTH_40M,
+    BAND_WIDTH_80M,
+    BAND_WIDTH_80P80M,
+    BAND_WIDTH_160M,
+    BAND_WIDTH_BUTT = 0xFF,
+};
+
+typedef enum {
+    CONN_HML_CAP_UNKNOWN = -1,
+    CONN_HML_SUPPORT = 0,
+    CONN_HML_NOT_SUPPORT = 1,
+} HmlCapabilityCode;
+
+typedef enum {
+    CONN_VSP_CAP_UNKNOWN = -1,
+    CONN_VSP_SUPPORT = 0,
+    CONN_VSP_NOT_SUPPORT = 1,
+} VspCapabilityCode;
+
+typedef enum {
+    CONN_HML_ENABLED,
+    CONN_HML_DISABLED,
+    CONN_HML_STATE_MAX,
+} SoftBusHmlState;
+
+typedef enum {
+    INVALID_POWER,
+    LOW_POWER,
+    DEFAULT_POWER,
+} LinkPowerMode;
+
+struct WifiDirectLink {
+    int32_t linkId;
+    char localIp[IP_STR_MAX_LEN];
+    char remoteIp[IP_STR_MAX_LEN];
+    enum WifiDirectLinkType linkType;
+    enum WifiDirectBandWidth bandWidth;
+    bool isReuse;
+    int channelId;
+    // localIpv6 and remoteIpv6 is only used with remotePort
+    char localIpv6[IP_STR_MAX_LEN];
+    char remoteIpv6[IP_STR_MAX_LEN];
+    int32_t remotePort;
+};
+
+struct WifiDirectSinkLink {
+    char remoteUuid[UUID_BUF_LEN];
+    int channelId;
+    enum WifiDirectBandWidth bandWidth;
+    enum WifiDirectLinkType linkType;
+
+    char localIp[IP_STR_MAX_LEN];
+    char remoteIp[IP_STR_MAX_LEN];
+
+    char remoteMac[MAC_ADDR_STR_LEN];
+};
+
+enum WifiDirectNegoChannelType {
+    NEGO_CHANNEL_NULL = 0,
+    NEGO_CHANNEL_AUTH = 1,
+    NEGO_CHANNEL_COC = 2,
+    NEGO_CHANNEL_ACTION = 3,
+    NEGO_CHANNEL_SHARE = 4,
+};
+
+struct WifiDirectNegotiateChannel {
+    enum WifiDirectNegoChannelType type;
+    union {
+        AuthHandle authHandle;
+        int32_t channelId;
+        uint32_t actionAddr;
+    } handle;
+};
+
+enum IpAddrType {
+    IPV4,
+    IPV6
+};
+
+enum StatisticLinkType {
+    STATISTIC_P2P = 0,
+    STATISTIC_HML = 1,
+    STATISTIC_TRIGGER_HML = 2,
+    STATISTIC_LINK_TYPE_NUM = 3,
+};
+
+enum StatisticBootLinkType {
+    STATISTIC_NONE = 0,
+    STATISTIC_WLAN = 1,
+    STATISTIC_BLE = 2,
+    STATISTIC_BR = 3,
+    STATISTIC_COC = 4,
+    STATISTIC_ACTION = 5,
+    STATISTIC_BLE_AND_ACTION = 6,
+    STATISTIC_BLE_TRIGGER = 7,
+    STATISTIC_RENEGOTIATE = 8,
+    STATISTIC_BOOT_LINK_TYPE_NUM = 9,
+};
+
+struct WifiDirectDfxInfo {
+    enum StatisticLinkType linkType;
+    enum StatisticBootLinkType bootLinkType;
+    int renegotiate;
+    int reuse;
+    int costTime;
+    uint16_t challengeCode;
+    int frequency;
+    int staChannel;
+    int hmlChannel;
+    int p2pChannel;
+    int apChannel;
+};
+
+struct WifiDirectConnectInfo {
+    uint32_t requestId;
+    int32_t pid;
+    enum WifiDirectConnectType connectType;
+    bool isStrictProtocol; // for HMLv1 | P2Pv2
+    struct WifiDirectNegotiateChannel negoChannel;
+    bool reuseOnly;
+    uint32_t expectApiRole;
+    bool isStrict;
+    char remoteNetworkId[NETWORK_ID_BUF_LEN];
+    char remoteMac[MAC_ADDR_STR_LEN];
+    bool isNetworkDelegate;
+    bool ratePreference;
+    int32_t bandWidth;
+    enum IpAddrType ipAddrType;
+
+    struct WifiDirectDfxInfo dfxInfo;
+    bool isVirtualLink;
+};
+
+struct WifiDirectDisconnectInfo {
+    uint32_t requestId;
+    int32_t pid;
+    int32_t linkId;
+    struct WifiDirectNegotiateChannel negoChannel;
+};
+
+struct WifiDirectForceDisconnectInfo {
+    uint32_t requestId;
+    int32_t pid;
+    char remoteUuid[UUID_BUF_LEN];
+    enum WifiDirectLinkType linkType;
+    struct WifiDirectNegotiateChannel negoChannel;
+};
+
+struct WifiDirectConnectCallback {
+    void (*onConnectSuccess)(uint32_t requestId, const struct WifiDirectLink *link);
+    void (*onConnectFailure)(uint32_t requestId, int32_t reason);
+};
+
+struct WifiDirectDisconnectCallback {
+    void (*onDisconnectSuccess)(uint32_t requestId);
+    void (*onDisconnectFailure)(uint32_t requestId, int32_t reason);
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
