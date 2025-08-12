7ee1ea0c0550d946a2fa1df2704bcc78a72d1d98
lbp <libaoping1@huawei.com>
2025-08-12 17:13:43 +0800

    fix:Optimize use case code
    Signed-off-by: lbp <libaoping1@huawei.com>

--- interfaces/kits/authentication/auth_connection_struct.h
+++ interfaces/kits/authentication/auth_connection_struct.h
@@ -1,17 +1,17 @@
 /*
-* Copyright (c) 2022-2025 Huawei Device Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef AUTH_CONNECTION_STRUCT_H
 #define AUTH_CONNECTION_STRUCT_H
@@ -30,24 +30,24 @@ extern "C" {
 #endif
 
 typedef struct {
-   uint64_t connId;
-   AuthConnInfo connInfo;
-   bool fromServer;
-   AuthDataHead head;
-   uint32_t len;
-   uint8_t data[0];
+    uint64_t connId;
+    AuthConnInfo connInfo;
+    bool fromServer;
+    AuthDataHead head;
+    uint32_t len;
+    uint8_t data[0];
 } RepeatDeviceIdData;
 
 typedef struct {
-   void (*onConnectResult)(uint32_t requestId, uint64_t connId, int32_t result, const AuthConnInfo *connInfo);
-   void (*onDisconnected)(uint64_t connId, const AuthConnInfo *connInfo);
-   void (*onDataReceived)(
+    void (*onConnectResult)(uint32_t requestId, uint64_t connId, int32_t result, const AuthConnInfo *connInfo);
+    void (*onDisconnected)(uint64_t connId, const AuthConnInfo *connInfo);
+    void (*onDataReceived)(
        uint64_t connId, const AuthConnInfo *connInfo, bool fromServer, const AuthDataHead *head, const uint8_t *data);
 } AuthConnListener;
 
 typedef struct {
-   int32_t fd;
-   int32_t ret;
+    int32_t fd;
+    int32_t ret;
 } AuthConnectResult;
 
 #define CONN_INFO         "conn=%{public}s:%{public}u"
