b86b1c7e7dafef05a7663c8aa154802ac85066eb
lbp <libaoping1@huawei.com>
2025-08-27 11:08:31 +0800

    fix:Optimize use case code
    Signed-off-by: lbp <libaoping1@huawei.com>

--- tests/core/adapter/unittest/lnn_kv_adapter_test.cpp
+++ tests/core/adapter/unittest/lnn_kv_adapter_test.cpp
@@ -356,34 +356,6 @@ HWTEST_F(KVAdapterTest, CloudSync001, TestSize.Level1)
 }
 
 /**
- * @tc.name: CloudSyncCallback001
- * @tc.desc: CloudSyncCallback succeed.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(KVAdapterTest, CloudSyncCallback001, TestSize.Level1)
-{
-    DistributedKv::ProgressDetail detail;
-    detail.code = DistributedKv::Status::SUCCESS;
-    detail.progress = DistributedKv::Progress::SYNC_FINISH;
-    kvStore->CloudSyncCallback(std::move(detail));
-}
-
-/**
- * @tc.name: CloudSyncCallback002
- * @tc.desc: CloudSyncCallback failed, Status code is ERROR.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(KVAdapterTest, CloudSyncCallback002, TestSize.Level1)
-{
-    DistributedKv::ProgressDetail detail;
-    detail.code = DistributedKv::Status::ERROR;
-    detail.progress = DistributedKv::Progress::SYNC_FINISH;
-    kvStore->CloudSyncCallback(std::move(detail));
-}
-
-/**
  * @tc.name: DeleteDataChangeListener001
  * @tc.desc: DeleteDataChangeListener is ok.
  * @tc.type: FUNC
