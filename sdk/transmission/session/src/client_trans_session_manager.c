0e00c5145523eda9caf80d5da222b496a8c58a98
yaowang <yaopwang@huawei.com>
2025-08-30 03:36:15 +0000

    update sdk/transmission/session/src/client_trans_session_manager.c.
    Signed-off-by: yaowang <yaopwang@huawei.com>

--- sdk/transmission/session/src/client_trans_session_manager.c
+++ sdk/transmission/session/src/client_trans_session_manager.c
@@ -2439,7 +2439,7 @@ int32_t ClientGetSessionNameByChannelId(int32_t channelId, int32_t channelType, 
         LIST_FOR_EACH_ENTRY(sessionNode, &(serverNode->sessionList), SessionInfo, node) {
             if (sessionNode->channelId != channelId || sessionNode->channelType != (ChannelType)channelType) {
                 TRANS_LOGE(
-                    TRANS_SDK, "channelId mismatch sessionNode:%{public}d channelId:%{public}d", sessionNode->channelId, channelId);
+                    TRANS_SDK, "channelId mismatch sessionNode->channelId:%{public}d channelId:%{public}d", sessionNode->channelId, channelId);
                 UnlockClientSessionServerList();
                 return SOFTBUS_INVALID_PARAM;
             }
