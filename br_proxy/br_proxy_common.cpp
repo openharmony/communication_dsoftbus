c91181d5df155ba078c2af0713d4b06f91f35230
黄凯 <huangkai71@huawei.com>
2025-09-04 03:14:50 +0000

    update br_proxy/br_proxy_common.cpp.
    Signed-off-by: 黄凯 <huangkai71@huawei.com>

--- br_proxy/br_proxy_common.cpp
+++ br_proxy/br_proxy_common.cpp
@@ -12,13 +12,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include <atomic>
+#include <cstring>
 #include <dlfcn.h>
 #include <securec.h>
-#include <cstring>
+#include <mutex>
 #include "accesstoken_kit.h"
+#include "br_proxy_common.h"
+#include "br_proxy_server_manager.h"
 #include "ipc_skeleton.h"
 #include "lnn_ohos_account_adapter.h"
+#include "message_handler.h"
+#include "softbus_adapter_mem.h"
 #include "softbus_error_code.h"
 #include "trans_log.h"
 
@@ -27,24 +32,31 @@
 #else
 #define BR_PROXY_ADAPTER_PATH "/system/lib/libbr_proxy_adapter.z.so"
 #endif
-
  
 using namespace OHOS;
 
 typedef int32_t (*GetAbilityNameFunc)(char *abilityName, int32_t userId, uint32_t abilityNameLen,
-    std::string bundleName);
-typedef int32_t (*StartAbilityFunc)(const char *bundleName, const char *abilityName);
+    std::string bundleName, int32_t *appIndex);
+typedef int32_t (*StartAbilityFunc)(const char *bundleName, const char *abilityName, int32_t appIndex);
 static void* g_abilityMgrHandle = nullptr;
 static StartAbilityFunc g_startAbilityFunc = nullptr;
 static GetAbilityNameFunc g_getAbilityName = nullptr;
+static int32_t BrProxyLoopInit(void);
 
-static int32_t AbilityManagerClientDynamicLoader(const char *bundleName, const char *abilityName)
+static int32_t AbilityManagerClientDynamicLoader(const char *bundleName, const char *abilityName, int32_t appIndex)
 {
     if (bundleName == nullptr || abilityName == nullptr) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] invalid param");
         return SOFTBUS_INVALID_PARAM;
     }
-
+    int32_t ret = BrProxyLoopInit();
+    if (ret != SOFTBUS_OK) {
+        TRANS_LOGE(TRANS_SVC, "[br_proxy] BrProxyLoopInit failed! ret=%{public}d", ret);
+        return ret;
+    }
+    if (g_abilityMgrHandle == nullptr) {
+        g_abilityMgrHandle = dlopen(BR_PROXY_ADAPTER_PATH, RTLD_LAZY);
+    }
     g_abilityMgrHandle = dlopen(BR_PROXY_ADAPTER_PATH, RTLD_LAZY);
     if (g_abilityMgrHandle == nullptr) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] dlopen failed!");
@@ -59,17 +71,20 @@ static int32_t AbilityManagerClientDynamicLoader(const char *bundleName, const c
         return SOFTBUS_INVALID_PARAM;
     }
 
-    return g_startAbilityFunc(bundleName, abilityName);
+    return g_startAbilityFunc(bundleName, abilityName, appIndex);
 }
 
-static int32_t GetAbilityName(char *abilityName, int32_t userId, uint32_t abilityNameLen, std::string bundleName)
+static int32_t GetAbilityName(char *abilityName, int32_t userId, uint32_t abilityNameLen,
+    std::string bundleName, int32_t *appIndex)
 {
-    if (abilityName == nullptr) {
+    if (abilityName == nullptr || appIndex == nullptr) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] invalid param");
         return SOFTBUS_INVALID_PARAM;
     }
 
-    g_abilityMgrHandle = dlopen(BR_PROXY_ADAPTER_PATH, RTLD_LAZY);
+    if (g_abilityMgrHandle == nullptr) {
+        g_abilityMgrHandle = dlopen(BR_PROXY_ADAPTER_PATH, RTLD_LAZY);
+    }
     if (g_abilityMgrHandle == nullptr) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] dlopen failed!");
         return SOFTBUS_INVALID_PARAM;
@@ -83,7 +98,7 @@ static int32_t GetAbilityName(char *abilityName, int32_t userId, uint32_t abilit
         return SOFTBUS_INVALID_PARAM;
     }
 
-    int32_t ret = g_getAbilityName(abilityName, userId, abilityNameLen, bundleName);
+    int32_t ret = g_getAbilityName(abilityName, userId, abilityNameLen, bundleName, appIndex);
     if (ret != SOFTBUS_OK) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] failed, ret = %{public}d", ret);
         return ret;
@@ -103,14 +118,15 @@ void BrProxyDynamicLoaderDeInit()
     }
 }
 
-extern "C" int32_t PullUpHap(const char *bundleName, const char *abilityName)
+extern "C" int32_t PullUpHap(const char *bundleName, const char *abilityName, int32_t appIndex)
 {
-    int32_t ret = AbilityManagerClientDynamicLoader(bundleName, abilityName);
+    #define CLOSE_DELAY_TIME 3000
+    int32_t ret = AbilityManagerClientDynamicLoader(bundleName, abilityName, appIndex);
     if (ret != SOFTBUS_OK) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] failed, ret = %{public}d", ret);
         return ret;
     }
-    BrProxyDynamicLoaderDeInit();
+    BrProxyPostDcloseMsgToLooperDelay(CLOSE_DELAY_TIME);
     return SOFTBUS_OK;
 }
 
@@ -130,8 +146,11 @@ extern "C" uint32_t GetCallerTokenId()
 }
 
 extern "C" int32_t GetCallerHapInfo(char *bundleName, uint32_t bundleNamelen,
-    char *abilityName, uint32_t abilityNameLen)
+    char *abilityName, uint32_t abilityNameLen, int32_t *appIndex)
 {
+    if (bundleName == nullptr || abilityName == nullptr || appIndex == nullptr) {
+        return SOFTBUS_INVALID_PARAM;
+    }
     auto callerToken = IPCSkeleton::GetCallingTokenID();
     auto type = Security::AccessToken::AccessTokenKit::GetTokenType(callerToken);
     if (type != Security::AccessToken::ATokenTypeEnum::TOKEN_HAP) {
@@ -145,7 +164,7 @@ extern "C" int32_t GetCallerHapInfo(char *bundleName, uint32_t bundleNamelen,
         return SOFTBUS_STRCPY_ERR;
     }
     int32_t userId = GetActiveOsAccountIds();
-    int32_t ret = GetAbilityName(abilityName, userId, abilityNameLen, hapTokenInfoRes.bundleName);
+    int32_t ret = GetAbilityName(abilityName, userId, abilityNameLen, hapTokenInfoRes.bundleName, appIndex);
     if (ret != SOFTBUS_OK) {
         TRANS_LOGE(TRANS_SVC, "[br_proxy] get abilityName failed, ret=%{public}d", ret);
         return ret;
@@ -163,5 +182,83 @@ extern "C" int32_t CheckPushPermission()
         return SOFTBUS_TRANS_TOKEN_HAP_ERR;
     }
     TRANS_LOGI(TRANS_SVC, "[br_proxy] The push identity passes the authentication.");
+    return SOFTBUS_OK;
+}
+
+const char *g_brProxyHandlerName = "brProxyHandlerName";
+SoftBusHandler g_brProxyLooperHandler = { 0 };
+typedef enum {
+    LOOP_DCLOSE_MSG,
+} BrProxyLoopMsg;
+ 
+static void BrProxyFreeLoopMsg(SoftBusMessage *msg)
+{
+    if (msg != nullptr) {
+        if (msg->obj != nullptr) {
+            SoftBusFree(msg->obj);
+        }
+        SoftBusFree((void *)msg);
+    }
+}
+ 
+static SoftBusMessage *BrProxyCreateLoopMsg(int32_t what, uint64_t arg1, uint64_t arg2, char *data)
+{
+    SoftBusMessage *msg = (SoftBusMessage *)SoftBusCalloc(sizeof(SoftBusMessage));
+    if (msg == nullptr) {
+        TRANS_LOGE(TRANS_MSG, "[br_proxy] msg calloc failed");
+        return nullptr;
+    }
+    msg->what = what;
+    msg->arg1 = arg1;
+    msg->arg2 = arg2;
+    msg->handler = &g_brProxyLooperHandler;
+    msg->FreeMessage = BrProxyFreeLoopMsg;
+    msg->obj = (void *)data;
+    return msg;
+}
+ 
+void BrProxyPostDcloseMsgToLooperDelay(uint32_t delayTime)
+{
+    SoftBusMessage *msg  = BrProxyCreateLoopMsg(LOOP_DCLOSE_MSG, 0, 0, nullptr);
+    TRANS_CHECK_AND_RETURN_LOGE(msg != nullptr, TRANS_CTRL, "[br_proxy] msg create failed");
+ 
+    g_brProxyLooperHandler.looper->PostMessageDelay(g_brProxyLooperHandler.looper, msg, delayTime);
+}
+ 
+static void BrProxyLoopMsgHandler(SoftBusMessage *msg)
+{
+    TRANS_CHECK_AND_RETURN_LOGE(msg != nullptr, TRANS_CTRL, "[br_proxy] param invalid");
+    TRANS_LOGI(TRANS_CTRL, "[br_proxy] trans loop process msgType=%{public}d", msg->what);
+    switch (msg->what) {
+        case LOOP_DCLOSE_MSG: {
+            BrProxyDynamicLoaderDeInit();
+            break;
+        }
+        default:
+            break;
+    }
+}
+ 
+static std::atomic<bool> hasInit(false);
+static std::mutex initMutex;
+ 
+static int32_t BrProxyLoopInit(void)
+{
+    if (hasInit.load()) {
+        return SOFTBUS_OK;
+    }
+ 
+    std::lock_guard<std::mutex> lock(initMutex);
+    if (hasInit.load()) {
+        return SOFTBUS_OK;
+    }
+    g_brProxyLooperHandler.name = (char *)g_brProxyHandlerName;
+    g_brProxyLooperHandler.looper = GetLooper(LOOP_TYPE_DEFAULT);
+    if (g_brProxyLooperHandler.looper == nullptr) {
+        TRANS_LOGE(TRANS_SVC, "[br_proxy] loop init failed");
+        return SOFTBUS_TRANS_INIT_FAILED;
+    }
+    g_brProxyLooperHandler.HandleMessage = BrProxyLoopMsgHandler;
+    hasInit.store(true);
     return SOFTBUS_OK;
 }
