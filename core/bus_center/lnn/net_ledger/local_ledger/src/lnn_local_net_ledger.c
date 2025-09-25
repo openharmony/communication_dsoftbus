34a05feff5630abd9dfec010ad6ef1066182ce25
wang_mingxu <wangmingxu9@h-partners.com>
2025-09-25 11:39:19 +0800

    head unit control_panel network onlie 2
    Signed-off-by: wang_mingxu <wangmingxu9@h-partners.com>

--- core/bus_center/lnn/net_ledger/local_ledger/src/lnn_local_net_ledger.c
+++ core/bus_center/lnn/net_ledger/local_ledger/src/lnn_local_net_ledger.c
@@ -2005,8 +2005,28 @@ static int32_t UpdateLocalUserId(const void *userId)
 }
 
 static int32_t UpdateDisplayId(const void *displayId)
+{
+    if (displayId == NULL) {
+        LNN_LOGE(LNN_LEDGER, "invalid param");
+        return SOFTBUS_INVALID_PARAM;
+    }
+    g_localNetLedger.localInfo.displayId = *(uint64_t *)displayId;
+    if (LnnSaveLocalDeviceInfoPacked(&g_localNetLedger.localInfo) != SOFTBUS_OK) {
+        LNN_LOGE(LNN_LEDGER, "update Bt mac to localdevinfo store fail");
+    }
+    return SOFTBUS_OK;
+}
 
 static int32_t L1GetDisplayId(void *displayId, uint32_t len)
+{
+    NodeInfo *info = &g_localNetLedger.localInfo;
+    if (displayId == NULL || len != sizeof(uint64_t)) {
+        LNN_LOGE(LNN_LEDGER, "invalid param");
+        return SOFTBUS_INVALID_PARAM;
+    }
+    *((uint64_t *)displayId) = info->displayId;
+    return SOFTBUS_OK;
+}
 
 static int32_t L1GetUserId(void *userId, uint32_t len)
 {
@@ -2198,6 +2218,7 @@ static LocalLedgerKey g_localKeyTable[] = {
     {BYTE_KEY_UDID_HASH, SHA_256_HASH_LEN, LlGetUdidHash, NULL},
     {BOOL_KEY_SCREEN_STATUS, NODE_SCREEN_STATUS_LEN, L1GetNodeScreenOnFlag, NULL},
     {BYTE_KEY_SPARK_CHECK, SPARK_CHECK_LENGTH, LlGetSparkCheck, UpdateLocalSparkCheck},
+    {NUM_KEY_DISPLAY_ID, sizeof(uint64_t), L1GetDisplayId, UpdateDisplayId},
 };
 
 static LocalLedgerKeyByIfname g_localKeyByIfnameTable[] = {
@@ -2624,6 +2645,11 @@ int32_t LnnGetLocalNumU64Info(InfoKey key, uint64_t *info)
 }
 
 int32_t LnnSetLocalNum64Info(InfoKey key, int64_t info)
+{
+    return LnnSetLocalInfo(key, (void *)&info);
+}
+
+int32_t LnnSetLocalNumU64Info(InfoKey key, uint64_t info)
 {
     return LnnSetLocalInfo(key, (void *)&info);
 }
