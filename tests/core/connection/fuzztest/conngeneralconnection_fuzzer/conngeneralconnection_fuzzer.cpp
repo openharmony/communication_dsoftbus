d11ad4f6035230a579cac3f03d8369af1b966c24
wu-fengshan <wufengshan1@h-partners.com>
2025-11-08 11:09:24 +0800

    add fuzz
    Signed-off-by: wu-fengshan <wufengshan1@h-partners.com>

--- tests/core/connection/fuzztest/conngeneralconnection_fuzzer/conngeneralconnection_fuzzer.cpp
+++ tests/core/connection/fuzztest/conngeneralconnection_fuzzer/conngeneralconnection_fuzzer.cpp
@@ -19,8 +19,86 @@
 #include "softbus_conn_interface_struct.h"
 
 #include "softbus_conn_general_connection.c"
-
+#include "softbus_conn_general_negotiation.c"
+#include "softbus_conn_ipc.c"
 namespace OHOS {
+static struct GeneralConnection *g_generalConnection = NULL;
+static void CreateParam(GeneralConnectionParam &param)
+{
+    if (strcpy_s(param.name, GENERAL_NAME_LEN, "test")) {
+        return;
+    }
+    if (strcpy_s(param.pkgName, PKG_NAME_SIZE_MAX, "testPkgName")) {
+        return;
+    }
+    if (strcpy_s(param.bundleName, BUNDLE_NAME_MAX, "testBundleNameServer")) {
+        return;
+    }
+}
+
+static void PrepareConnection(bool isClient)
+{
+    GeneralConnectionParam param = {
+        .pid = 0,
+    };
+    CreateParam(param);
+    const char *addr = "11:22:33:44:55:66";
+    int32_t ret = SOFTBUS_OK;
+    g_generalConnection = CreateConnection(&param, addr, 1, isClient, &ret);
+    if (ret != SOFTBUS_OK) {
+        return;
+    }
+}
+
+static void DestroyConnection(void)
+{
+    SoftBusMutexLock(&g_generalManager.connections->lock);
+    struct GeneralConnection *item = NULL;
+    struct GeneralConnection *nextItem = NULL;
+    LIST_FOR_EACH_ENTRY_SAFE(item, nextItem, &g_generalManager.connections->list, struct GeneralConnection, node) {
+        if (item->underlayerHandle == g_generalConnection->underlayerHandle) {
+            ListDelete(&item->node);
+            ConnReturnGeneralConnection(&item);
+        }
+    }
+    g_generalConnection = nullptr;
+    (void)SoftBusMutexUnlock(&g_generalManager.connections->lock);
+}
+
+void OnConnectSuccessFuzz(GeneralConnectionParam *info, uint32_t generalHandle)
+{
+    (void)info;
+    (void)generalHandle;
+}
+
+void OnConnectFailedFuzz(GeneralConnectionParam *info, uint32_t generalHandle, int32_t reason)
+{
+    (void)info;
+    (void)generalHandle;
+    (void)reason;
+}
+
+void OnAcceptConnectFuzz(GeneralConnectionParam *info, uint32_t generalHandle)
+{
+    (void)info;
+    (void)generalHandle;
+}
+
+void OnDataReceivedFuzz(GeneralConnectionParam *info, uint32_t generalHandle, const uint8_t *data, uint32_t dataLen)
+{
+    (void)info;
+    (void)generalHandle;
+    (void)data;
+    (void)dataLen;
+}
+
+void OnConnectionDisconnectedFuzz(GeneralConnectionParam *info, uint32_t generalHandle, int32_t reason)
+{
+    (void)info;
+    (void)generalHandle;
+    (void)reason;
+}
+
 void OnCommDataReceivedTest()
 {
     uint32_t connectionId;
@@ -44,6 +122,14 @@ void SendTest()
     GenerateInt32(pid);
 
     Send(generalHandle, payload.data(), payload.size(), pid);
+
+    PrepareConnection(true);
+    if (g_generalConnection == nullptr) {
+        return;
+    }
+    SaveConnection(g_generalConnection);
+    GetConnectionByGeneralIdAndCheckPid(g_generalConnection->generalId, 1);
+    DestroyConnection();
 }
 
 void ConnectTest(FuzzedDataProvider &provider)
@@ -85,6 +171,10 @@ void CreateServerTest(FuzzedDataProvider &provider)
     GenerateInt32(param.pid);
 
     CreateServer(&param);
+
+    CreateParam(param);
+    CreateServer(&param);
+    CreateServer(&param);
 }
 
 void CloseServerTest(FuzzedDataProvider &provider)
@@ -99,6 +189,9 @@ void CloseServerTest(FuzzedDataProvider &provider)
         return;
     }
 
+    CloseServer(&param);
+
+    CreateParam(param);
     CloseServer(&param);
 }
 
@@ -136,20 +229,108 @@ void ClearAllGeneralConnectionTest(FuzzedDataProvider &provider)
 
     ClearAllGeneralConnection(pkgName, pid);
 }
+
+void ProcessInnerMessageByTypeTest(FuzzedDataProvider &provider)
+{
+    GeneralConnectionInfo info = {{0}};
+    ProcessInnerMessageByType(0, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE, &info);
+    ProcessInnerMessageByType(0, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE_ACK, &info);
+    ProcessInnerMessageByType(0, GENERAL_CONNECTION_MSG_TYPE_MERGE, &info);
+    ProcessInnerMessageByType(0, GENERAL_CONNECTION_MSG_TYPE_RESET, &info);
+    ProcessInnerMessageByType(0, GENERAL_CONNECTION_MSG_TYPE_MAX, &info);
+}
+
+void OnCommDisconnected(FuzzedDataProvider &provider)
+{
+    ConnectionInfo info = {0};
+    OnCommDisconnected(1, &info);
+
+    PrepareConnection(true);
+    if (g_generalConnection == nullptr) {
+        return;
+    }
+    g_generalConnection->underlayerHandle = 1;
+    SaveConnection(g_generalConnection);
+    OnCommDisconnected(g_generalConnection->underlayerHandle, &info);
+    DestroyConnection();
+
+    PrepareConnection(false);
+    if (g_generalConnection == nullptr) {
+        return;
+    }
+    g_generalConnection->underlayerHandle = 1;
+    g_generalConnection->state = STATE_CONNECTED;
+    SaveConnection(g_generalConnection);
+    OnCommDisconnected(g_generalConnection->underlayerHandle, &info);
+    DestroyConnection();
+}
+
+void GeneralConnectionPackMsgTest(FuzzedDataProvider &provider)
+{
+    GeneralConnectionInfo info;
+    std::string name = provider.ConsumeRandomLengthString(GENERAL_NAME_LEN - 1);
+    if (strcpy_s(info.name, GENERAL_NAME_LEN, name.c_str()) != EOK) {
+        return;
+    }
+    std::string bundleName = provider.ConsumeRandomLengthString(BUNDLE_NAME_MAX - 1);
+    if (strcpy_s(info.bundleName, BUNDLE_NAME_MAX, bundleName.c_str()) != EOK) {
+        return;
+    }
+    GenerateUint32(info.abilityBitSet);
+    GenerateInt32(info.ackStatus);
+    GenerateUint32(info.updateHandle);
+    GenerateUint32(info.localId);
+    GenerateUint32(info.peerId);
+    GeneralConnectionPackMsg(&info, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE);
+    GeneralConnectionPackMsg(&info, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE_ACK);
+    GeneralConnectionPackMsg(&info, GENERAL_CONNECTION_MSG_TYPE_RESET);
+    GeneralConnectionPackMsg(&info, GENERAL_CONNECTION_MSG_TYPE_MERGE);
+    GeneralConnectionPackMsg(&info, GENERAL_CONNECTION_MSG_TYPE_NORMAL);
+}
+
+void GeneralConnectionUnpackTest(FuzzedDataProvider &provider)
+{
+    std::vector<uint8_t> data;
+    GeneratePayload(data);
+    GeneralConnectionInfo info = {{0}};
+    GeneralConnectionUnpackMsg(data.data(), data.size(), &info, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE);
+    GeneralConnectionUnpackMsg(data.data(), data.size(), &info, GENERAL_CONNECTION_MSG_TYPE_HANDSHAKE_ACK);
+    GeneralConnectionUnpackMsg(data.data(), data.size(), &info, GENERAL_CONNECTION_MSG_TYPE_RESET);
+    GeneralConnectionUnpackMsg(data.data(), data.size(), &info, GENERAL_CONNECTION_MSG_TYPE_MERGE);
+    GeneralConnectionUnpackMsg(data.data(), data.size(), &info, GENERAL_CONNECTION_MSG_TYPE_NORMAL);
+}
 } // namespace OHOS
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     DataGenerator::Write(data, size);
     FuzzedDataProvider provider(data, size);
+    static bool isInit = false;
+    if (!isInit) {
+        LooperInit();
+        ConnServerInit();
+        static GeneralConnectionListener listener = {
+            .onAcceptConnect = OHOS::OnAcceptConnectFuzz,
+            .onConnectFailed = OHOS::OnConnectFailedFuzz,
+            .onConnectionDisconnected = OHOS::OnConnectionDisconnectedFuzz,
+            .onConnectSuccess = OHOS::OnConnectSuccessFuzz,
+            .onDataReceived = OHOS::OnDataReceivedFuzz,
+        };
+        RegisterListener(&listener);
+    }
 
     OHOS::OnCommDataReceivedTest();
     OHOS::SendTest();
     OHOS::ConnectTest(provider);
     OHOS::CreateServerTest(provider);
     OHOS::GetPeerDeviceIdTest(provider);
+    OHOS::ProcessInnerMessageByTypeTest(provider);
+    OHOS::OnCommDisconnected(provider);
     OHOS::ClearAllGeneralConnectionTest(provider);
-
+    OHOS::CloseServerTest(provider);
+    OHOS::GeneralConnectionPackMsgTest(provider);
+    OHOS::GeneralConnectionUnpackTest(provider);
+    sleep(1);
     DataGenerator::Clear();
     return 0;
 }
