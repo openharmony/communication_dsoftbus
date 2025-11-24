ee5476f6ed1253eaaa9239e8693f912e52df702c
huangkai71 <huangkai71@huawei.com>
2025-11-24 20:52:42 +0800

    br_proxy代码优化
    Signed-off-by: huangkai71 <huangkai71@huawei.com>

--- br_proxy/br_proxy_module.c
+++ br_proxy/br_proxy_module.c
@@ -260,7 +260,8 @@ static int32_t BrProxyPostCond(int32_t sessionId)
         }
         int32_t ret = SoftBusCondSignal(&nodeInfo->cond);
         if (ret != SOFTBUS_OK) {
-            TRANS_LOGE(TRANS_SDK, "[br_proxy] cond signal failed! sessionId:%{public}d, ret:%{public}d", sessionId, ret);
+            TRANS_LOGE(TRANS_SDK, "[br_proxy] cond signal failed!
+                sessionId:%{public}d, ret:%{public}d", sessionId, ret);
             (void)SoftBusMutexUnlock(&(g_sessionList->lock));
             return ret;
         }
