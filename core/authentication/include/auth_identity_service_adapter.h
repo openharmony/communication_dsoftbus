928e2624d8890d15a387b701d475349f050fec0d
ma-haoyun <mahaoyun2@huawei.com>
2026-02-24 17:54:30 +0800

    fix:add diffs for OH
    Signed-off-by: ma-haoyun <mahaoyun2@huawei.com>

--- core/authentication/include/auth_identity_service_adapter.h
+++ core/authentication/include/auth_identity_service_adapter.h
@@ -19,6 +19,7 @@
 #include "auth_hichain.h"
 #include "device_auth.h"
 #include "softbus_common.h"
+#include "softbus_def.h"
 
 #ifdef __cplusplus
 #if __cplusplus
@@ -26,16 +27,29 @@ extern "C" {
 #endif
 #endif
 
+typedef enum {
+    ACCOUNT_RELATED = 1, /**< ACCOUNT_RELATED form identity_service_define */
+    ACCOUNT_UNRELATED = 2, /**< ACCOUNT_UNRELATED form identity_service_define */
+    ACCOUNT_SHARED = 3, /**< ACCOUNT_SHARED form identity_service_define */
+    ACCOUNT_BUTT,
+} SoftbusCredType;
+
+typedef enum {
+    SUBJECT_MASTER_CONTROLLER = 1, /**< SUBJECT_MASTER_CONTROLLER form identity_service_define */
+    SUBJECT_ACCESSORY_DEVICE = 2, /**< SUBJECT_ACCESSORY_DEVICE form identity_service_define */
+} SoftBusSubjectType;
+
 typedef struct {
-    int32_t credIdType;
+    SoftbusCredType credIdType;
     int32_t subject;
     char udid[UDID_BUF_LEN];
     char userId[MAX_ACCOUNT_HASH_LEN];
+    char credId[CRED_ID_STR_LEN];
 } SoftBusCredInfo;
 
 int32_t IdServiceRegCredMgr(void);
 void IdServiceUnRegCredMgr(void);
-bool IdServiceIsPotentialTrustedDevice(const char *udidHash, const char *accountIdHash, bool isSameAccount);
+bool IdServiceIsPotentialTrustedDevice(const char *shortUdidHash, const char *shortAccountIdHash, bool isSameAccount);
 
 int32_t IdServiceQueryCredential(int32_t userId, const char *udidHash, const char *accountidHash,
     bool isSameAccount, char **credList);
@@ -47,6 +61,8 @@ int32_t IdServiceAuthCredential(int32_t userId, int64_t authReqId, const char *a
 int32_t IdServiceProcessCredData(int64_t authSeq, const uint8_t *data, uint32_t len, DeviceAuthCallback *cb);
 char *IdServiceGetCredIdFromCredList(int32_t userId, const char *credList);
 void IdServiceDestroyCredentialList(char **returnData);
+int32_t IdServiceGetCredTypeByCredId(int32_t userId, const char *credId, int32_t *credType);
+int32_t IdServiceGetCredInfoByUdid(const char *udid, SoftBusCredInfo *credInfo);
 
 #ifdef __cplusplus
 #if __cplusplus
