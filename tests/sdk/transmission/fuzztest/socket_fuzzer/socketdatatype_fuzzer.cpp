01ebeea31d06810db253a641fce5dc7362bf15bb
wangyipeng <wangyipeng11@huawei.com>
2026-03-24 19:46:19 +0800

    Co-Authored-By:Agent
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

---
+++ tests/sdk/transmission/fuzztest/socket_fuzzer/socketdatatype_fuzzer.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "socket_fuzzer.h"
+
+#include <securec.h>
+#include <string>
+
+#include "fuzz_data_generator.h"
+#include "socket.h"
+
+namespace OHOS {
+void SocketTestWithDataType(const uint8_t *data, size_t size)
+{
+    if ((data == nullptr) || (size < sizeof(TransDataType))) {
+        return;
+    }
+
+    TransDataType socketDataType = DATA_TYPE_BUTT;
+    if (memcpy_s(&socketDataType, sizeof(TransDataType), data, sizeof(TransDataType)) != EOK) {
+        return;
+    }
+
+    SocketInfo info = {
+        .name = const_cast<char *>(DEFAULT_SOCKET_NAME),
+        .peerName = const_cast<char *>(DEFAULT_SOCKET_PEER_NAME),
+        .peerNetworkId = const_cast<char *>(DEFAULT_SOCKET_PEER_NETWORKID),
+        .pkgName = const_cast<char *>(DEFAULT_SOCKET_PKG_NAME),
+        .dataType = socketDataType,
+    };
+
+    (void)Socket(info);
+}
+
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int32_t LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::SocketTestWithDataType(data, size);
+    return 0;
+}
