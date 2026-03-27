8bc37071d4f92d2ee2cda030e8bd9ac341cb0f2d
shen_panda <shenjiahao5@huawei.com>
2026-03-27 14:59:46 +0800

    fix add mock file
    Signed-off-by: shen_panda <shenjiahao5@huawei.com>

--- tests/core/authentication/unittest/auth_test.cpp
+++ tests/core/authentication/unittest/auth_test.cpp
@@ -20,12 +20,14 @@
 
 #include "auth_channel.h"
 #include "auth_common.h"
+#include "auth_common_mock.h"
 #include "auth_connection.h"
 #include "auth_hichain.h"
 #include "auth_interface.c"
 #include "auth_interface.h"
 #include "auth_log.h"
 #include "auth_manager.h"
+#include "auth_net_ledger_mock.h"
 #include "auth_request.h"
 #include "auth_session_fsm.h"
 #include "auth_session_key.h"
@@ -41,6 +43,7 @@
 #include "softbus_socket.h"
 
 namespace OHOS {
+using namespace testing;
 using namespace testing::ext;
 constexpr uint32_t TEST_DATA_LEN = 10;
 constexpr uint32_t CRYPT_DATA_LEN = 200;
@@ -2442,5 +2445,160 @@ HWTEST_F(AuthTest, AUTH_DISCONNECT_Test_001, TestSize.Level1)
 {
     uint64_t connId = GenConnId(AUTH_LINK_TYPE_RAW_ENHANCED_P2P, 1);
     EXPECT_NO_FATAL_FAILURE(DisconnectAuthDevice(&connId));
+}
+
+/*
+ * @tc.name: CONVERT_TO_DISCOVERET_TYPE_TEST_001
+ * @tc.desc: auth ConvertToDiscoveryType test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, CONVERT_TO_DISCOVERET_TYPE_TEST_001, TestSize.Level1)
+{
+    DiscoveryType type;
+    AuthLinkType ret;
+    type = DISCOVERY_TYPE_WIFI;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_WIFI);
+
+    type = DISCOVERY_TYPE_BLE;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_BLE);
+
+    type = DISCOVERY_TYPE_SLE;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_SLE);
+
+    type = DISCOVERY_TYPE_BR;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_BR);
+
+    type = DISCOVERY_TYPE_P2P;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_P2P);
+
+    type = DISCOVERY_TYPE_SESSION_KEY;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_SESSION_KEY);
+
+    type = DISCOVERY_TYPE_USB;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_USB);
+
+    type = DISCOVERY_TYPE_COUNT;
+    ret = ConvertToAuthLinkType(type);
+    EXPECT_EQ(ret, AUTH_LINK_TYPE_MAX);
+}
+
+/*
+ * @tc.name: AUTH_GENSEQ_TEST_001
+ * @tc.desc: auth GenSeq test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_GENSEQ_TEST_001, TestSize.Level1)
+{
+    bool isServer = false;
+    NiceMock<AuthNetLedgertInterfaceMock> ledgerMock;
+    EXPECT_CALL(ledgerMock, LnnGetLocalStrInfo).WillOnce(Return(SOFTBUS_ERR));
+    int64_t ret = GenSeq(isServer);
+    EXPECT_NE(ret, 0);
+}
+
+/*
+ * @tc.name: AUTH_GENSEQ_TEST_002
+ * @tc.desc: auth GenSeq test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_GENSEQ_TEST_002, TestSize.Level1)
+{
+    bool isServer = false;
+    int64_t ret = 0;
+    NiceMock<AuthNetLedgertInterfaceMock> ledgerMock;
+    NiceMock<AuthCommonInterfaceMock> authMock;
+
+    EXPECT_CALL(ledgerMock, LnnGetLocalStrInfo).WillOnce(Return(SOFTBUS_OK));
+    EXPECT_CALL(authMock, SoftBusGenerateStrHash).WillOnce(Return(SOFTBUS_ERR));
+    ret = GenSeq(isServer);
+    EXPECT_NE(ret, 0);
+}
+
+/*
+ * @tc.name: AUTH_GENSEQ_TEST_003
+ * @tc.desc: auth GenSeq test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_GENSEQ_TEST_003, TestSize.Level1)
+{
+    bool isServer = false;
+    int64_t ret = 0;
+    NiceMock<AuthNetLedgertInterfaceMock> ledgerMock;
+    NiceMock<AuthCommonInterfaceMock> authMock;
+
+    EXPECT_CALL(ledgerMock, LnnGetLocalStrInfo).WillOnce(Return(SOFTBUS_OK));
+    EXPECT_CALL(authMock, SoftBusGenerateStrHash).WillOnce(Return(SOFTBUS_OK));
+    ret = GenSeq(isServer);
+    EXPECT_NE(ret, 0);
+}
+
+/*
+ * @tc.name: AUTH_COMNMO_DENINIT_TEST_001
+ * @tc.desc: auth AuthCommonInit test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_COMNMO_DENINIT_TEST_001, TestSize.Level1)
+{
+    NiceMock<AuthCommonInterfaceMock> authMock;
+    EXPECT_CALL(authMock, SoftBusMutexDestroy).WillOnce(Return(SOFTBUS_ERR));
+    EXPECT_NO_FATAL_FAILURE(AuthCommonDeinit());
+
+    EXPECT_CALL(authMock, SoftBusMutexDestroy).WillOnce(Return(SOFTBUS_OK));
+    EXPECT_NO_FATAL_FAILURE(AuthCommonDeinit());
+}
+
+/*
+ * @tc.name: AUTH_COMNMON_INIT_TEST_001
+ * @tc.desc: auth AuthCommonInit test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_COMNMON_INIT_TEST_001, TestSize.Level1)
+{
+    int32_t ret = 0;
+    NiceMock<AuthCommonInterfaceMock> authMock;
+    EXPECT_CALL(authMock, SoftBusMutexInit).WillOnce(Return(SOFTBUS_ERR));
+    ret = AuthCommonInit();
+    EXPECT_EQ(ret, SOFTBUS_LOCK_ERR);
+
+    EXPECT_CALL(authMock, SoftBusMutexInit).WillOnce(Return(SOFTBUS_OK));
+    ret = AuthCommonInit();
+    EXPECT_EQ(ret, SOFTBUS_LOCK_ERR);
+}
+
+/*
+ * @tc.name: AUTH_GET_PEER_UDID_BY_NETWORKID_TEST_001
+ * @tc.desc: auth GetPeerUdidByNetworkId test
+ * @tc.type: FUNC
+ * @tc.level: Level1
+ * @tc.require:
+ */
+HWTEST_F(AuthTest, AUTH_GET_PEER_UDID_BY_NETWORKID_TEST_001, TestSize.Level1)
+{
+    int32_t ret = 0;
+    const char *networkId = "testudid";
+    char udid[UDID_BUF_LEN] = { 0 };
+    NiceMock<AuthNetLedgertInterfaceMock> ledgerMock;
+    EXPECT_CALL(ledgerMock, LnnRetrieveDeviceInfoByNetworkIdPacked).WillOnce(Return(SOFTBUS_ERR));
+    ret = GetPeerUdidByNetworkId(networkId, udid, UDID_BUF_LEN);
+    EXPECT_EQ(ret, SOFTBUS_NOT_FIND);
 }
 } // namespace OHOS
