991d63582be1c26239bf67745c664e1d54cb3dd8
xqwst <wangshuangtao4@h-partners.com>
2026-04-15 19:16:47 +0800

    fix ut error
    Signed-off-by: xqwst <wangshuangtao4@h-partners.com>

--- tests/adapter/bluetooth/softbus_adapter_bt_common_test.cpp
+++ tests/adapter/bluetooth/softbus_adapter_bt_common_test.cpp
@@ -23,7 +23,7 @@
 #include "assert_helper.h"
 #include "bluetooth_mock.h"
 
-#define STATE_LISTENER_MAX_NUM 22
+#define STATE_LISTENER_MAX_NUM 23
 
 using namespace testing::ext;
 using ::testing::_;
