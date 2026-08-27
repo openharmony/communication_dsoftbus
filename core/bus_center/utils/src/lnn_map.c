ac846ac1dcca6308fcb870c828c0ba676ea88788
xftz <fandonghai1@h-partners.com>
2026-08-27 11:30:50 +0800

    fix:Data verification optimization
    Signed-off-by: xftz <fandonghai1@h-partners.com>
    
    
    revert merge request: fix:Data verification optimization
    
    292660d785449d823a4a7861e5dc5a398979b11b
    
    This revert merge request !12454

--- core/bus_center/utils/src/lnn_map.c
+++ core/bus_center/utils/src/lnn_map.c
@@ -356,6 +356,9 @@ MapIterator *LnnMapInitIterator(Map *map)
  */
 bool LnnMapHasNext(MapIterator *it)
 {
+    if (it->map->nodeSize > HDF_MAP_KEY_MAX_SIZE) {
+        LNN_LOGW(LNN_STATE, "nodeSize=%{public}d", it->map->nodeSize);
+    }
     return (it->nodeNum < it->map->nodeSize);
 }
 
