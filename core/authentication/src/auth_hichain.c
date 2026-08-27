76969d79ef138f8789a99f4df669bc5305043aff
Lucky-M <mahaoyun2@h-partners.com>
2026-08-27 19:55:45 +0800

    update: 更新文件 auth_hichain.c
    Signed-off-by: Lucky-M <mahaoyun2@h-partners.com>

--- core/authentication/src/auth_hichain.c
+++ core/authentication/src/auth_hichain.c
@@ -406,11 +406,7 @@ static int32_t ParseGroupInfo(const char *groupInfoStr, GroupInfo *groupInfo)
     }
     groupInfo->groupType = (GroupType)groupType;
     groupInfo->userId = 0;
-    if (!GetJsonObjectInt32Item(msg, "osAccountId", &groupInfo->userId)) {
-        AUTH_LOGE(AUTH_HICHAIN, "get osAccountId fail");
-        cJSON_Delete(msg);
-        return SOFTBUS_AUTH_GET_GROUP_TYPE_FAIL;
-    }
+    (void)GetJsonObjectInt32Item(msg, "osAccountId", &groupInfo->userId);
     cJSON_Delete(msg);
     return SOFTBUS_OK;
 }
